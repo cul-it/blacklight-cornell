6096f1995fd1e0b1a6ac0548cd171313b9827d72
John A. Cline <jac244@lib-dev-008.serverfarm.cornell.edu>
2012-12-05 12:59:59 -0500

    new directory for capistrano

---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server "jac244-dev.library.cornell.edu", :app, :web, :db, :primary => true
+set :deploy_to, "/users/jac244/blacklight-cornell"
---
+++ config/deploy/staging.rb
@@ -0,0 +1,2 @@
+server "jac244-dev.library.cornell.edu", :app, :web
+set :deploy_to, "/users/jac244/blacklight-cornell-staging"
