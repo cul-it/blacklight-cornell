a2368523c3420470f9e44d2d03e1cc7e4a73c7c2
Nick Cappadona <nick.cappadona@cornell.edu>
2012-12-06 11:10:22 -0500

    Add migration file for sessions table
    rake db:migrate no longer breaks the app.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121203142624) do
+ActiveRecord::Schema.define(:version => 20121206160100) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -33,11 +33,14 @@ ActiveRecord::Schema.define(:version => 20121203142624) do
   add_index "searches", ["user_id"], :name => "index_searches_on_user_id"
 
   create_table "sessions", :force => true do |t|
-     t.string   "session_id", :null => false
-     t.text     "data"
-     t.datetime "created_at", :null => false
-     t.datetime "updated_at", :null => false
-   end
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
