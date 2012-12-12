ecd08c4f1639ab227c47596b7a0ab889ed7d387f
John A. Cline <jac244@lib-dev-008.serverfarm.cornell.edu>
2012-12-12 10:34:49 -0500

    discoveryaccess-23

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206160100) do
+ActiveRecord::Schema.define(:version => 20121207182848) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -21,6 +21,24 @@ ActiveRecord::Schema.define(:version => 20121206160100) do
     t.datetime "updated_at",  :null => false
     t.string   "user_type"
   end
+
+  create_table "models", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "models", ["email"], :name => "index_models_on_email", :unique => true
+  add_index "models", ["reset_password_token"], :name => "index_models_on_reset_password_token", :unique => true
 
   create_table "searches", :force => true do |t|
     t.text     "query_params"
