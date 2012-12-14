46268db7a7f3af4ae1a0af00df61c33a5f0c2d6a
John A. Cline <jac244@lib-dev-008.serverfarm.cornell.edu>
2012-12-14 13:19:21 -0500

    fiddle with range_limit

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -189,10 +189,10 @@ class CatalogController < ApplicationController
     # facet bar
     config.add_facet_field 'format', :label => 'Format', :limit => 3
     config.add_facet_field 'pub_date', :label => 'Publication Year', :range => {
-  :num_segments => 6,
-  :assumed_boundaries => [1300, Time.now.year + 5],
-  :segments => true
-}, :show => true
+      :num_segments => 6,
+      :assumed_boundaries => [1300, Time.now.year + 1],
+      :segments => true
+    }, :show => true
 #    config.add_facet_field 'pub_date_sort', :label => 'Publication Year', :range => {
 #  :num_segments => 6,
 #  :assumed_boundaries => [1300, Time.now.year + 2],
