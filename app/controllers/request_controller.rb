dc37e03fcf1201adc55dbac56157753342b1f1da
Nick Cappadona <nick.cappadona@cornell.edu>
2013-01-25 17:01:52 -0500

    Whoops...forgot to add @isbn to hold requests
    Now the borrow direct alert will render on the hold request form (if
    the item has an ISBN) as was attended with adaab726d.

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -26,6 +26,7 @@ class RequestController < ApplicationController
     @ti =  @document[:title_display]
     @au =  @document[:author_display]
     @id =  params[:id]
+    @isbn = @document[:isbn_display].present? ? @document[:isbn_display].first : false
     logger.debug   "HOLD details: #{@hd.inspect}"
     # the details offers an array of records, one element for each holding.
     if (!@hd.nil?)
