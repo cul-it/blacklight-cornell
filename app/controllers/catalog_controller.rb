ade64be4061d06cd3f212e14a1f6d9dd7a330a6e
Melissa Wallace <mhk33@cornell.edu>
2013-02-04 10:06:52 -0500

    change Content label to Fiction/Non\-fiction

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -199,7 +199,7 @@ class CatalogController < ApplicationController
     config.add_facet_field 'subject_topic_facet', :label => 'Subject/Genre', :limit => 5
     config.add_facet_field 'subject_geo_facet', :label => 'Subject: Region', :limit => 5
     config.add_facet_field 'subject_era_facet', :label => 'Subject: Era', :limit => 5
-    config.add_facet_field 'subject_content_facet', :label => 'Content', :limit => 5
+    config.add_facet_field 'subject_content_facet', :label => 'Fiction/Non-fiction', :limit => 5
     config.add_facet_field 'lc_1letter_facet', :label => 'Call number', :limit => 5
     config.add_facet_field 'location_facet', :label => 'Library location', :limit => 5
     config.add_facet_field 'hierarchy_facet', :hierarchy => true
