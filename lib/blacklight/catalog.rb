ff37abe0a1f559a21fb895210c036173a839ea99
John A. Cline <jac244@lib-dev-008.serverfarm.cornell.edu>
2013-02-15 12:13:15 -0500

    fix journal title bug put there by git

--- lib/blacklight/catalog.rb
+++ lib/blacklight/catalog.rb
@@ -61,19 +61,21 @@ module Blacklight::Catalog
           params[:f] = {}
         end
           params[:f] = {"format" => ["Journal"]}
+#          unless(!params[:q])
           params[:q] = params[:q]
           params[:search_field] = "journal title"
       end
       
       (@response, @document_list) = get_search_results
-      logger.debug("dagwood10 = #{@document_list}")
       if params.nil? || params[:f].nil?
         @filters = []
       else
         @filters = params[:f] || []
       end
-      
-#      params[:search_field] = ""
+
+      if params[:search_field] == "journal title"      
+        params[:search_field] = ""
+      end
              
       respond_to do |format|
         format.html { save_current_search_params }
