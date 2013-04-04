37b0014e9cf7401e9f1e46c0a97476474b54c216
Matthew Connolly <mjc12@cornell.edu>
2013-04-04 11:24:51 -0400

    Configure rspec to use vcr and webmock

---
+++ spec/support/vcr_setup.rb
@@ -0,0 +1,6 @@
+VCR.configure do |c|
+  #the directory where your cassettes will be saved
+  c.cassette_library_dir = 'spec/vcr'
+  # your HTTP request service. You can also use fakeweb, typhoeus, and more
+  c.hook_into :webmock
+end
