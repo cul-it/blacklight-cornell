d7b63f6fa87db5ffeb52dcfca332292ee635b408
Nick Cappadona <nick.cappadona@cornell.edu>
2013-05-02 13:07:21 -0400

    DNA #573 Dynamic bottom edge for sticky pagination
    Set to height of <footer> for better responsive support.

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -1,15 +1,16 @@
 function getDocHeight() {
-    var D = document;
-    return Math.max(
-        Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
-        Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
-        Math.max(D.body.clientHeight, D.documentElement.clientHeight)
-    );
+  var D = document;
+  return Math.max(
+    Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+    Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+    Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+  );
 }
 
 $(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() == getDocHeight()) {
-       $('.navbar-fixed-bottom').css({bottom:'100px'});
-   }
-   else $('.navbar-fixed-bottom').css({bottom:'0px'})
+  if($(window).scrollTop() + $(window).height() == getDocHeight()) {
+    var footerHeight = $('footer').height();
+    $('.navbar-fixed-bottom').css({bottom: footerHeight});
+  }
+  else $('.navbar-fixed-bottom').css({bottom:'0'})
 });
