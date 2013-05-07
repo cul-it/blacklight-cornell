7c010923caf2841b224f2c95e0297c5ddb604b6a
Rick Silterra <es287@cornell.edu>
2013-05-07 16:11:25 -0400

    start of multi-bib changes

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ BlacklightCornell::Application.routes.draw do
   match 'backend/holdings/:id' => 'backend#holdings', :as => 'backend_holdings'
   match 'backend/holdings_short/:id' => 'backend#holdings_short', :as => 'backend_holdings_short'
   match 'backend/holdings_shorth/:id' => 'backend#holdings_shorth', :as => 'backend_holdings_shorth'
+  match 'backend/holdings_shorthm/:id' => 'backend#holdings_shorthm', :as => 'backend_holdings_shorthm', :constraints => { :id => /.+/}
   match 'backend/holdings_mail/:id' => 'backend#holdings_mail', :as => 'backend_holdings_mail'
   match 'backend/clio_recall/:id', :to => "backend#clio_recall" , :as => :clio_recall
   match 'backend/feedback_mail', :to => "backend#feedback_mail"
