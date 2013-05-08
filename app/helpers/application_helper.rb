78cf34683d2f7b23cbba598fb4acbfbe0149160b
jac244 <jac244@cornell.edu>
2013-05-08 09:54:45 -0400

    Add link_to_previous_bookmark and link_to_next_bookmark methods. They do not exist in blacklight gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,17 @@ module ApplicationHelper
     @alternating_line[id] = @alternating_line[id] == "even" ? "odd" : "even"
   end
 
+  def link_to_previous_bookmark(previous_bookmark, counter)
+    link_to_unless previous_bookmark.nil?, raw(t('views.pagination.previous')), previous_bookmark, :class => "previous", :rel => 'prev', :'data-counter' => counter - 1 do
+      content_tag :span, raw(t('views.pagination.previous')), :class => 'previous'
+    end
+  end
+
+  def link_to_next_bookmark(next_bookmark, counter)
+    link_to_unless next_bookmark.nil?, raw(t('views.pagination.next')), next_bookmark, :class => "next", :rel => 'next', :'data-counter' => counter + 1 do
+      content_tag :span, raw(t('views.pagination.next')), :class => 'next'
+    end
+  end
+
 
 end
