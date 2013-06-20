409b6faa6d5cb6d1e4e509677e5ca260fb24f6e2
Rick Silterra <es287@cornell.edu>
2013-06-20 14:49:32 -0400

    update username, and destination of deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 #server "#{user}-dev.library.cornell.edu", :app, :web, :db, :primary => true
-server "da-stg-web.library.cornell.edu", :app, :web, :db, :primary => true
+server "da-prod-web1.library.cornell.edu", :app, :web, :db, :primary => true
 set :deploy_to, "/users/#{user}/blacklight-cornell-staging"
 set :branch, "cap-deploy"
