3a86de95d71bc170b8e6fad119e1c6f70db3798b
Rick Silterra <es287@cornell.edu>
2013-07-02 15:05:37 -0400

    trying to get both migrate, and allmigrate to work

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,8 @@
 #server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
 role :app, "da-prod-web2.library.cornell.edu","da-prod-web1.library.cornell.edu"
 role :web,"da-prod-web2.library.cornell.edu","da-prod-web1.library.cornell.edu" 
-role :db, "da-prod-web1.library.cornell.edu"
-role :db, "da-prod-web2.library.cornell.edu"
+role :db, "da-prod-web1.library.cornell.edu", :primary => true
+role :db, "da-prod-web2.library.cornell.edu", :primary => true
 
 set :deploy_to, "/users/#{user}/blacklight-cornell-staging"
 set :branch, "cap-deploy"
