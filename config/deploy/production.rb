201ba8bb8418c6d436e8472c057658001784d473
Rick Silterra <es287@cornell.edu>
2013-07-02 10:14:55 -0400

    add second prod machine to production config.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "#{user}-dev.library.cornell.edu", :app, :web, :primary => true
+server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
 set :deploy_to, "/users/#{user}/blacklight-cornell-production"
 set :branch, "dev"
