6f0ddff89443eee02fc6c909ccf195fc2d192d2f
Rick Silterra <es287@cornell.edu>
2013-07-02 10:21:13 -0400

    try diff syntax for staging, and prod config.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,11 @@
-server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
+#server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
+role :app, "da-prod-web1.library.cornell.edu"
+role :web, "da-prod-web1.library.cornell.edu"
+role :db, "da-prod-web1.library.cornell.edu", :primary => true
+
+role :app, "da-prod-web2.library.cornell.edu"
+role :web, "da-prod-web2.library.cornell.edu"
+role :db, "da-prod-web2.library.cornell.edu"
+
 set :deploy_to, "/users/#{user}/blacklight-cornell-production"
 set :branch, "dev"
