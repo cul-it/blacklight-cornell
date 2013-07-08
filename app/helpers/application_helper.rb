0c7dea76a2abe6a60c00af4952fb768b8d57b1b7
jac244 <jac244@cornell.edu>
2013-07-08 14:40:33 -0400

    moved link_to_document here from blacklight_helper_behavior.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,5 +19,16 @@ module ApplicationHelper
     end
   end
 
+  def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true})
+    opts[:label] ||= blacklight_config.index.show_link.to_sym
+    label = render_document_index_label doc, opts
+    if params[:controller] == 'bookmarks'
+      docID = doc.id
+      link_to label, '/catalog/' + docID #, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :results_view].include? k  })
+    else
+      link_to label, doc, { :'data-counter' => opts[:counter] }.merge(opts.reject { |k,v| [:label, :counter, :results_view].include? k  })
+    end
+  end
+
 
 end
