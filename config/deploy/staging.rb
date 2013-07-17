583bc4a30962b4f8effdd760a2c0d72a80fd7250
Rick Silterra <es287@cornell.edu>
2013-07-17 15:48:08 -0400

    Correct for latest staging names

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,10 @@
 #server "#{user}-dev.library.cornell.edu", :app, :web, :db, :primary => true
 #server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
-role :app, "da-stg-web.library.cornell.edu"
-role :web, "da-stg-web.library.cornell.edu" 
-role :db,  "da-stg-web.library.cornell.edu", :primary => true
+role :app, "search-stg.library.cornell.edu"
+role :web, "search-stg.library.cornell.edu" 
+role :db,  "search-stg.library.cornell.edu", :primary => true
 
 set :deploy_to, "/libweb/#{user}/blacklight-cornell"
 #set :deploy_to, "/users/#{user}/blacklight-cornell"
+# actually this is a tag
 set :branch, "staging-publicbeta-0.2"
