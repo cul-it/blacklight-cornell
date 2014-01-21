e4ba9c30db9ae9290285f91696055625ee8eaf4f
Rick Silterra <es287@cornell.edu>
2014-01-21 08:44:33 -0500

    update destination machines

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,15 @@
 #server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
-role :app, "search1.library.cornell.edu"
-role :web, "search1.library.cornell.edu"
-role :db, "search1.library.cornell.edu", :primary => true
+role :app, "newcatalog1.library.cornell.edu"
+role :web, "newcatalog1.library.cornell.edu"
+role :db, "newcatalog1.library.cornell.edu", :primary => true
 
-role :app, "search2.library.cornell.edu"
-role :web, "search2.library.cornell.edu"
-role :db, "search2.library.cornell.edu", :primary => true
+role :app, "newcatalog2.library.cornell.edu"
+role :web, "newcatalog2.library.cornell.edu"
+role :db, "newcatalog2.library.cornell.edu", :primary => true
+
+role :app, "newcatalog3.library.cornell.edu"
+role :web, "newcatalog3.library.cornell.edu"
+role :db, "newcatalog3.library.cornell.edu", :primary => true
 
 set :deploy_to, "/libweb/#{user}/blacklight-cornell"
 #this avoids an error message from git, but i don't think it's really necessary.
