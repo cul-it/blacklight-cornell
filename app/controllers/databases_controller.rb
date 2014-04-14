5d8bb23812166bcab2b8bd3cb813b4bad9a9866c
jac244 <jac244@cornell.edu>
2014-04-14 13:33:18 -0400

    change query to retrieve title and summary

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -4,7 +4,7 @@ class DatabasesController < ApplicationController
   def subject
      clnt = HTTPClient.new
 #     @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%22anthropology+%28core%29%22&wt=ruby&indent=true") # do |chunk|
-      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%7B!lucene%7Ddatabase_b%3Atrue+&rows=30&fl=id%2Csixfivethree_display&wt=ruby&indent=true")
+      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q={!lucene}database_b%3Atrue+&rows=30&fl=id%2Csixfivethree_display%2Ctitle_display%2C+summary_display&wt=ruby&indent=true")
        @anthropologyResponse = eval(@anthroString)
        @anthropology = @anthropologyResponse['response']['docs']
       @psychologyString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%22psychology+%28core%29%22&wt=ruby&indent=true")
