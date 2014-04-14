93f8713d4362cb7521d4f741ddd7742813f8e1ce
jac244 <jac244@cornell.edu>
2014-04-14 12:35:42 -0400

    change query for anthro

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -3,7 +3,8 @@ class DatabasesController < ApplicationController
   
   def subject
      clnt = HTTPClient.new
-     @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%22anthropology+%28core%29%22&wt=ruby&indent=true") # do |chunk|
+#     @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%22anthropology+%28core%29%22&wt=ruby&indent=true") # do |chunk|
+      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%7B!lucene%7Ddatabase_b%3Atrue+&rows=30&fl=id%2Csixfivethree_display&wt=ruby&indent=true")
        @anthropologyResponse = eval(@anthroString)
        @anthropology = @anthropologyResponse['response']['docs']
       @psychologyString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/select?q=%22psychology+%28core%29%22&wt=ruby&indent=true")
@@ -19,3 +20,4 @@ class DatabasesController < ApplicationController
     end
   
 end
+
