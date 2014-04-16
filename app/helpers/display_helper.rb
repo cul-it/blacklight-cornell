4a25cbc09284a4f6d5296a59a11ef61c40d2f3c1
jac244 <jac244@cornell.edu>
2014-04-16 14:47:12 -0400

    clean up code on line 756

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -762,9 +762,11 @@ module DisplayHelper
     document ||= @document
     # Test to ensure that display_title is not missing
     # -- some records in Voyager are missing the title (#DISCOVERYACCESS-552)
-    blacklight_config.show.html_title = "freaky"
+    blacklight_config.show.html_title = @document['title_displayed']
     if @document[blacklight_config.show.html_title].present?
       render_field_value(document[blacklight_config.show.html_title].html_safe)
+    else
+      render_field_value("Default title".html_safe)
     end
   end
 
