f4e606f4f234fba21ad087d114bb0e186c3ee5f1
jac244 <jac244@cornell.edu>
2014-04-17 09:50:52 -0400

    changed to new query

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -3,16 +3,16 @@ class DatabasesController < ApplicationController
   
   def subject
      clnt = HTTPClient.new
-      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=anthropology&wt=ruby&indent=true") # do |chunk|
+      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=anthropology&wt=ruby")
        @anthropologyResponse = eval(@anthroString)
        @anthropology = @anthropologyResponse['response']['docs']
-      @psychologyString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22psychology+%28core%29%22&wt=ruby&indent=true")
+      @psychologyString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22psychology+%28core%29%22&wt=ruby")
        @psychologyResponse = eval(@psychologyString)
        @psychology = @psychologyResponse['response']['docs']
-      @dictionariesString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22dictionaries+and+encyclopedias+%28core%29%22&wt=ruby&indent=true")
+      @dictionariesString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22dictionaries+and+encyclopedias+%28core%29%22&wt=ruby")
        @dictionariesResponse = eval(@dictionariesString)
        @dictionaries = @dictionariesResponse['response']['docs']
-      @libinfosciString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22library+and+information+science+%28core%29%22&wt=ruby&indent=true")
+      @libinfosciString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22library+and+information+science+%28core%29%22&wt=ruby")
        @libinfosciResponse = eval(@libinfosciString)
        @libinfosci = @libinfosciResponse['response']['docs']
        
