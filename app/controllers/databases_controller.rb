58262b94f6d051baea1b0d0cb4f070f1392bbafb
Jenn Colt <jrc88@cornell.edu>
2014-04-18 10:14:29 -0400

    fix case on query. add vern title switch

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -3,15 +3,15 @@ class DatabasesController < ApplicationController
   
   def subject
      clnt = HTTPClient.new
-      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=anthropology&wt=ruby")
+      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Anthropology+%28Core%29%22&&wt=ruby")
        @anthropologyResponse = eval(@anthroString)
        @anthropology = @anthropologyResponse['response']['docs']
       @psychologyString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22psychology+%28core%29%22&wt=ruby")
        @psychologyResponse = eval(@psychologyString)
        @psychology = @psychologyResponse['response']['docs']
-      @dictionariesString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22dictionaries+and+encyclopedias+%28core%29%22&wt=ruby")
-       @dictionariesResponse = eval(@dictionariesString)
-       @dictionaries = @dictionariesResponse['response']['docs']
+      @statisticalString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Statistical+Information%22&wt=ruby")
+       @statisticalResponse = eval(@statisticalString)
+       @statistical = @statisticalResponse['response']['docs']
       @libinfosciString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22library+and+information+science+%28core%29%22&wt=ruby")
        @libinfosciResponse = eval(@libinfosciString)
        @libinfosci = @libinfosciResponse['response']['docs']
