ca17b6505bd350e0e9c5f7655f1e21e3168037bf
Jenn Colt <jrc88@cornell.edu>
2014-04-18 10:41:15 -0400

    split sample anthro page into core and non-core, add appropriate query

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -3,13 +3,16 @@ class DatabasesController < ApplicationController
   
   def subject
      clnt = HTTPClient.new
-      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Anthropology+%28Core%29%22&&wt=ruby")
+      @anthroString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Anthropology%22&&wt=ruby")
        @anthropologyResponse = eval(@anthroString)
        @anthropology = @anthropologyResponse['response']['docs']
+      @anthroCoreString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Anthropology+%28Core%29%22&&wt=ruby")
+       @anthropologyCoreResponse = eval(@anthroCoreString)
+       @anthropologyCore = @anthropologyCoreResponse['response']['docs']
       @psychologyString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22psychology+%28core%29%22&wt=ruby")
        @psychologyResponse = eval(@psychologyString)
        @psychology = @psychologyResponse['response']['docs']
-      @statisticalString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Statistical+Information%22&wt=ruby")
+      @statisticalString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22Computers+and+Information+Science%22&wt=ruby")
        @statisticalResponse = eval(@statisticalString)
        @statistical = @statisticalResponse['response']['docs']
       @libinfosciString = clnt.get_content("http://da-dev-solr.library.cornell.edu/solr/blacklight/databases?q=%22library+and+information+science+%28core%29%22&wt=ruby")
