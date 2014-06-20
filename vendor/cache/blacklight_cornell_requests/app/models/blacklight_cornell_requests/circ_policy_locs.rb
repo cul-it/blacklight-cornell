e307fca651574eb5748edabedb560084c7d6e188
Rick Silterra <es287@cornell.edu>
2014-06-20 10:49:23 -0400

    update files

--- vendor/cache/blacklight_cornell_requests/app/models/blacklight_cornell_requests/circ_policy_locs.rb
+++
@@ -1,9 +0,0 @@
-module BlacklightCornellRequests
-  class Circ_policy_locs < ActiveRecord::Base
-    attr_accessible :circ_group_id,:pickup_location
-    
-    def readonly?
-      true
-    end
-  end
-end
