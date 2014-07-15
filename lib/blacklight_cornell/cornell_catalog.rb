25af31305b6c8c4b96a862d6985b1e15d825d70f
jac244 <jac244@cornell.edu>
2014-07-15 08:58:25 -0400

    check for # in query string update advance_search

--- lib/blacklight_cornell/cornell_catalog.rb
+++ lib/blacklight_cornell/cornell_catalog.rb
@@ -55,7 +55,7 @@ module BlacklightCornell::CornellCatalog extend Blacklight::Catalog
     # Journal title search hack.
     if params[:search_field] == "journal title"
       if params[:f].nil?
-        params[:f] = {}
+        params[:f] = {"format" => ["Journal"]}
       end
         params[:f].merge("format" => ["Journal"])
         # unless(!params[:q])
