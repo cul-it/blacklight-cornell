7da1f3de4975a3f918a64ffa7c145c04c984a7bd
Rick Silterra <es287@cornell.edu>
2014-08-11 16:08:01 -0400

    supplemental info should be displayed

--- app/helpers/cornell_catalog_helper.rb
+++ app/helpers/cornell_catalog_helper.rb
@@ -164,7 +164,7 @@ module CornellCatalogHelper
             oneloc["notes"] = "Notes: " + notes.join(' ') unless notes.blank? 
             notes_by_mid[id.to_s] = oneloc["notes"]
             oneloc["summary_holdings"]="Library has: " + summary_holdings.join(' ') unless summary_holdings.blank? 
-            oneloc["supplemental_holdings_desc"]=suppl_holdings.join(';') unless suppl_holdings.blank? 
+            oneloc["supplements"]=suppl_holdings.join(';') unless suppl_holdings.blank? 
             if (!@current_suppl.nil?) 
               if !(@current_suppl.select{|x| x["MFHD_ID"] ==  mfhd_id  && x["PREDICT"] == 'Y'}).blank?
                 Rails.logger.debug "\nes287_debug **** #{__FILE__}:#{__LINE__} selected current  = " +  (@current_suppl.select{|x| x["MFHD_ID"] ==  mfhd_id }).inspect
