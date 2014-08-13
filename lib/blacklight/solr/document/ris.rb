aead8374e3404de31863be799669a7a71642149c
Matthew Connolly <mjc12@cornell.edu>
2014-08-13 15:43:15 -0400

    Update RIS mime types.

--- lib/blacklight/solr/document/ris.rb
+++ lib/blacklight/solr/document/ris.rb
@@ -1,15 +1,15 @@
+# This module registers the RIS export format with the system so that we
+# can offer export options for Mendeley and Zotero.
 module Blacklight::Solr::Document::RIS
   
   def self.extended(document)
     # Register our exportable formats
-    Rails.logger.warn "mjc12test: call1"
     Blacklight::Solr::Document::RIS.register_export_formats( document )
   end
 
   def self.register_export_formats(document)
-    Rails.logger.warn "mjc12test: call2"
-    document.will_export_as(:mendeley, "text/plain")
-    document.will_export_as(:zotero, "text/plain")
+    document.will_export_as(:mendeley, "application/x-research-info-systems")
+    document.will_export_as(:zotero, "application/x-research-info-systems")
   end
 
 end
