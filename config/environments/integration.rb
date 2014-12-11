0ba94a814e96e1f9091f079704b9c61e0ecbf6d7
Rick Silterra <es287@cornell.edu>
2014-12-11 16:28:18 -0500

    DISCOVERYACCESS-1510 -- add integration deploy target
    Former-commit-id: 881e58fb7e15b9b11b4a5cc68b73fa65adedd357

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -1,6 +1,7 @@
 BlacklightCornell::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  config.eager_load = false
+  # config.eager_load = true
   # Code is not reloaded between requests
   config.cache_classes = true
 
