8157dc1ed82b81e4006b0a4f2cfdf5a1d2b4fc21
Rick Silterra <es287@cornell.edu>
2014-12-12 10:06:00 -0500

    DISCOVERYACCESS-1510 -- update for integration mode, and integration target

--- config/environments/integration.rb
+++ config/environments/integration.rb
@@ -11,6 +11,7 @@ BlacklightCornell::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = true
+  # config.serve_static_assets = false 
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -19,13 +20,14 @@ BlacklightCornell::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  # config.assets.digest = false
+  config.assets.digest = true 
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
