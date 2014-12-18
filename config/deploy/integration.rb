73d9aa605cc90597781afa2fbe4d4e3a8826e444
Rick Silterra <es287@cornell.edu>
2014-12-18 10:04:17 -0500

    DISCOVERYACESS-1510 -- uplevel requests gem
    Former-commit-id: 84258701ad12641d2b4bfd95fef882b91b75fceb

--- config/deploy/integration.rb
+++ config/deploy/integration.rb
@@ -8,10 +8,11 @@ set :deploy_to, "/cul/web/newcatalog-int.library.cornell.edu/rails-app"
 # actually this is a tag
 #set :branch, "staging-publicbeta-0.2"
 # this is set by jenkins, otherwise you can set it.
-#set :branch, ENV['GIT_BRANCH']
 set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
-#set :branch,"dev"
 set :bundle_flags,    "--local "
+
+set :rails_env, 'integration'
+
 desc "Install  (redefine for integration) env -- too sensitive for git - production"
 task :install_env, :roles => [ :app, :db, :web ] do
          run "cp #{deploy_to}/../conf/integration.env  #{shared_path}/.env"
