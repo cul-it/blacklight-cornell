7b5c961d54b22a51053a150828b6f260476727ae
Rick Silterra <es287@cornell.edu>
2014-12-19 11:28:55 -0500

    changes to allow 2.1.5 code to run under 1.9.3 again
    Former-commit-id: b04d2a340e78d18c1c9b2400e927c0262ed5b019

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :user, "rails"
 #  'GEM_PATH'     => "/usr/local/rvm/gems/ruby-2.1.5:/usr/local/rvm/gems/ruby-2.1.5@global",
 #  'BUNDLE_PATH'  => "/usr/local/rvm/gems/ruby-2.1.5@global/gems/bundler-1.3.5/",  # If you are using bundler.
 #  'BUNDLE_PATH'  => "/usr/local/rvm/bin/bundle"  # If you are using bundler.
-}
+#}
 #Deploy to may vary depending on target stage
 #set :deploy_to, "/libweb/#{user}"
 set :deploy_via, :copy
