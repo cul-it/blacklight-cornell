eeadc7e453f4ce163441c40675a829117a314f15
Rick Silterra <es287@cornell.edu>
2015-02-18 20:06:41 -0500

    DISCOVERYACCESS-1594 -- link on 404 page needs to be mailto
    Former-commit-id: 883aebe8aaea1dd3303c94564e374948ac230c89

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -14,6 +14,10 @@ When /^(?:|I )go to (.+)$/ do |page_name|
   visit path_to(page_name)
 end
 
+When /^(?:|I )go to literal (.+)$/ do |page_name|
+  visit page_name
+end
+
 Then /^(?:|I )should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
   if current_path.respond_to? :should
@@ -25,4 +29,12 @@ end
 
 Then /^show me the page$/ do
   print page.html
+end
+
+Then /^I should not see an error$/ do
+  (200 .. 399).should include(page.status_code)
+end
+
+Then /^I should see an error$/ do
+  (400 .. 599).should include(page.status_code)
 end
