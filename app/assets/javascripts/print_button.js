e58b5eb5675f7966ebfd6af65017774f45e46030
Melissa Wallace <mhk33@cornell.edu>
2015-04-02 13:42:14 -0400

    library menu dropdown, footer, pagination, and start the item view
    Former-commit-id: 3c43db1a5e9f658bb11701f5e0a7613186c9ebb3

--- app/assets/javascripts/print_button.js
+++ app/assets/javascripts/print_button.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
- $('ul#item-tools').append('<li class="print"><a href="#print" class="btn btn-mini"><i class="fa fa-print"></i> Print</a></li>');
+ $('ul#item-tools').append('<li class="print"><a href="#print" class="btn btn-default btn-xs"><i class="fa fa-print"></i> Print</a></li>');
  $('ul#item-tools li.print a').click(function() {
   window.print();
   return false;
