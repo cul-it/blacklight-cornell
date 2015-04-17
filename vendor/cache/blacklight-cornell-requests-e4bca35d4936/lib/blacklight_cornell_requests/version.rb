3faabfe46070d520d71abf57ab288e458180a06d
Rick Silterra <es287@cornell.edu>
2015-04-17 11:52:07 -0400

    update requests gem with new version, and no zoom

--- vendor/cache/blacklight-cornell-requests-e4bca35d4936/lib/blacklight_cornell_requests/version.rb
+++ vendor/cache/blacklight-cornell-requests-e4bca35d4936/lib/blacklight_cornell_requests/version.rb
@@ -2,5 +2,5 @@ module BlacklightCornellRequests
   # Seems that we're using month/year for the version number for now.
   # Guess once we hit the end of the year, we might want to switch
   # to something a little more traditional?
-  VERSION = "1.0.1"
+  VERSION = "1.1.0"
 end
