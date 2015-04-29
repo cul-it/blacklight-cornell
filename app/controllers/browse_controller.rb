9d9710dad66e692ca2ad7928ac687e095e9b55db
starsplatter <jenncd@gmail.com>
2015-04-29 13:58:49 -0400

    add author-title search

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -64,6 +64,35 @@ class BrowseController < ApplicationController
         #Rails.logger.info("Petunia3 = #{params[:q]}")
       end
 
+      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Author-Title"
+        #Rails.logger.info("Petunia1 = #{params[:q]}")
+        #params[:q].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:q]}")
+        dbclnt = HTTPClient.new
+        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
+        #solr = Blacklight.solr_config[:url]
+        p =  {"q" => '["' + params[:q].gsub("-"," ").gsub(/[^a-z0-9\s]/i, '') +'" TO *]' }
+        start = {"start" => params[:start]}
+        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        if params[:order] == "reverse"
+          p =  {"q" => '[* TO "' + params[:q] +'"}' }
+
+          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/reverse?" + p.to_param + '&' + start.to_param  )
+          @headingsResultString = @headingsResultString
+        else
+          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/browse?" + p.to_param + '&' + start.to_param  )
+        end
+        if !@headingsResultString.nil?
+           @headingsResponseFull = eval(@headingsResultString)
+        else
+           @headingsResponseFull = eval("Could not find")
+        end
+        @headingsResponse = @headingsResponseFull['response']['docs']
+        params[:q].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:q]}")
+      end
+
 
     end
     def info
