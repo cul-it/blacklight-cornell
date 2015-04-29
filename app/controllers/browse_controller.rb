64969598dc102ffbc30361fd5c76412f0348145f
starsplatter <jenncd@gmail.com>
2015-04-29 15:17:31 -0400

    add author-title to controller. correct info page display
    Former-commit-id: b6f46774b4ea9071392834a19c83c187c15b85bd

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -137,7 +137,26 @@ class BrowseController < ApplicationController
         params[:q].gsub!('%20', ' ')
         #Rails.logger.info("Petunia3 = #{params[:q]}")
       end
-
+      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Author-Title"
+        #Rails.logger.info("Petunia1 = #{params[:q]}")
+        #params[:q].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:q]}")
+        dbclnt = HTTPClient.new
+        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
+        #solr = Blacklight.solr_config[:url]
+        p =  {"q" => '"' + params[:q].gsub("-"," ").gsub(/[^a-z0-9\s]/i, '') +'"' } 
+        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/browse?" + p.to_param )
+        if !@headingsResultString.nil?
+           @headingsResponseFull = eval(@headingsResultString)
+        else
+           @headingsResponseFull = eval("Could not find")
+        end
+        @headingsResponse = @headingsResponseFull['response']['docs']
+        params[:q].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:q]}")
+      end
     end
 
 end
