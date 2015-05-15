108e448d2f711c79405454600eccc89b8c848f85
starsplatter <jenncd@gmail.com>
2015-05-15 09:24:59 -0400

    change id of authorities search so it can exist next to catalog search
    Former-commit-id: 3960c08c87e47858df135895d80e1531323609d0

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1050,7 +1050,9 @@ include ActionView::Helpers::NumberHelper
 
   #switch to determine if a view is part of the main catalog and should get the header
   def part_of_catalog?
-    if params[:controller] =='catalog' || params[:controller]=='bookmarks' || request.original_url.include?("request") || params[:controller]=='search_history' || params[:controller] == 'advanced' || params[:controller]=='aeon'
+    if params[:controller] =='catalog' || params[:controller]=='bookmarks' || 
+      request.original_url.include?("request") || params[:controller]=='search_history' || 
+      params[:controller] == 'advanced' || params[:controller]=='aeon' || params[:controller]=='browse'
       return true
     end
   end
