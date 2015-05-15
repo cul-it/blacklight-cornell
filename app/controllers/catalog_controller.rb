139b45d95f7aa7cffe6e85ad9e35750b4750e401
Rick Silterra <es287@cornell.edu>
2015-05-15 11:09:04 -0400

    eliminate warning about deprecated method
    Former-commit-id: 2e786e7b34d09495ef7a7b1c85012442b6435925

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -588,7 +588,7 @@ class CatalogController < ApplicationController
       elsif !flash[:error]
         # Don't have to show a CAPTCHA and there are no errors, so we can send the email
         email ||= RecordMailer.email_record(@documents, {:to => params[:to], :message => params[:message], :location => params[:location], :callnumber => params[:callnumber], :templocation => params[:templocation], :status => params[:itemStatus]}, url_gen_params, params)
-        email.deliver
+        email.deliver_now
         flash[:success] = "Email sent"
         redirect_to catalog_path(params[:id]) unless request.xhr?
       end
