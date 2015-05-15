3960c08c87e47858df135895d80e1531323609d0
starsplatter <jenncd@gmail.com>
2015-05-15 09:24:59 -0400

    change id of authorities search so it can exist next to catalog search

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -3,23 +3,27 @@ class BrowseController < ApplicationController
   include Blacklight::Catalog
   include BlacklightCornell::CornellCatalog
   #include BlacklightUnapi::ControllerExtension
-
+  before_filter :heading
+  
+  def heading
+   @heading='Browse'
+  end
     def index
 
-      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Author"
-        #Rails.logger.info("Petunia1 = #{params[:q]}")
-        #params[:q].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:q]}")
+      if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Author"
+        #Rails.logger.info("Petunia1 = #{params[:authq]}")
+        #params[:authq].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
         #solr = Blacklight.solr_config[:url]
-        p =  {"q" => '["' + params[:q].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
+        p =  {"q" => '["' + params[:authq].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
 .gsub(/\s{2,}/,' ').gsub(/\s*$/,'') +'" TO *]' }
         start = {"start" => params[:start]}
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         if params[:order] == "reverse"
-          p =  {"q" => '[* TO "' + params[:q] +'"}' }
+          p =  {"q" => '[* TO "' + params[:authq] +'"}' }
 
           @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/author/reverse?" + p.to_param + '&' + start.to_param  )
           @headingsResultString = @headingsResultString
@@ -32,24 +36,24 @@ class BrowseController < ApplicationController
            @headingsResponseFull = eval("Could not find")
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
-        params[:q].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:q]}")
+        params[:authq].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
 
-      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Subject"
-        #Rails.logger.info("Petunia1 = #{params[:q]}")
-        #params[:q].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:q]}")
+      if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Subject"
+        #Rails.logger.info("Petunia1 = #{params[:authq]}")
+        #params[:authq].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
         #solr = Blacklight.solr_config[:url]
-        p =  {"q" => '["' + params[:q].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
+        p =  {"q" => '["' + params[:authq].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
 .gsub(/\s{2,}/,' ').gsub(/\s*$/,'') +'" TO *]' }
         start = {"start" => params[:start]}
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         if params[:order] == "reverse"
-          p =  {"q" => '[* TO "' + params[:q] +'"}' }
+          p =  {"q" => '[* TO "' + params[:authq] +'"}' }
 
           @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/subject/reverse?" + p.to_param + '&' + start.to_param  )
           @headingsResultString = @headingsResultString
@@ -62,24 +66,24 @@ class BrowseController < ApplicationController
            @headingsResponseFull = eval("Could not find")
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
-        params[:q].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:q]}")
+        params[:authq].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
 
-      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Author-Title"
-        #Rails.logger.info("Petunia1 = #{params[:q]}")
-        #params[:q].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:q]}")
+      if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Author-Title"
+        #Rails.logger.info("Petunia1 = #{params[:authq]}")
+        #params[:authq].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
         #solr = Blacklight.solr_config[:url]
-        p =  {"q" => '["' + params[:q].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
+        p =  {"q" => '["' + params[:authq].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
 .gsub(/\s{2,}/,' ').gsub(/\s*$/,'') +'" TO *]' }
         start = {"start" => params[:start]}
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         if params[:order] == "reverse"
-          p =  {"q" => '[* TO "' + params[:q] +'"}' }
+          p =  {"q" => '[* TO "' + params[:authq] +'"}' }
 
           @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/reverse?" + p.to_param + '&' + start.to_param  )
           @headingsResultString = @headingsResultString
@@ -92,24 +96,24 @@ class BrowseController < ApplicationController
            @headingsResponseFull = eval("Could not find")
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
-        params[:q].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:q]}")
+        params[:authq].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
 
 
     end
     def info
-      if !params[:q].nil? and params[:q] != ""
-        #Rails.logger.info("Petunia1 = #{params[:q]}")
-        #params[:q].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:q]}")
+      if !params[:authq].nil? and params[:authq] != ""
+        #Rails.logger.info("Petunia1 = #{params[:authq]}")
+        #params[:authq].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
         #solr = Blacklight.solr_config[:url]
-        p =  {"q" => '"' + params[:q].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
+        p =  {"q" => '"' + params[:authq].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
 .gsub(/\s{2,}/,' ').gsub(/\s*$/,'') +'"' } 
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/author/browse?" + p.to_param )
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
@@ -117,21 +121,21 @@ class BrowseController < ApplicationController
            @headingsResponseFull = eval("Could not find")
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
-        params[:q].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:q]}")
+        params[:authq].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
 
-      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Subject"
-        #Rails.logger.info("Petunia1 = #{params[:q]}")
-        #params[:q].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:q]}")
+      if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Subject"
+        #Rails.logger.info("Petunia1 = #{params[:authq]}")
+        #params[:authq].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
         #solr = Blacklight.solr_config[:url]
-        p =  {"q" => '"' + params[:q].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
+        p =  {"q" => '"' + params[:authq].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
 .gsub(/\s{2,}/,' ').gsub(/\s*$/,'') +'"' } 
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/subject/browse?" + p.to_param )
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
@@ -139,20 +143,20 @@ class BrowseController < ApplicationController
            @headingsResponseFull = eval("Could not find")
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
-        params[:q].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:q]}")
+        params[:authq].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
-      if !params[:q].nil? and params[:q] != "" and params[:browse_type] == "Author-Title"
-        #Rails.logger.info("Petunia1 = #{params[:q]}")
-        #params[:q].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:q]}")
+      if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Author-Title"
+        #Rails.logger.info("Petunia1 = #{params[:authq]}")
+        #params[:authq].gsub!(' ','%20')
+        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
         #solr = Blacklight.solr_config[:url]
-        p =  {"q" => '"' + params[:q].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
+        p =  {"q" => '"' + params[:authq].gsub(/["!@#$%^&*()=_+|;:",.?"]/, ' ')
 .gsub(/\s{2,}/,' ').gsub(/\s*$/,'') +'"' } 
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:q] + "&wt=ruby&indent=true&defType=dismax")
+        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/browse?" + p.to_param )
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
@@ -160,8 +164,8 @@ class BrowseController < ApplicationController
            @headingsResponseFull = eval("Could not find")
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
-        params[:q].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:q]}")
+        params[:authq].gsub!('%20', ' ')
+        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
     end
 
