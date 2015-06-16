18cc820b0e9280909408d47bc9b5f4f85cde6994
Rick Silterra <es287@cornell.edu>
2015-06-16 12:11:11 -0400

    have browse controller follow config of solr.yml, i hope

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -9,25 +9,19 @@ class BrowseController < ApplicationController
    @heading='Browse'
   end
     def index
+        base_solr = Blacklight.solr_config[:url].gsub(/\/solr\/.*/,'/solr')
+        Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{base_solr}")
 
       if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Author"
-        #Rails.logger.info("Petunia1 = #{params[:authq]}")
-        #params[:authq].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
-        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
-        #solr = Blacklight.solr_config[:url]
         p =  {"q" => '["' + params[:authq] +'" TO *]' }
         start = {"start" => params[:start]}
-        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         if params[:order] == "reverse"
           p =  {"q" => '[* TO "' + params[:authq] +'"}' }
-
-          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/author/reverse?" + p.to_param + '&' + start.to_param  )
+          @headingsResultString = dbclnt.get_content(base_solr + "/author/reverse?" + p.to_param + '&' + start.to_param  )
           @headingsResultString = @headingsResultString
         else
-          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/author/browse?" + p.to_param + '&' + start.to_param )
+          @headingsResultString = dbclnt.get_content(base_solr + "/author/browse?" + p.to_param + '&' + start.to_param )
         end
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
@@ -40,23 +34,16 @@ class BrowseController < ApplicationController
       end
 
       if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Subject"
-        #Rails.logger.info("Petunia1 = #{params[:authq]}")
-        #params[:authq].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
-        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
-        #solr = Blacklight.solr_config[:url]
         p =  {"q" => '["' + params[:authq] +'" TO *]' }
         start = {"start" => params[:start]}
-        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
         if params[:order] == "reverse"
           p =  {"q" => '[* TO "' + params[:authq] +'"}' }
 
-          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/subject/reverse?" + p.to_param + '&' + start.to_param  )
+          @headingsResultString = dbclnt.get_content(base_solr +"/subject/reverse?" + p.to_param + '&' + start.to_param  )
           @headingsResultString = @headingsResultString
         else
-          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/subject/browse?" + p.to_param + '&' + start.to_param  )
+          @headingsResultString = dbclnt.get_content(base_solr + "/subject/browse?" + p.to_param + '&' + start.to_param  )
         end
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
@@ -65,7 +52,6 @@ class BrowseController < ApplicationController
         end
         @headingsResponse = @headingsResponseFull['response']['docs']
         params[:authq].gsub!('%20', ' ')
-        #Rails.logger.info("Petunia3 = #{params[:authq]}")
       end
 
       if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Author-Title"
@@ -82,10 +68,10 @@ class BrowseController < ApplicationController
         if params[:order] == "reverse"
           p =  {"q" => '[* TO "' + params[:authq] +'"}' }
 
-          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/reverse?" + p.to_param + '&' + start.to_param  )
+          @headingsResultString = dbclnt.get_content(base_solr +"/authortitle/reverse?" + p.to_param + '&' + start.to_param  )
           @headingsResultString = @headingsResultString
         else
-          @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/browse?" + p.to_param + '&' + start.to_param  )
+          @headingsResultString = dbclnt.get_content(base_solr +"/authortitle/browse?" + p.to_param + '&' + start.to_param  )
         end
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
@@ -100,17 +86,12 @@ class BrowseController < ApplicationController
 
     end
     def info
+        base_solr = Blacklight.solr_config[:url].gsub(/\/solr\/.*/,'/solr')
+        Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{base_solr}")
       if !params[:authq].nil? and params[:authq] != ""
-        #Rails.logger.info("Petunia1 = #{params[:authq]}")
-        #params[:authq].gsub!(' ','%20')
-        #Rails.logger.info("Petunia2 = #{params[:authq]}")
         dbclnt = HTTPClient.new
-        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = #{Blacklight.solr_config.inspect}")
-        #solr = Blacklight.solr_config[:url]
         p =  {"q" => '"' + params[:authq] +'"' } 
-        #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
-        #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
-        @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/author/browse?" + p.to_param )
+        @headingsResultString = dbclnt.get_content(base_solr +"/author/browse?" + p.to_param )
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
         else
@@ -131,7 +112,7 @@ class BrowseController < ApplicationController
         p =  {"q" => '"' + params[:authq] +'"' } 
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
         #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
-        @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/subject/browse?" + p.to_param )
+        @headingsResultString = dbclnt.get_content(base_solr +"/subject/browse?" + p.to_param )
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
         else
@@ -151,7 +132,7 @@ class BrowseController < ApplicationController
         p =  {"q" => '"' + params[:authq] +'"' } 
         #Rails.logger.info("es287_debug #{__FILE__} #{__LINE__}  = " + "#{solr}/databases?"+p.to_param)
         #@dbResultString = dbclnt.get_content("#{solr}/databases?q=" + params[:authq] + "&wt=ruby&indent=true&defType=dismax")
-        @headingsResultString = dbclnt.get_content("http://da-stg-ssolr.library.cornell.edu/solr/authortitle/browse?" + p.to_param )
+        @headingsResultString = dbclnt.get_content(base_solr +"/authortitle/browse?" + p.to_param )
         if !@headingsResultString.nil?
            @headingsResponseFull = eval(@headingsResultString)
         else
