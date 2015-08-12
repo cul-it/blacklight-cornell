36381ce97b32778265a11e58a0bd83c57d0c933a
jac244 <jac244@cornell.edu>
2015-08-12 14:15:29 -0400

    add advanced_search route
    Former-commit-id: 5b35587834580212cdb212df6409a43b988a2151

--- config/routes.rb
+++ config/routes.rb
@@ -27,14 +27,14 @@ BlacklightCornell::Application.routes.draw do
 # commenting out until certain this is a dead-end route  get 'backend/clio_recall/:id', :to => "backend#clio_recall" , :as => :clio_recall
   get 'backend/feedback_mail', :to => "backend#feedback_mail"
 
-#ArgumentError: Invalid route name, already in use: 'catalog_email' 
-#You may have defined two routes with the same name using the `:as` option, or you may be overriding a route already defined by a resource with the same naming. For the latter, you can restrict the routes created with `resources` as explained here: 
+#ArgumentError: Invalid route name, already in use: 'catalog_email'
+#You may have defined two routes with the same name using the `:as` option, or you may be overriding a route already defined by a resource with the same naming. For the latter, you can restrict the routes created with `resources` as explained here:
   #post 'catalog/sms' => 'catalog#sms', :as => 'catalog_sms' # :via => :post
   get 'catalog/check_captcha' => 'catalog#check_captcha', :as => 'check_captcha'
 
   resources :catalog, only:  [:post, :get]
   get 'catalog/email' => 'catalog#email', :as => 'xcatalog_email', :via => :post
-  
+
   get '/browse/authors' => 'browse#authors', :as => 'browse_authors'
   get '/browse/info' => 'browse#info', :as => 'browse_info'
   get '/browse' => 'browse#index', :as => 'browse_index'
@@ -42,7 +42,7 @@ BlacklightCornell::Application.routes.draw do
   get '/browse_subject' => 'browse#index_subject', :as => 'browse_index_subject'
   get '/browse/heading_subject' => 'browse#show_subject', :as => 'browse_show_subject'
   get '/browse_authortitle' => 'browse#index_authortitle', :as => 'browse_index_authortitle'
-  
+
 
   match '/aeon/:bibid' => 'aeon#request_aeon', :as => 'request_aeon', :via => [:post, :put, :get]
   get '/databases' => 'databases#index', :as => 'databases_index'
@@ -51,12 +51,14 @@ BlacklightCornell::Application.routes.draw do
   get '/databases/subject/:q' => 'databases#subject', :as => 'databases_subject'
   get '/databases/show/:id' => 'databases#show', :as => 'databases_show'
   get '/databases/searchERMdb/' => 'databases#searchERMdb', :as => 'databases_searchERMdb'
-  
+
   get '/search', :to => 'search#index', :as => 'search_index'
   match "search/:engine", :to => "search#single_search", :as => "single_search", via: [ :get ]
   get '/digitalcollections' => 'digitalcollections#index', :as => 'digitalcollections_index'
   get '/digitalcollections/searchdigreg/' => 'digitalcollections#searchdigreg', :as => 'digitalcollections_searchdigreg'
 
+  get '/advanced_search', :to =>'advanced_search#index', :as => 'advanced_search_index'
+  get '/edit', :to =>'advanced_search#edit', :as => 'advanced_search_edit'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
