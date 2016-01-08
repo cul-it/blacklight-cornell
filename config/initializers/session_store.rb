87c5526e6e074d86263bba5086127c8e34b6fb4b
Rick Silterra <es287@cornell.edu>
2016-01-08 10:22:39 -0500

    Don't use exception for authenticy token failure, use null session instead. Set session to time out in 10 minutes.
    Former-commit-id: 270a1c1845589b239779b99854b0f9fc51152556

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,5 +5,5 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-BlacklightCornell::Application.config.session_store :active_record_store
+BlacklightCornell::Application.config.session_store  :active_record_store,  :expire_after => 10.minutes 
 ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
