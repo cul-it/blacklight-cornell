6952014874b625bf5b582faf7cc39ff4a7b5525f
jac244 <jac244@cornell.edu>
2016-01-12 10:46:31 -0500

    turn call to Databases.update back on

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,6 @@
 scheduler = Rufus::Scheduler.new
 scheduler.cron '45 01 * * *' do
 #scheduler.every '3m' do
-#  Rails.logger.info("Calling Databases.update #{Time.now}")
-#  Databases.update
+  Rails.logger.info("Calling Databases.update #{Time.now}")
+  Databases.update
 end
