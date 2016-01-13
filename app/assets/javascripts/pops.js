7adffa06ccdb50109cf08a60639f0c958aea4589
starsplatter <jenncd@gmail.com>
2016-01-13 16:10:10 -0500

    fix twitch

--- app/assets/javascripts/pops.js
+++ app/assets/javascripts/pops.js
@@ -1,12 +1,10 @@
-$(document).ready(function() {
 $('*[data-poload]').click(function() {
     var e=$(this);
-    e.off('focus');
+     e.off('click');
     $.get(e.data('poload'),function(d) {
-        e.popover({content: d, html:true,trigger:'focus'}).popover('show');
+        e.popover({content: d, html:true, trigger:'focus'}).popover('show');
     });
 });
-}); 
 
 
 
