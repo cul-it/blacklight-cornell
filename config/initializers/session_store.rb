92bd0b982c63b3f539bbf54001786569fd778b43
Rick Silterra <es287@cornell.edu>
2016-04-19 14:51:33 -0400

    Add env variable to enable redis session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,16 +6,20 @@
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # WAS
-BlacklightCornell::Application.config.session_store  :active_record_store,  :expire_after => 30.minutes 
-#ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
-#BlacklightCornell::Application.config.session_store :redis_session_store, {
-#  key: '_blacklightcornell_session',
-#  redis: {
-#    db: 2,
-#    expire_after: 10.minutes,
-#    key_prefix: 'blacklightcornell:session:',
+if ENV['REDIS_SESSION_HOST']
+  ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
+  BlacklightCornell::Application.config.session_store :redis_session_store, {
+  key: '_blacklightcornell_session',
+  redis: {
+    db: 2,
+    expire_after: 10.minutes,
+    key_prefix: 'blacklightcornell:session:',
 #    host: 'newcatalog-test.49jyrp.0001.use1.cache.amazonaws.com', # Redis host name, default is localhost
-#    port: 6379   # Redis port, default is 6379
-#  }
-#}
+    host: ENV['REDIS_SESSION_HOST'],
+    port: ENV['REDIS_SESSION_PORT']    # Redis port, default is 6379
+  }
+}
+else 
+  BlacklightCornell::Application.config.session_store  :active_record_store,   {:expire_after => 30.minutes }
 
+end
