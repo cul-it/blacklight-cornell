ab236f9baf299863dc845f427282e2e0f4c770ea
starsplatter <jenncd@gmail.com>
2016-07-14 12:10:37 -0400

    test for browse and authorities

---
+++ features/step_definitions/browse_steps.rb
@@ -0,0 +1,16 @@
+When /^I fill in the authorities search box with '(.*?)'$/ do |query|
+  query.gsub!(/\\"/, '"')
+  fill_in('authq', :with => query)
+end
+
+Given /^I request the personal name author item view for (.*?)$/ do |author|
+  visit "/browse/info?authq=#{author}&browse_type=Author&headingtype=Personal%20Name"
+end
+
+Given /^I request the geographic name subject item view for (.*?)$/ do |subject|
+  visit "/browse/info?authq=#{subject}&browse_type=Subject&headingtype=Geographic%20Name"
+end
+
+Given /^I request the author title item view for (.*?)$/ do |at|
+  visit "/browse/info?authq=#{at}&browse_type=Author-Title"
+end
