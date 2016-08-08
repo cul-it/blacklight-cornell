47eb9f427349b4a649e4126fbe5d70308f58d289
jac244 <jac244@cornell.edu>
2016-08-08 16:09:54 -0400

    parse almost done
    Former-commit-id: 3c990d47d4c2def53bf03f89c076682729308a1d

--- app/helpers/cornell_params_helper.rb
+++ app/helpers/cornell_params_helper.rb
@@ -66,11 +66,61 @@ module CornellParamsHelper
   
   def parse_for_stemming(params)
     query_string = params[:q]
-    space_count = query_string.scan(/ /).count
-    dubquote_count = query_string.scan(/\"/).count
-    Rails.logger.info("STEM = #{dubquote_count}")
-    Rails.logger.info("STEMspace = #{space_count}")
-    return query_string
+    if query_string =~ /^\".*\"$/
+      Rails.logger.info("STEMfullstringquoted = #{query_string}")
+      return query_string
+    else 
+      unless query_string.nil?
+        space_count = query_string.scan(/ /).count
+        dubquote_count = query_string.scan(/\"/).count
+        if dubquote_count % 2 != 0
+           query_string = query_string + '"'
+        end
+        stringChars = query_string.chars
+        i = -1
+        allquotes = []
+        while i = query_string.index('"', i+1)
+          allquotes << i
+        end
+        q_row = []
+        querystringcount = stringChars.count
+        if allquotes.count > 0
+         startnum = 0
+         endcount = allquotes.count - 1
+         params[:q_row] = []
+#         params[:q_row] = query_string.scan(/".*?"/)
+         allquotes.each_slice(2) do |a, b|
+           if startnum == 0 and query_string[0] != '"'
+             params[:q_row] << query_string[0..a-2]
+             params[:q_row] << query_string[a..b]
+             startnum = b
+           #  next
+           else
+            if startnum < a 
+              params[:q_row] << query_string[startnum+2..a-1]
+            else
+              params[:q_row] << query_string[a..b]
+            end
+            startnum = b
+           # next
+           end
+           if b == allquotes[allquotes.count - 1]
+             if b < querystringcount
+               params[:q_row] << query_string[b+2..querystringcount -1]
+             end
+           end
+         end
+         quote_location = query_string.index(/\"/)
+         Rails.logger.info("STEM = #{query_string[0]}")
+         Rails.logger.info("STEMspace = #{space_count}")
+         Rails.logger.info("STEMwhere = #{quote_location}")
+         Rails.logger.info("STEMChars = #{stringChars}") 
+         Rails.logger.info("STEMQuotLocs = #{allquotes}") 
+         Rails.logger.info("STEMQ_ROW = #{params[:q_row]}")
+       end
+      end
+      return query_string
+    end
   end
 
   def solr_search_params(my_params = params || {})
