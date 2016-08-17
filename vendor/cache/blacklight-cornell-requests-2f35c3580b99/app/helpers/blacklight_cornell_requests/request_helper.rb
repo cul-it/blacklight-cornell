f2252bca2c50d4bf31461a9fd439aefb80e09ff8
Rick Silterra <es287@cornell.edu>
2016-08-17 16:10:27 -0400

    DISCOVERYACCESS-2786 -- adapt unapi to BL6

---
+++ vendor/cache/blacklight-cornell-requests-2f35c3580b99/app/helpers/blacklight_cornell_requests/request_helper.rb
@@ -0,0 +1,22 @@
+module BlacklightCornellRequests
+  module RequestHelper
+
+  	# Time estimates are now delivered from the model in ranges (e.g., [1, 3])
+  	# instead of integers. This function converts the range into a string for display
+  	def delivery_estimate_display time_estimate
+
+  		if time_estimate[0] == time_estimate[1]
+  			pluralize(time_estimate[0], 'working day')
+  		else
+  			"#{time_estimate[0]} to #{time_estimate[1]} working days"
+  		end
+  	
+  	end
+    
+    # Retrieve the 'responsibility' field value (used by several views)
+    def take_responsibility
+      Blacklight::ShowPresenter.new(@document, self).field_value 'title_responsibility_display'
+    end
+      
+  end
+end
