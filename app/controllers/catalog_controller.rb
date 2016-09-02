b2014d88dae42397e9cd3230edd6bcba39cb93e8
Rick Silterra <es287@cornell.edu>
2016-09-01 23:06:57 -0400

    I could not merge the touhotfix -- i had to manually implement the changes.
    Former-commit-id: a81af0410867cfd32930cf90da1f56ddc2bcf5dd

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -837,9 +837,9 @@ end
      if dbcode.nil? or dbcode == '' #check for providerCode being nil
            @defaultRightsText = "Use default rights text"
      else
-       @ermDBResult = ::Erm_data.where(Database_Code: "\'#{dbcode[0]}\'", Prevailing: 'true')
+       @ermDBResult = ::Erm_data.where(Database_Code: dbcode, Provider_Code: providercode, Prevailing: 'true')
        if @ermDBResult.size < 1
-         @ermDBResult = ::Erm_data.where("Provider_Code = \'#{providercode[0]}\' AND Prevailing = 'true' AND (Database_Code =  '' OR Database_Code IS NULL)")
+         @ermDBResult = ::Erm_data.where("Provider_Code = \'#{providercode}\' AND Prevailing = 'true' AND (Database_Code =  '' OR Database_Code IS NULL)")
 
          if @ermDBResult.size < 1
         #   @defaultRightsText = "DatabaseCode and ProviderCode returns nothing"
