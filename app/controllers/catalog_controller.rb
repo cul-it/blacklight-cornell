17256ea113dd5d67c92e1d8e4f4b38fe99acd0f8
starsplatter <jenncd@gmail.com>
2016-10-14 16:28:15 -0400

    only show facets when a search has been done

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -180,27 +180,26 @@ class CatalogController < ApplicationController
     # facet bar
     config.add_facet_field 'online', :label => 'Access', :limit => 2, :collapse => false
     config.add_facet_field 'format', :label => 'Format', :limit => 10, :collapse => false
-    config.add_facet_field 'author_facet', :label => 'Author, etc.', :limit => 5
+    config.add_facet_field 'author_facet', :label => 'Author, etc.', :limit => 5, if: :has_search_parameters?
     config.add_facet_field 'pub_date_facet', :label => 'Publication Year', :range => {
       :num_segments => 6,
       :assumed_boundaries => [1300, Time.now.year + 1],
       :segments => true,
       :include_in_advanced_search => false
-    }, :show => true, :include_in_advanced_search => false
+    }, :show => true, :include_in_advanced_search => false, if: :has_search_parameters?
 
     config.add_facet_field 'workid_facet', :label => 'Work', :show => false
     config.add_facet_field 'language_facet', :label => 'Language', :limit => 5 , :show => true
-    config.add_facet_field 'fast_topic_facet', :label => 'Subject', :limit => 5
-    config.add_facet_field 'fast_geo_facet', :label => 'Subject: Region', :limit => 5
-    config.add_facet_field 'fast_era_facet', :label => 'Subject: Era', :limit => 5
-    config.add_facet_field 'fast_genre_facet', :label => 'Genre', :limit => 5
-    config.add_facet_field 'subject_content_facet', :label => 'Fiction/Non-Fiction', :limit => 5
+    config.add_facet_field 'fast_topic_facet', :label => 'Subject', :limit => 5, if: :has_search_parameters?
+    config.add_facet_field 'fast_geo_facet', :label => 'Subject: Region', :limit => 5, if: :has_search_parameters?
+    config.add_facet_field 'fast_era_facet', :label => 'Subject: Era', :limit => 5, if: :has_search_parameters?
+    config.add_facet_field 'fast_genre_facet', :label => 'Genre', :limit => 5, if: :has_search_parameters?
+    config.add_facet_field 'subject_content_facet', :label => 'Fiction/Non-Fiction', :limit => 5, if: :has_search_parameters?
     config.add_facet_field 'lc_alpha_facet', :label => 'Call Number', :limit => 5, :show => false
     config.add_facet_field 'location_facet', :label => 'Library Location', :limit => 5
-    config.add_facet_field 'hierarchy_facet', :hierarchy => true
     config.add_facet_field 'authortitle_facet', :show => false, :label => "Author-Title"
      config.add_facet_field 'lc_callnum_facet',
-                            if: :expandable_search?,
+                            if: :has_search_parameters?,
                            label: 'Call Number',
                            partial: 'blacklight/hierarchy/facet_hierarchy',
                            sort: 'index'
