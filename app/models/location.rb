8b4b0434f0ababd84c702e52824d66a7e1676f4b
Rick Silterra <es287@cornell.edu>
2016-10-17 10:54:08 -0400

    add aeon_elibible to location model
    Former-commit-id: ce55046b0c96b7ed13f6640023a6de7b3355715a

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,10 @@
 class Location < ActiveRecord::Base
+
+  def self.aeon_eligible?(code)
+    return where("code = ?", code).first.rmc_aeon 
+  end
+
+  def aeon_eligible?(code)
+    return where("code = ?", code).rmc_aeon 
+  end
 end
