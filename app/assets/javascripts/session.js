89cb9351639cea7ed7f35274c60e9cda8d449a4e
Rick Silterra <es287@cornell.edu>
2017-01-12 15:34:45 -0500

    DISCOVERYACCESS-2349 -- Correct typo in time calculation
    Former-commit-id: d1f58e0cc8aaedd803a0b35a53296f603b9dc43d

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -3,4 +3,4 @@ function sessionAlert() {
     ref = confirm("Press OK to refresh session. Otherwise it will reset in 30 seconds.");
     if (ref)  location.reload(true);
 }
-window.setTimeout(sessionAlert, (1000**60*60*4)-(30*1000));
+window.setTimeout(sessionAlert, (1000*60*60*4)-(30*1000));
