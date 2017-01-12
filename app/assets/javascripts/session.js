ec76ce037b692f185e7702a18a210abf7975c591
Rick Silterra <es287@cornell.edu>
2017-01-12 10:39:57 -0500

    DISCOVERYACCESS-2349 Session timer prompt 4 hours.

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -3,4 +3,4 @@ function sessionAlert() {
     ref = confirm("Press OK to refresh session.");
     if (ref)  location.reload(true);
 }
-window.setTimeout(sessionAlert, 30000);
+window.setTimeout(sessionAlert, 1000*60*4);
