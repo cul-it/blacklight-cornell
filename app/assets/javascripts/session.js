6e2dee4a40071923b4a85276a191cb52a3f6f5b8
Rick Silterra <es287@cornell.edu>
2017-01-12 21:55:58 -0500

    DISCOVERYACCESS-2349 Improve appearance of alert box.
    Former-commit-id: 5355775ef5c61a2402f7ac25789309a179abcec3

--- app/assets/javascripts/session.js
+++ app/assets/javascripts/session.js
@@ -1,6 +1,20 @@
-
 function sessionAlert() {
-    ref = confirm("Press OK to refresh session. Otherwise it will reset in 30 seconds.");
-    if (ref)  location.reload(true);
+    bootbox.confirm({
+    message: "Press OK to refresh session. Otherwise it will reset in 30 seconds.",
+    backdrop: true,
+    buttons: {
+        confirm: {
+            label: 'Yes',
+            className: 'btn-success'
+        },
+        cancel: {
+            label: 'No',
+            className: 'btn-danger'
+        }
+    },
+    callback: function (result) {
+        if (result) { location.reload(true); }
+    }
+});
 }
 window.setTimeout(sessionAlert, (1000*60*60*4)-(30*1000));
