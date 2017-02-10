b8cea4541a80f5b0f0343398783dabc1b506e687
Rick Silterra <es287@cornell.edu>
2017-02-10 12:04:02 -0500

    Try to do some error detection in the redis connection. Try to make sure that appsignal knows about it.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,14 +7,17 @@
 # (create the session table with "rails generate session_migration")
 # WAS
 
-if ENV['REDIS_SESSION_HOST'] && (ENV['RAILS_ENV'] == 'production')
+if ENV['REDIS_SESSION_HOST'] && ((ENV['RAILS_ENV'] == 'production') or (ENV['RAILS_ENV'] == 'integration'))  
   ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
   BlacklightCornell::Application.config.session_store :redis_session_store, {
   key: '_blacklightcornell_session',
+  on_redis_down: ->(e, env, sid) { Rails.logger.error("Error: #{Time.now} : #{e} could not connect to redis.")  
+                                   Appsignal.send_error(e)
+                                 },
   redis: {
     db: 2,
     expire_after: 4.hours,
-    key_prefix: 'blacklightcornell:session:',
+    key_prefix: "#{ENV['RAILS_ENV']}blacklightcornell:session:",
 #    host: 'newcatalog-test.49jyrp.0001.use1.cache.amazonaws.com', # Redis host name, default is localhost
     host: ENV['REDIS_SESSION_HOST'],
     port: ENV['REDIS_SESSION_PORT']    # Redis port, default is 6379
