124246232ffaa2958de544e319e60f7e7583bb78
Rick Silterra <es287@cornell.edu>
2017-05-08 11:20:01 -0400

    Add env variables to control vcr usage.
    Former-commit-id: df7b247e3ca2c378322b42f6531710cf8fcb7751

--- features/support/vcr_setup.rb
+++ features/support/vcr_setup.rb
@@ -1,6 +1,6 @@
 require 'vcr'
 #use_mock_and_vcr = false 
-use_mock_and_vcr = true 
+use_mock_and_vcr = ENV['VCR_USE_MOCK_AND_VCR'] ? true : false  
 VCR.configure do |c|
   #the directory where your cassettes will be saved
   c.cassette_library_dir = 'features/cassettes'
@@ -9,7 +9,7 @@ VCR.configure do |c|
     then  
       # your HTTP request service. You can also use fakeweb, typhoeus, and more
       c.hook_into :webmock
-      c.allow_http_connections_when_no_cassette = false # This means that we *always* have to use VCR for HTTP
+      c.allow_http_connections_when_no_cassette = ENV['VCR_ALLOW_HTTP'] ? true : false #false means we *always* have to use VCR for HTTP
       #c.allow_http_connections_when_no_cassette = true # This means that we don't *always* have to use VCR for HTTP, only when we want
     else 
       VCR.turn_off!
