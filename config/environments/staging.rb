dcea3988a08cc166b37c3bdc2258a3b501190d45
Alan McCarty <jam265@cornell.edu>
2017-06-20 13:08:24 -0400

    set assets to precompile

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,7 @@ BlacklightCornell::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -20,7 +20,7 @@ BlacklightCornell::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
