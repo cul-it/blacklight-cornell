b3855fda151a7f2b530aa0e3d11a607763d0d416
Jenn <jenncd@gmail.com>
2017-08-08 09:24:32 -0400

    retrieve thumbnails over https (#393)

--- app/search_engines/bento_search/digital_collections_engine.rb
+++ app/search_engines/bento_search/digital_collections_engine.rb
@@ -33,8 +33,9 @@ class BentoSearch::DigitalCollectionsEngine
       elsif i['description_tesim'].present?
         item.abstract = i['description_tesim'][0].to_s
       end
-      if i['media_URL_size_1_tesim'].present?
-        item.format_str = i['media_URL_size_1_tesim'][0].to_s
+      if i['content_metadata_image_iiif_info_ssm'].present?
+        item.format_str = i['content_metadata_image_iiif_info_ssm'][0].to_s
+        item.format_str = item.format_str.gsub('info.json','full/100,/0/native.jpg')
         end
       if i['date_tesim'].present?
         item.publication_date = i['date_tesim'][0].to_s
