f6a986bf85be66a6fc3292fb4eba540308c5f1be
Jenn <jenncd@gmail.com>
2017-09-15 08:07:47 -0400

    Discoveryaccess 3725 (#410)
    * integrate browse into simple search. get rid of old search form
    
    * rearrange fields

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -4,6 +4,8 @@ class BrowseController < ApplicationController
   include BlacklightCornell::CornellCatalog
   #include BlacklightUnapi::ControllerExtension
   before_filter :heading
+  before_action :redirect_catalog
+
   #attr_accessible :authq, :start, :order, :browse_type
 
   def heading
@@ -152,4 +154,15 @@ class BrowseController < ApplicationController
       end
     end
 end
+
+def redirect_catalog
+     if params[:browse_type]
+       if params[:browse_type].include?('catalog')
+         field=params[:browse_type].split(':')[1]
+         redirect_to "/catalog?q=#{CGI.escape params[:authq]}&search_field=#{field}"
+       end
+     end
+   end
+
+
 end
