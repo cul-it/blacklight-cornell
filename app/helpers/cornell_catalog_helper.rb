e1b4b57b67feaa8f8784f1de189bc43285a5315b
James Reidy <jreidy@cornell.edu>
2017-11-07 10:26:04 -0500

    Remove obsolete nested_hash_value

--- app/helpers/cornell_catalog_helper.rb
+++ app/helpers/cornell_catalog_helper.rb
@@ -1592,13 +1592,3 @@ def feed_item_content(document)
 	formatted = description.join("<br />")
 	return formatted
 end
-
-def nested_hash_value(obj,key)
-  if obj.respond_to?(:key?) && obj.key?(key)
-    obj[key]
-  elsif obj.respond_to?(:each)
-    r = nil
-    obj.find{ |*a| r=nested_hash_value(a.last,key) }
-    r
-  end
-end
