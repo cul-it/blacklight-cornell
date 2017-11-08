34c535666f67c6e06cce6611baed8f89c68661cd
James Reidy <jreidy@cornell.edu>
2017-11-08 12:35:25 -0500

    Only return an acquired date when one is known

--- app/helpers/cornell_catalog_helper.rb
+++ app/helpers/cornell_catalog_helper.rb
@@ -1557,12 +1557,11 @@ end
 
 def acquired_date(document)
 	if document['acquired_dt'].present?
-		Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}:  acquired_dt found"
 		# use acquired date as a date
 		acquired_date = DateTime.parse(document['acquired_dt'])
 	else
-		# use current date
-		acquired_date = DateTime.now
+		# nil means the acquired date is unknown!
+		acquired_date = nil
 	end
 	return acquired_date
 end
