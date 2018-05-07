ae34847c51e5831b74ec0566a354bcd878dca72d
Rick Silterra <es287@cornell.edu>
2018-05-07 16:16:16 -0400

    Add google login test

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -14,7 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.new(email: semail) 
       @user.save!
     end
-    provider = 'google'
+    provider = 'Google'
     if @user.persisted?
       flash[:notice] = I18n.t("devise.omniauth_callbacks.success", kind: provider)
       sign_in_and_redirect @user, event: :authentication
---
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+RSpec.feature "user logs in" do
+  scenario "using google oauth2" do
+    stub_omniauth
+    visit 'logins' 
+    expect(page).to have_link("Sign in with your google id")
+    click_link "Sign in with your google id"
+    expect(page).to have_link("Sign out")
+    expect(page).to have_link("Book Bag")
+    expect(page).to have_content("Successfully authenticated from Google account")
+  end
+
+  def stub_omniauth
+    # first, set OmniAuth to run in test mode
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: "google_oauth2",
+      uid: "12345678910",
+      info: {
+        email: "ditester@example.com",
+        first_name: "Diligent",
+        last_name: "Tester"
+      },
+      credentials: {
+        token: "abcdefg12345",
+        refresh_token: "12345abcdefg",
+        expires_at: DateTime.now
+      }
+    })
+  end
+end
