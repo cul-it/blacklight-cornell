0c5373a13ca664893472abf7123c7b56ac9e6e0f
Rick Silterra <es287@cornell.edu>
2018-05-09 10:26:49 -0400

    add user confirmation of google account to use

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,7 +232,8 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], 
     {
-      scope: 'email,profile'
+      scope: 'email,profile',
+      prompt: 'select_account'
     }
  
 
