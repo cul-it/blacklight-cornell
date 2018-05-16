12be7bf2ba5514ff60cd30b972435318df38e439
James Reidy <jreidy@cornell.edu>
2018-05-16 13:45:29 -0400

    script to calculate server time

---
+++ app/assets/javascripts/serverDate.js
@@ -0,0 +1,30 @@
+// https://www.webdeveloper.com/forum/d/228309-getting-server-datetime-with-no-server-side-script
+var xmlHttp;
+function srvTime(){
+    try {
+        //FF, Opera, Safari, Chrome
+        xmlHttp = new XMLHttpRequest();
+    }
+    catch (err1) {
+        //IE
+        try {
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        }
+        catch (err2) {
+            try {
+                xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            catch (eerr3) {
+                //AJAX not supported, use CPU time.
+                alert("AJAX not supported");
+            }
+        }
+    }
+    xmlHttp.open('HEAD',window.location.href.toString(),false);
+    xmlHttp.setRequestHeader("Content-Type", "text/html");
+    xmlHttp.send('');
+    return xmlHttp.getResponseHeader("Date");
+}
+
+var st = srvTime();
+var date = new Date(st);
