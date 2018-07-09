d1d73105f241fed3e3e0dc3510ee36c7237accb0
James Reidy <jreidy@cornell.edu>
2018-07-09 14:53:09 -0400

    remove idp_cert_fingerprint

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -215,7 +215,6 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   OneLogin::RubySaml::Attributes.single_value_compatibility = false 
   config.omniauth :saml,
-    idp_cert_fingerprint: ENV['SAML_IDP_CERT_FINGERPRINT'],
     idp_cert: ENV['SAML_IDP_CERT'],
     idp_sso_target_url: ENV['SAML_IDP_TARGET_URL'],
     issuer: ENV['SAML_SP_ISSUER'],
