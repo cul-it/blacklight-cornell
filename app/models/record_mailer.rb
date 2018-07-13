d8e1e2f8d2d20c5f2d6237401d3147b9e57c048f
James Reidy <jreidy@cornell.edu>
2018-07-13 09:23:12 -0400

    limit less

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -17,7 +17,7 @@ class RecordMailer < ActionMailer::Base
     Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: email_record"
     Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: Details: " + details.inspect
     Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: Params: " + params.inspect
-    puts caller(0..5)
+    puts caller(0..10)
 
     if @callnumber.nil?
       @callnumber = params["callnumber"]
