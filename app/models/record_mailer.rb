d849aac519ea87ac9ad80fb75df74a2ffa11b5fb
James Reidy <jreidy@cornell.edu>
2018-07-13 12:15:25 -0400

    look at documents

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -17,7 +17,8 @@ class RecordMailer < ActionMailer::Base
     Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: email_record"
     Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: Details: " + details.inspect
     Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: Params: " + params.inspect
-    puts caller(0..10)
+    Rails.logger.debug "jgr25_log #{__FILE__} #{__LINE__}: documents: " + @documents.inspect
+    #puts caller(0..10)
 
     if @callnumber.nil?
       @callnumber = params["callnumber"]
