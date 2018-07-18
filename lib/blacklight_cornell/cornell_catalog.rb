e057afae7411b1544a12e9da25a8285dc51a5141
jac244 <jac244@cornell.edu>
2018-07-18 14:24:30 -0400

    fix for constraints, quick hack for quoted strings still needs work

--- lib/blacklight_cornell/cornell_catalog.rb
+++ lib/blacklight_cornell/cornell_catalog.rb
@@ -119,6 +119,9 @@ Blacklight::Catalog::SearchHistoryWindow = 12 # how many searches to save in ses
     extra_head_content << view_context.auto_discovery_link_tag(:atom, url_for(params.merge(:format => 'atom')), :title => t('blacklight.search.atom_feed') )
     set_bag_name 
     # make sure we are not going directly to home page
+   if !params[:qdisplay].nil?
+     params[:qdisplay] = ''
+   end
    search_session[:per_page] = params[:per_page]
     temp_search_field = ''
     journal_titleHold = ''
@@ -149,7 +152,6 @@ Blacklight::Catalog::SearchHistoryWindow = 12 # how many searches to save in ses
     end
  #      params[:q] = '"journal of parasitology"'
  #     params[:search_field] = 'quoted'
-    Rails.logger.info("FARTS = #{params[:q]}")
     logger.info "es287_debug #{__FILE__}:#{__LINE__}:#{__method__} params = #{params.inspect}"
     (@response, @document_list) = search_results(params)
     logger.info "es287_debug #{__FILE__}:#{__LINE__}:#{__method__} response = #{@response[:responseHeader].inspect}"
@@ -719,8 +721,9 @@ def check_params(params)
      if (params[:search_field] != 'journal title ' and params[:search_field] != 'call number')# or params[:action] == 'range_limit'
        qparam_display = params[:q]
        params[:qdisplay] = params[:q]
+    #   params[:q] = parseQuoted(params[:q])
        if !params[:search_field].include?('browse')
-        qarray = params[:q].split
+        qarray = params[:q].split('" ')
        else
         qarray = [params[:q]]
        end
@@ -781,7 +784,8 @@ def check_params(params)
                  if bits.include?(':')
                    bits.gsub!(':','\\:')
                  end
-                 if bits.first == '"' and bits.last == '"'
+                 if bits.first == '"' 
+                    bits = bits + '"'
                     if fieldname == ''
                      params[:q] << '+quoted:' + bits + ' '
                     else 
@@ -804,7 +808,6 @@ def check_params(params)
           end
        end   
     end
-
     #    if params[:search_field] = "call number"
     #      params[:q] = "\"" << params[:q] << "\""
     #    end
@@ -817,7 +820,7 @@ def check_params(params)
     #    if params[:q].blank?
     #      params[:q] = '*'
     #    end 
-    Rails.logger.info("BROZETTI = #{params}")
+#    params[:q] = '(+\\\"combined heat and power\\\") AND (+cogeneration)'
    return params
   end
   
@@ -892,7 +895,17 @@ def check_params(params)
      end    
   end
 
-    
+  def parseQuoted(q)
+    if q.first == '"' and q.last == '"'
+      return q
+    else
+      howmany = q.count('"')
+      if !howmany.even?
+        q = q + '"'
+      end
+    end
+    return q
+  end  
 
 
   def set_bag_name
