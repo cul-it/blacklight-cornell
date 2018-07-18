77a9d804e242a2b8741eb5c107ada4cb0df392ea
James Reidy <jreidy@cornell.edu>
2018-07-18 13:26:23 -0400

    rename clearselected

--- app/controllers/book_bags_controller.rb
+++ app/controllers/book_bags_controller.rb
@@ -133,7 +133,7 @@ class BookBagsController < CatalogController
     redirect_to :action => "index"
   end
 
-  def clear_selected
+  def clearselected
     @bms =@bb.index
     all_docs = @bms.map {|b| b.sub!("bibid-",'')}
     puts all_docs.inspect
