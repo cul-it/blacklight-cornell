4b154910cd202e9991874c7aefe92c7e7ef2b20f
jac244 <jac244@cornell.edu>
2018-09-10 13:56:30 -0400

    turn off frozen strings

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 # operations on strings are so prevalent must unfreeze them.
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
