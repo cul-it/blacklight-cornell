449979758ead60df8ad385bec41cf7a53ccf23b8
Melissa Wallace <mhk33@lib-mhk33-maclap.local>
2018-09-10 13:45:48 -0400

    DISCOVERYACCESS-4201: add tooltip to select box on search results and non-logged-in item view

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -1,3 +1,15 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// Add tooltip to select box on search results
+$(".select-item").attr({
+	"data-toggle": "tooltip",
+	"data-placement": "bottom",
+	"title": "Sign in first to email items or save them to Book Bag"
+});
+$(".show-select-box").attr({
+	"data-toggle": "tooltip",
+	"data-placement": "bottom",
+	"title": "Sign in first to email items or save them to Book Bag"
+});
