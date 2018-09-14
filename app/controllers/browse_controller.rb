44b5007f523b24cc26b2dcef55e33ed88ef1a89e
James Reidy <jreidy@cornell.edu>
2018-09-14 14:48:48 -0400

    return full response so parameters are available

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -115,7 +115,7 @@ class BrowseController < ApplicationController
        else
           @headingsResponseFull = eval("Could not find")
        end
-       @headingsResponse = @headingsResponseFull['response']['docs']
+       @headingsResponse = @headingsResponseFull
        params[:authq].gsub!('%20', ' ')
        #Rails.logger.info("jgr25_debug #{__FILE__} #{__LINE__}  = headingResponse: " + @headingsResponse.inspect )
       end
