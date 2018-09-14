605020f7521c6ee35ac0c081ea1709a8a8d25921
James Reidy <jreidy@cornell.edu>
2018-09-14 12:05:29 -0400

    parameter to match solr

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -9,7 +9,7 @@ class BrowseController < ApplicationController
   @@browse_index_author = ENV['BROWSE_INDEX_AUTHOR'].nil? ? 'author' : ENV['BROWSE_INDEX_AUTHOR']
   @@browse_index_subject = ENV['BROWSE_INDEX_SUBJECT'].nil? ? 'subject' : ENV['BROWSE_INDEX_SUBJECT']
   @@browse_index_authortitle = ENV['BROWSE_INDEX_AUTHORTITLE'].nil? ? 'authortitle' : ENV['BROWSE_INDEX_AUTHORTITLE']
-  @@browse_index_callnumber = ENV['BROWSE_INDEX_CALLNUMBER'].nil? ? 'callnumber' : ENV['BROWSE_INDEX_CALLNUMBER']
+  @@browse_index_callnumber = ENV['BROWSE_INDEX_CALLNUMBER'].nil? ? 'callnum' : ENV['BROWSE_INDEX_CALLNUMBER']
   def heading
    @heading='Browse'
   end
