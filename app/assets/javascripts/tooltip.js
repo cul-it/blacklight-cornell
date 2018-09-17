b121ac71149fe28419629c2e025c02ab1f13828f
Melissa Wallace <mhk33@cornell.edu>
2018-09-17 10:21:15 -0400

    DISCOVERYACCESS-4201: remove not needed js

--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -2,12 +2,7 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-// Add tooltip to select box on search results
-$(".select-item").attr({
-	"data-toggle": "tooltip",
-	"data-placement": "bottom",
-	"title": "Sign in first to email items or save them to Book Bag"
-});
+// Add tooltip to select box when not signed in
 $(".show-select-box").attr({
 	"data-toggle": "tooltip",
 	"data-placement": "bottom",
