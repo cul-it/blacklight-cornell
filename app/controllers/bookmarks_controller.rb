88d645ad2321fad8f79b5f2b2d259ba633a939e3
James Reidy <jreidy@cornell.edu>
2018-09-17 16:02:14 -0400

    add bookmarks_controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+class BookmarksController < CatalogController  
+  include Blacklight::Bookmarks
+
+  def email_login_required
+    Rails.logger.level = 0
+    Rails.logger.info("jgr25_debug #{__FILE__} #{__LINE__}  = " + "bookmarks email_login_required")
+    Rails.logger.level = :warn
+
+    flash[:notice] = I18n.t('blacklight.bookmarks.need_login') and raise Blacklight::Exceptions::AccessDenied
+    redirect_to :action => "index"
+  end
+
+end 
