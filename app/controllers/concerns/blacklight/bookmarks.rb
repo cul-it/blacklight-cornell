05dddc8f0e92c119102c2392e7830fbbb86be734
James Reidy <jreidy@cornell.edu>
2018-09-17 16:02:42 -0400

    move email_login_required to bookmarks_controller

--- app/controllers/concerns/blacklight/bookmarks.rb
+++ app/controllers/concerns/blacklight/bookmarks.rb
@@ -149,13 +149,4 @@ module Blacklight::Bookmarks
     action_name == "index"
   end
 
-  def email_login_required
-    Rails.logger.level = 0
-    Rails.logger.info("jgr25_debug #{__FILE__} #{__LINE__}  = " + "bookmarks email_login_required")
-    Rails.logger.level = :warn
-
-    flash[:notice] = I18n.t('blacklight.bookmarks.need_login') and raise Blacklight::Exceptions::AccessDenied
-    redirect_to :action => "index"
-  end
-
 end
