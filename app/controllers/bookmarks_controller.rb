b69e8f9e2db5d15905368176bbe8e3625020e835
James Reidy <jreidy@cornell.edu>
2018-09-19 13:01:02 -0400

    pass login_path down to partial

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -22,7 +22,8 @@ class BookmarksController < CatalogController
  
   # same as show_email_login_required_bookmarks but for the catalog item view
   def show_email_login_required_item
-    render :partial=>"bookmarks/email_login_required_item_view"
+    login = ENV['GOOGLE_CLIENT_ID'] ?  catalog_logins_path :  user_saml_omniauth_authorize_path
+    render :partial=>"bookmarks/email_login_required_item_view", locals: { login_path: login }
   end
 
 end 
