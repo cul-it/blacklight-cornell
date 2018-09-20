451dd3f0e6a90b990844a569fe301a949e929f4c
James Reidy <jreidy@cornell.edu>
2018-09-20 09:06:08 -0400

    remove unused methods

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,19 +2,6 @@
 class BookmarksController < CatalogController  
   include Blacklight::Bookmarks
 
-  def heading
-    @heading='SelectedItems'
-   end
- 
-  def email_login_required
-    Rails.logger.level = 0
-    Rails.logger.info("jgr25_debug #{__FILE__} #{__LINE__}  = " + "bookmarks email_login_required")
-    Rails.logger.level = :warn
-
-    flash[:notice] = I18n.t('blacklight.bookmarks.need_login') and raise Blacklight::Exceptions::AccessDenied
-    redirect_to 'bookmarks'
-  end
-
   # displays the email_login_required form partial... used by an AJAX request
   def show_email_login_required_bookmarks
     render :partial=>"bookmarks/email_login_required"
