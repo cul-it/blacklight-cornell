9d9732ff6cf24a299b93aa35efba3edd6d019268
James Reidy <jreidy@cornell.edu>
2018-09-20 11:23:48 -0400

    Use the same base_solr from ENV['SOLR_URL'] like all the other browses.

--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -102,7 +102,7 @@ class BrowseController < ApplicationController
 
       if !params[:authq].nil? and params[:authq] != "" and params[:browse_type] == "Call-Number"
         # http://da-prod-solr.library.cornell.edu/solr/callnum/browse?q=%7B!tag=mq%7D%5B%22HD8011%22%20TO%20*%5D
-        call_no_solr = "http://da-prod-solr.library.cornell.edu/solr"
+        call_no_solr = base_solr
         start = {"start" => params[:start]}
         dbclnt = HTTPClient.new
         if params[:order] == "reverse"
