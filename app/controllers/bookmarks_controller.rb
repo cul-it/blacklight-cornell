5c09e5f3cdcad3b146d9b4b2e3d07174d977c26b
James Reidy <jreidy@cornell.edu>
2018-09-20 09:06:27 -0400

    remove debugging code

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,11 +9,6 @@ class BookmarksController < CatalogController
  
   # same as show_email_login_required_bookmarks but for the catalog item view
   def show_email_login_required_item
-    item_path = request.env['PATH_INFO']
-    Rails.logger.level = 0
-    Rails.logger.info("jgr25_debug #{__FILE__} #{__LINE__}  = " + "params " + params.inspect )
-    Rails.logger.level = :warn
-
     login = ENV['GOOGLE_CLIENT_ID'] ?  catalog_logins_path :  user_saml_omniauth_authorize_path
     render :partial=>"bookmarks/email_login_required_item_view", locals: { login_path: login, document_id: params[:id] }
   end
