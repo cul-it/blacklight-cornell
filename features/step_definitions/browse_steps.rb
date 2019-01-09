e2482b1bed8c5727e34f8956dd0f9b491d70e312
James Reidy <jreidy@cornell.edu>
2019-01-09 17:05:46 -0500

    specify /li also

--- features/step_definitions/browse_steps.rb
+++ features/step_definitions/browse_steps.rb
@@ -16,7 +16,7 @@ Given /^I request the author title item view for (.*?)$/ do |at|
 end
 
 Given /^I click a link with text '(.*?)' within '(.*?)'$/ do |text, id|
-  within (page.find(:id, id).find(:xpath,"//a[text()='#{text}']", visible: :all)) {
+  within (page.find(:id, id).find(:xpath,"//li/a[text()='#{text}']", visible: :all)) {
     click_link(text);
   }
 end
