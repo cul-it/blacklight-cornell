bf04b6c1010afc482c0530310e39e147fafd3784
James Reidy <jreidy@cornell.edu>
2019-03-26 16:55:17 -0400

    10 per page in the IR list

--- app/controllers/institutional_repositories_controller.rb
+++ app/controllers/institutional_repositories_controller.rb
@@ -10,7 +10,7 @@ class InstitutionalRepositoriesController < ApplicationController
     @query = params["q"].nil? ? '' : params["q"]
     @oq = @query
     @page = params["page"].nil? ?  1 : params["page"]
-    @per_page = 5
+    @per_page = 10
     @results = BentoSearch.get_engine(:institutionalRepositories).search(@query, :oq => @oq, 
       :per_page => @per_page, :page => @page)
   end
