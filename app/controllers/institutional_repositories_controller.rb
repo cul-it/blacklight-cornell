cef1653fef585bf983c31ff2ce654112392bab50
James Reidy <jreidy@cornell.edu>
2019-07-12 16:02:51 -0400

    whitespace

--- app/controllers/institutional_repositories_controller.rb
+++ app/controllers/institutional_repositories_controller.rb
@@ -14,8 +14,8 @@ class InstitutionalRepositoriesController < ApplicationController
     @use_dev_solr = params["use_dev_solr"].nil? ? nil : 1
     @search_pages_also = params["search_pages_also"].nil? ? nil : 1
     @local_dev = params["local_dev"].nil? ? nil : 1
-    @results = BentoSearch.get_engine(:institutionalRepositories).search(@query, :oq => @oq, 
-      :per_page => @per_page, :page => @page, :use_dev_solr => @use_dev_solr, 
+    @results = BentoSearch.get_engine(:institutionalRepositories).search(@query, :oq => @oq,
+      :per_page => @per_page, :page => @page, :use_dev_solr => @use_dev_solr,
       :search_pages_also => @search_pages_also, :local_dev => @local_dev)
   end
 end
