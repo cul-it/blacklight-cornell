deac199d3f47a70085b75e7080300864eac3439a
James Reidy <jreidy@cornell.edu>
2019-07-15 15:17:23 -0400

    replace test.rb with contents of development.rb for local testing

--- config/environments/test2.rb
+++ config/environments/test2.rb
@@ -1,33 +1,39 @@
 BlacklightCornell::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
- config.eager_load = false
- #config.eager_load = true
+  config.eager_load = false
+  #config.eager_load = true 
+  config.allow_concurrency = false 
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true 
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false 
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.smtp_settings = {
-    :address => 'localhost',
-    :domain => 'cornell.edu',
-    :user_name => 'culsearch@cornell.edu'
-  }
+
+  config.action_mailer.delivery_method = :test
+  #config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.smtp_settings = {
+  #  :address => 'localhost',
+  #  :domain => 'cornell.edu',
+  #  :user_name => 'culsearch@cornell.edu'
+  #}
+  config.action_mailer.default_url_options = { host: 'localhost', 
+                                             port: 3001 }
 
   # Print deprecation notices to the Rails logger
-  config.active_support.deprecation = :log
+  #config.active_support.deprecation = :log
+  #config.active_support.deprecation = :log
+  ActiveSupport::Deprecation.silenced = true
   # See everything in the log (default is :info)
- #config.log_level = ENV["LOG_LEVEL"].blank?  ? :debug : ENV["LOG_LEVEL"].to_sym
  config.log_level = ENV["LOG_LEVEL"].blank?  ? :debug : ENV["LOG_LEVEL"].to_sym
 
   # Only use best-standards-support built into browsers
@@ -35,7 +41,7 @@ BlacklightCornell::Application.configure do
 
   # Raise exception on mass assignment protection for Active Record models
   #config.active_record.mass_assignment_sanitizer = :strict
-  #config.active_record.mass_assignment_sanitizer = false
+  #config.active_record.mass_assignment_sanitizer = false 
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
@@ -46,7 +52,7 @@ BlacklightCornell::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
+  
   # this allows WEBrick to handle pipe symbols in query parameters
 #URI::DEFAULT_PARSER = URI::Parser.new(:UNRESERVED => URI::REGEXP::PATTERN::UNRESERVED + '|')
 
@@ -63,3 +69,4 @@ BlacklightCornell::Application.configure do
 #   :room_name => 'Discovery and Access'
 # }
 end
+
