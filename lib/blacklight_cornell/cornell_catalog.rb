f11c35a8806e25abf0ccedeaae4de3f1272d26ed
tworrall <tlw72@cornell.edu>
2019-09-19 11:27:41 -0400

    use deprecated_document_list instead of document list

--- lib/blacklight_cornell/cornell_catalog.rb
+++ lib/blacklight_cornell/cornell_catalog.rb
@@ -98,7 +98,8 @@ Blacklight::Catalog::SearchHistoryWindow = 12 # how many searches to save in ses
     logger.info "es287_debug #{__FILE__}:#{__LINE__}:#{__method__} zparams = #{zparams.inspect}"
     extra_head_content << view_context.auto_discovery_link_tag(:rss, url_for(params.to_unsafe_h.merge(:format => 'rss')), :title => t('blacklight.search.rss_feed') )
     extra_head_content << view_context.auto_discovery_link_tag(:atom, url_for(params.to_unsafe_h.merge(:format => 'atom')), :title => t('blacklight.search.atom_feed') )
-    (@response, @document_list) = search_service.oclc_search_results(zparams)
+    (@response, deprecated_document_list) = search_service.oclc_search_results(zparams)
+    @document_list = deprecated_document_list
     logger.info "es287_debug #{__FILE__}:#{__LINE__}:#{__method__} response = #{@response[:responseHeader].inspect}"
     num = @response["response"]["numFound"]
     logger.info "es287_debug #{__FILE__}:#{__LINE__}:#{__method__} num = #{num.inspect}"
