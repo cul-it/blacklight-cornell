d3a8439d9243f856ba94849303d7b61070369673
James Reidy <jreidy@cornell.edu>
2019-12-04 10:25:32 -0500

    Revert "my handbuilt list of databases from eds"
    This reverts commit 84ab217e2e3010749464ced5f71264508beca493.

--- config/ebsco_dbs.rb
+++ config/ebsco_dbs.rb
@@ -1,93 +1,218 @@
-$ebsco_dbs = []
-
-    $ebsco_dbs.push( 'a2h' )  # Academic Search Alumni Edition 
-    $ebsco_dbs.push( 'aph' )  # Academic Search Premier 
-    $ebsco_dbs.push( 'awn' )  # Africa-Wide Information 
-    $ebsco_dbs.push( 'gnh' )  # AgeLine 
-    $ebsco_dbs.push( 'agr' )  # Agricola 
-    $ebsco_dbs.push( 'apn' )  # Alternative Press Index 
-    $ebsco_dbs.push( 'alr' )  # Alternative Press Index Archive 
-    $ebsco_dbs.push( 'ahl' )  # America: History & Life 
-    $ebsco_dbs.push( 'h9h' )  # American Antiquarian Society (AAS) Historical Periodicals Collection: Series 1 
-    $ebsco_dbs.push( 'h9i' )  # American Antiquarian Society (AAS) Historical Periodicals Collection: Series 2 
-    $ebsco_dbs.push( 'h9j' )  # American Antiquarian Society (AAS) Historical Periodicals Collection: Series 3 
-    $ebsco_dbs.push( 'h9k' )  # American Antiquarian Society (AAS) Historical Periodicals Collection: Series 4 
-    $ebsco_dbs.push( 'h9m' )  # American Antiquarian Society (AAS) Historical Periodicals Collection: Series 5 
-    $ebsco_dbs.push( 'sbh' )  # American Bibliography of Slavic and East European Studies 
-    $ebsco_dbs.push( 'ant' )  # Anthropology Plus 
-    $ebsco_dbs.push( 'asb' )  # Applied Science & Business Periodicals Retrospective: 1913-1983 (H.W. Wilson) 
-    $ebsco_dbs.push( 'ada' )  # Architectural Digest Archive 
-    $ebsco_dbs.push( 'aft' )  # Art Full Text (H.W. Wilson) 
-    $ebsco_dbs.push( 'air' )  # Art Index Retrospective (H.W. Wilson) 
-    $ebsco_dbs.push( 'ami' )  # Art Museum Image Gallery 
-    $ebsco_dbs.push( 'apg' )  # Associated Press Images Collection 
-    $ebsco_dbs.push( 'reh' )  # Atla Religion Database 
-    $ebsco_dbs.push( 'nlabk' )  # Audiobook Collection (EBSCOhost) 
-    $ebsco_dbs.push( 'bvh' )  # Avery Index to Architectural Periodicals 
-    $ebsco_dbs.push( 'bas' )  # Bibliography of Asian Studies 
-    $ebsco_dbs.push( 'fph' )  # Bibliography of Native North Americans 
-    $ebsco_dbs.push( 'bwa' )  # Bloomberg Businessweek Archive 
-    $ebsco_dbs.push( 'bah' )  # Business Source Alumni Edition 
-    $ebsco_dbs.push( 'bth' )  # Business Source Complete 
-    $ebsco_dbs.push( 'buh' )  # Business Source Premier 
-    $ebsco_dbs.push( 'chd' )  # Chicano Database 
-    $ebsco_dbs.push( 'cin20' )  # CINAHL 
-    $ebsco_dbs.push( 'ufh' )  # Communication & Mass Media Complete 
-    $ebsco_dbs.push( 'cax' )  # Communication Abstracts 
-    $ebsco_dbs.push( 'i3h' )  # Criminal Justice Abstracts with Full Text 
-    $ebsco_dbs.push( 'nlebk' )  # eBook Collection (EBSCOhost) 
-    $ebsco_dbs.push( 'eoh' )  # EconLit with Full Text 
-    $ebsco_dbs.push( 'egh' )  # Environment Index 
-    $ebsco_dbs.push( 'ega' )  # Ergonomics Abstracts 
-    $ebsco_dbs.push( 'eric' )  # ERIC 
-    $ebsco_dbs.push( 'hev' )  # European Views of the Americas: 1493 to 1750 
-    $ebsco_dbs.push( 'ffw' )  # Fish, Fisheries & Aquatic Biodiversity Worldwide 
-    $ebsco_dbs.push( 'geh' )  # GeoRef 
-    $ebsco_dbs.push( 'guh' )  # GeoRef In Process 
-    $ebsco_dbs.push( '8gh' )  # GreenFILE 
-    $ebsco_dbs.push( 'hpi' )  # Health and Psychosocial Instruments 
-    $ebsco_dbs.push( 'hia' )  # Historical Abstracts 
-    $ebsco_dbs.push( 'htm' )  # History of Science, Technology & Medicine 
-    $ebsco_dbs.push( 'hjh' )  # Hospitality & Tourism Complete 
-    $ebsco_dbs.push( 'hoh' )  # Hospitality & Tourism Index 
-    $ebsco_dbs.push( '22h' )  # Human Resources Abstracts 
-    $ebsco_dbs.push( 'hgh' )  # Humanities International Index 
-    $ebsco_dbs.push( 'lpb' )  # Index to Legal Periodicals and Books (H.W. Wilson) 
-    $ebsco_dbs.push( 'lpr' )  # Index to Legal Periodicals Retrospective: 1908-1981 (H.W. Wilson) 
-    $ebsco_dbs.push( 'ijh' )  # International Political Science Abstracts 
-    $ebsco_dbs.push( 'fqh' )  # Left Index 
-    $ebsco_dbs.push( 'qth' )  # LGBT Life with Full Text 
-    $ebsco_dbs.push( 'lii' )  # Library Literature & Information Science Index  (H.W. Wilson) 
-    $ebsco_dbs.push( 'lxh' )  # Library, Information Science & Technology Abstracts 
-    $ebsco_dbs.push( 'lma' )  # Life Magazine Archive 
-    $ebsco_dbs.push( 'lfh' )  # Literary Reference Center 
-    $ebsco_dbs.push( 'msn' )  # MathSciNet via EBSCOhost 
-    $ebsco_dbs.push( 'fxh' )  # Middle Eastern & Central Asian Studies 
-    $ebsco_dbs.push( 'mth' )  # Military & Government Collection 
-    $ebsco_dbs.push( 'kah' )  # MLA Directory of Periodicals 
-    $ebsco_dbs.push( 'mzh' )  # MLA International Bibliography 
-    $ebsco_dbs.push( 'mah' )  # Music Index 
-    $ebsco_dbs.push( 'oah' )  # Old Testament Abstracts 
-    $ebsco_dbs.push( '24h' )  # Peace Research Abstracts 
-    $ebsco_dbs.push( 'phl' )  # Philosopher's Index 
-    $ebsco_dbs.push( 'pix' )  # Play Index (H.W. Wilson) 
-    $ebsco_dbs.push( 'pdh' )  # PsycARTICLES 
-    $ebsco_dbs.push( 'psyh' )  # PsycINFO 
-    $ebsco_dbs.push( '25h' )  # Race Relations Abstracts 
-    $ebsco_dbs.push( 'rgr' )  # Readers' Guide Retrospective: 1890-1982 (H.W. Wilson) 
-    $ebsco_dbs.push( 'bwh' )  # Regional Business News 
-    $ebsco_dbs.push( 'ram' )  # RILM Abstracts of Music Literature 
-    $ebsco_dbs.push( 'rip' )  # RIPM – With Full Text 
-    $ebsco_dbs.push( 'rsb' )  # Russian Academy of Sciences Bibliographies 
-    $ebsco_dbs.push( 'sxr' )  # Short Story Index Retrospective: 1915-1983 (H.W. Wilson) 
-    $ebsco_dbs.push( 'sxi' )  # Sociology Source Ultimate 
-    $ebsco_dbs.push( 'tdh' )  # Textile Technology Index 
-    $ebsco_dbs.push( '23h' )  # Urban Studies Abstracts 
-    $ebsco_dbs.push( 'fzh' )  # Wildlife & Ecology Studies Worldwide 
-    $ebsco_dbs.push( 'fyh' )  # Women's Studies International 
-    $ebsco_dbs.push( 'wpr' )  # World Politics Review 
-    $ebsco_dbs.push( 'imh' )  # Image Collection 
-    $ebsco_dbs.push( 'iqv' )  # Image Quick View Collection 
-    $ebsco_dbs.push( 'fsv' )  # Business Videos 
-    $ebsco_dbs.push( 'ivh' )  # Literary Image Collection 
-
+$ebsco_dbs = [
+    'edp100850',
+    'edp1150561',
+    'edp12108744',
+    'edp1378213',
+    'edp1378223',
+    'edp14316855',
+    'edp16241925',
+    'edp1644170',
+    'edp171496',
+    'edp17281089',
+    'edp1769340',
+    'edp20685',
+    'edp20690',
+    'edp20692',
+    'edp20698',
+    'edp21969706',
+    'edp489828',
+    'edp71405',
+    'edp824192',
+    'edp968750',
+    '22h',
+    '23h',
+    '24h',
+    '25h',
+    '8gh',
+    'aft',
+    'agr',
+    'ahl',
+    'air',
+    'alr',
+    'ami',
+    'ant',
+    'apg',
+    'aph',
+    'apn',
+    'asb',
+    'awn',
+    'bas',
+    'bth',
+    'bvh',
+    'bwa',
+    'bwh',
+    'cax',
+    'chd',
+    'chis',
+    'chiv',
+    'chs',
+    'cin20',
+    'cmedm',
+    'ddu',
+    'e870sww',
+    'edb',
+    'edo',
+    'edsaan',
+    'edsace',
+    'edsacl',
+    'edsagd',
+    'edsagr',
+    'edsamb',
+    'edsamd',
+    'edsarc',
+    'edsare',
+    'edsart',
+    'edsarx',
+    'edsasc',
+    'edsasp',
+    'edsavd',
+    'edsbhl',
+    'edsbio',
+    'edsblc',
+    'edsbps',
+    'edsbre',
+    'edscau',
+    'edscgd',
+    'edscoc',
+    'edscri',
+    'edscrl',
+    'edscrn',
+    'edsdoj',
+    'edsebo',
+    'edseee',
+    'edselc',
+    'edselp',
+    'edsemr',
+    'edsewc',
+    'edsfra',
+    'edsgao',
+    'edsgbe',
+    'edsgea',
+    'edsgec',
+    'edsggo',
+    'edsggr',
+    'edsgin',
+    'edsglr',
+    'edsgml',
+    'edsgmw',
+    'edsgov',
+    'edsgtd',
+    'edsgus',
+    'edsgvr',
+    'edsgwh',
+    'edshld',
+    'edshol',
+    'edshrc',
+    'edshst',
+    'edshtl',
+    'edsibw',
+    'edsimf',
+    'edsjbp',
+    'edsjov',
+    'edsjsr',
+    'edsjst',
+    'edskan',
+    'edsknv',
+    'edsluc',
+    'edsmgo',
+    'edsmor',
+    'edsnbk',
+    'edsnmj',
+    'edsnol',
+    'edsnvl',
+    'edsoaa',
+    'edsoad',
+    'edsoao',
+    'edsobb',
+    'edsodb',
+    'edsohw',
+    'edsoio',
+    'edsomo',
+    'edsonp',
+    'edsoro',
+    'edsovi',
+    'edspap',
+    'edsper',
+    'edspgr',
+    'edspmu',
+    'edspsy',
+    'edspzh',
+    'edsram',
+    'edsrbp',
+    'edsrdx',
+    'edsrem',
+    'edsrev',
+    'edsrge',
+    'edsric',
+    'edsrsa',
+    'edsscc',
+    'edssci',
+    'edsskl',
+    'edssmt',
+    'edsspo',
+    'edsssb',
+    'edsvec',
+    'edswah',
+    'edswbe',
+    'edswcl',
+    'edswsc',
+    'edswss',
+    'ega',
+    'egh',
+    'eoh',
+    'eric',
+    'ers',
+    'ffw',
+    'fph',
+    'fqh',
+    'fxh',
+    'fyh',
+    'fzh',
+    'geh',
+    'gnh',
+    'guh',
+    'h9h',
+    'h9i',
+    'h9j',
+    'h9k',
+    'h9m',
+    'hev',
+    'hgh',
+    'hia',
+    'hjh',
+    'hoh',
+    'hpi',
+    'htm',
+    'i3h',
+    'ijh',
+    'kah',
+    'lfh',
+    'lii',
+    'lpb',
+    'lpr',
+    'lxh',
+    'mah',
+    'msn',
+    'mth',
+    'mzh',
+    'nlabk',
+    'nlebk',
+    'oah',
+    'pdh',
+    'phl',
+    'pix',
+    'psyh',
+    'qth',
+    'ram',
+    'reh',
+    'rgr',
+    'rip',
+    'rsb',
+    'sbh',
+    'ssx',
+    'sxi',
+    'sxr',
+    'tdh',
+    'ufh',
+    'wpr'
+    ]
