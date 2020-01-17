82247a04631ff7854b9f8a3cca2d030601ace2d0
James Reidy <jreidy@cornell.edu>
2020-01-17 09:39:24 -0500

    avoid No registered engine for identifier 'web' (BentoSearch::NoSuchEngine)
    cucumber features/catalog_search/search_history.feature:2

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -34,7 +34,7 @@ class SearchController < ApplicationController
           #searcher = BentoSearch::MultiSearcher.new(:worldcat, :solr, :summon_bento, :web, :bestbet, :summonArticles)
           #searcher = BentoSearch::MultiSearcher.new(:worldcat, :solr, :ebsco_ds, :web, :bestbet, :summonArticles)
           #searcher = BentoSearch::ConcurrentSearcher.new(:worldcat, :solr, :ebscohost, :summon_bento, :bestbet, :digitalCollections, :libguides, :summonArticles)
-          searcher = BentoSearch::ConcurrentSearcher.new(:worldcat, :solr, :ebsco_ds, :web, :bestbet, :digitalCollections, :libguides)
+          searcher = BentoSearch::ConcurrentSearcher.new(:worldcat, :solr, :ebsco_ds, :bestbet, :digitalCollections, :libguides)
           searcher.search(@query, :oq =>original_query,:per_page => 3)
           @results = searcher.results.dup
           #@results = searcher.results
