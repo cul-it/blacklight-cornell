22a3e82233cf4e3a253f028f9da3c4f15351ada4
James Reidy <jreidy@cornell.edu>
2020-03-24 10:24:20 -0400

    Use IR_SOLR_URL from .env to set the solr url for IRs instead of Dev Solr and Local Development checkbox related values

--- app/search_engines/bento_search/institutional_repositories_engine.rb
+++ app/search_engines/bento_search/institutional_repositories_engine.rb
@@ -72,12 +72,6 @@ class BentoSearch::InstitutionalRepositoriesEngine
   end
 
   def get_solr_url(args)
-      # use this address for servers
-      solr_url = "http://digcoll#{dev}.#{internal}library.cornell.edu:8983/solr/repositories/"
-
-      # use this address for local development
-      #:solr_url => "http://digcoll.internal.library.cornell.edu:8983/solr/repositories/"
-
     solr_url = ENV['IR_SOLR_URL']
     Rails.logger.level = Logger::DEBUG # jgr25
     Rails.logger.debug "jgr25_debug solr_url = #{solr_url.to_yaml} \n#{__FILE__}:#{__LINE__}"
