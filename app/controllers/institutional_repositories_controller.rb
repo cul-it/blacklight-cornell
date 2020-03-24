6742e6d248f31bb3b8777fc2485705c43b8de034
James Reidy <jreidy@cornell.edu>
2020-03-24 10:30:37 -0400

    eliminate Include Pages checkbox - search metadata only

--- app/controllers/institutional_repositories_controller.rb
+++ app/controllers/institutional_repositories_controller.rb
@@ -11,9 +11,7 @@ class InstitutionalRepositoriesController < ApplicationController
     @oq = @query
     @page = params["page"].nil? ?  1 : params["page"]
     @per_page = 10
-    @search_pages_also = params["search_pages_also"].nil? ? nil : 1
     @results = BentoSearch.get_engine(:institutionalRepositories).search(@query, :oq => @oq,
-      :per_page => @per_page, :page => @page,
-      :search_pages_also => @search_pages_also)
+      :per_page => @per_page, :page => @page)
   end
 end
