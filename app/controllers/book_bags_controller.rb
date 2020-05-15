7a74c928c24273b30ac01640952cae7c6375dc29
James Reidy <jreidy@cornell.edu>
2020-05-15 12:57:47 -0400

    controller updates

--- app/controllers/book_bags_controller.rb
+++ app/controllers/book_bags_controller.rb
@@ -100,7 +100,7 @@ class BookBagsController < CatalogController
     Rails.logger.info("es287_debug #{__FILE__} #{__LINE__} #{__method__} @bb = #{@bb.inspect}")
     Rails.logger.info("es287_debug #{__FILE__} #{__LINE__} #{__method__} value = #{value.inspect}")
     if @bb.count < MAX_BOOKBAGS_COUNT
-      success = @bb.create(value)
+      success = @bb.create_all([value])
       user_session[:bookbag_count] = @bb.count
     end
     if request.xhr?
@@ -117,6 +117,7 @@ class BookBagsController < CatalogController
 
   def addbookmarks
     @savedll = Rails.logger.level # at any time
+    Rails.logger.level = Logger::INFO
     if current_or_guest_user.bookmarks.count > 0
       bm = current_or_guest_user.bookmarks
       Rails.logger.info("jgr25_debug #{__FILE__} #{__LINE__} #{__method__} bookmarks = #{bm.inspect}")
@@ -132,13 +133,13 @@ class BookBagsController < CatalogController
         bookmark_ids = bookmark_ids.split(0, bookmark_max)
       end
       if not bookmark_ids.to_s.empty?
-        bookmark_ids.each do | v |
-          if /[0-9]+/.match(v)
-            v.prepend('bibid-')
-            success = @bb.create(v)
-          end
-        end
-        user_session[:bookbag_count] = @bb.count
+        @bb.create_all(bookmark_ids)
+        # bookmark_ids.each do | v |
+        #   if /[0-9]+/.match(v)
+        #     v.prepend('bibid-')
+        #     success = @bb.create(v)
+        #   end
+        user_session[:bookbag_count] = @bb.count unless user_session.nil?
       end
     end
     Rails.logger.level = @savedll
@@ -147,10 +148,10 @@ class BookBagsController < CatalogController
 
   def delete
     @bibid = params[:id]
-    value = "bibid-#{@bibid}"
+    value = [@bibid]
     Rails.logger.info("es287_debug #{__FILE__} #{__LINE__} #{__method__} @bb = #{@bb.inspect}")
     Rails.logger.info("es287_debug #{__FILE__} #{__LINE__} #{__method__} value = #{value.inspect}")
-    success = @bb.delete(value)
+    success = @bb.delete_all(value)
     user_session[:bookbag_count] = @bb.count
     if request.xhr?
       success ? render(json: { bookmarks: { count: @bb.count }}) : render(plain: "", status: "500")
@@ -223,82 +224,21 @@ class BookBagsController < CatalogController
     end
   end
 
-#  def email
-#    # file = File.open("jgr25_debug.log", File::WRONLY | File::APPEND | File::CREAT)
-#    # logger = Logger.new(file)
-#    # logger.level = :info
-#    # logger.info("jgr25_debug #{__FILE__}:#{__LINE__}  request.xhr?  = #{request.xhr?.inspect}")
-#    # logger.info("jgr25_debug #{__FILE__}:#{__LINE__}  request.post?  = #{request.post?.inspect}")
-#    @bms =@bb.index
-#    all_docs = @bms.map {|b| b.sub!("bibid-",'')}
-#    if request.post?
-#      url_gen_params = {:host => request.host_with_port, :protocol => request.protocol, :params => params}
-#      if params[:to] && params[:to].match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
-#        all_docs.each_slice(20) do |docs|
-#          @response, @documents = search_service.fetch docs
-#          # logger.info("jgr25_debug #{__FILE__}:#{__LINE__}  docs  = #{docs.inspect}")
-#          url_gen_params = {:host => request.host_with_port, :protocol => request.protocol, :params => params}
-#          email ||= RecordMailer.email_record(@documents, {:to => params[:to], :message => params[:message], :callnumber => params[:callnumber], :status => params[:itemStatus],}, url_gen_params, params)
-#          email.deliver_now
-#        end
-#        flash[:success] = "Email sent"
-#        redirect_to solr_document_path(params[:id]) unless request.xhr?
-#      else
-#        flash[:error] = I18n.t('blacklight.email.errors.to.invalid', :to => params[:to])
-#      end
-#    end
-
-    # logger.info("jgr25_debug #{__FILE__}:#{__LINE__}  finished emails  = #{flash.inspect}")
-
-#   @bms =@bb.index
-#   docs = @bms.map {|b| b.sub!("bibid-",'')}
-#   @response, @documents = search_service.fetch docs
-#   Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__}  request.xhr?  = #{request.xhr?.inspect}")
-#   Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__}  flash  = #{flash.inspect}")
-#   if   ENV['SAML_IDP_TARGET_URL']
-#     if request.xhr? && flash[:success]
-#       if docs.size < 2
-#
-#         if !params[:id][0].nil?
-#           bibid = params[:id][0]
-#           render :js => "window.location = '/catalog/#{bibid}'"
-#         else
-#           render :js => "window.location = '/catalog"
-#         end
-#
-#       else
-#         render :js => "window.location = '/book_bags/index'"
-#       end
-#       return
-#     end
-#   end
-#   unless !request.xhr? && flash[:success]
-#     respond_to do |format|
-#       format.js { render :layout => false }
-#       format.html
-#     end
-#   end
-# end
-
-    # grabs a bunch of documents to export to endnote or ris.
-    def endnote
-      Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__}  params = #{params.inspect}")
-      if params[:id].nil?
-        @bms =@bb.index
-        docs = @bms.map {|b| b.sub!("bibid-",'')}
-        @response, @documents = search_service.fetch(docs, :per_page => 1000,:rows => 1000)
-        Rails.logger.debug("es287_debug #{__FILE__}:#{__LINE__}  @documents = #{@documents.size.inspect}")
-      else
-        @response, @documents = search_service.fetch(params[:id])
-      end
-      respond_to do |format|
-        format.endnote  { render :layout => false } #wrapped render :layout => false in {} to allow for multiple items jac244
-        format.ris      { render 'ris', :layout => false }
-      end
+  # grabs a bunch of documents to export to endnote or ris.
+  def endnote
+    Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__}  params = #{params.inspect}")
+    if params[:id].nil?
+      @bms =@bb.index
+      docs = @bms.map {|b| b.sub!("bibid-",'')}
+      @response, @documents = search_service.fetch(docs, :per_page => 1000,:rows => 1000)
+      Rails.logger.debug("es287_debug #{__FILE__}:#{__LINE__}  @documents = #{@documents.size.inspect}")
+    else
+      @response, @documents = search_service.fetch(params[:id])
     end
-
-
-
-
+    respond_to do |format|
+      format.endnote  { render :layout => false } #wrapped render :layout => false in {} to allow for multiple items jac244
+      format.ris      { render 'ris', :layout => false }
+    end
+  end
 
 end
