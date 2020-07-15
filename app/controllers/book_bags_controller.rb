216b7187879f4500ae7ba5624183b6446bf3886d
James Reidy <jreidy@cornell.edu>
2020-07-15 09:02:30 -0400

    causes redirect loop if you check login here

--- app/controllers/book_bags_controller.rb
+++ app/controllers/book_bags_controller.rb
@@ -257,6 +257,10 @@ Rails.logger.level = save_level
   end
 
   def index
+    # if !user_signed_in?
+    #   flash[:notice] = I18n.t('blacklight.bookmarks.use_bookmarks')
+    #   redirect_to bookmarks_path
+    # end
     @bms =@bb.index
     if @bb.is_a? BookBag
       docs = @bms.each {|v| v.to_s }
