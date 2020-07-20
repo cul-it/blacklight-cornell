6a696a96c2be47100f1e2f29a057edf9f4346530
James Reidy <jreidy@cornell.edu>
2020-07-20 10:22:16 -0400

    check controller also

--- app/controllers/concerns/blacklight/bookmarks.rb
+++ app/controllers/concerns/blacklight/bookmarks.rb
@@ -202,7 +202,7 @@ module Blacklight::Bookmarks
   private
 
   def verify_user
-    unless current_or_guest_user || (action == "index" && token_or_current_or_guest_user)
+    unless current_or_guest_user || (controller == "bookmarks" && action == "index" && token_or_current_or_guest_user)
       flash[:notice] = I18n.t('blacklight.bookmarks.need_login')
       raise Blacklight::Exceptions::AccessDenied
     end
