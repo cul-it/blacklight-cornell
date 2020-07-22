cd8ee6e203a82f35b729267012c313b812fc0767
James Reidy <jreidy@cornell.edu>
2020-07-22 08:56:46 -0400

    move bookmark preservation methods back to book_bags

--- app/controllers/book_bags_controller.rb
+++ app/controllers/book_bags_controller.rb
@@ -147,6 +147,7 @@ Rails.logger.level = save_level
   end
 
   def initialize
+    @@jgr25_debug = 1
     super
     #******************
     save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
@@ -383,5 +384,32 @@ Rails.logger.level = save_level
   Rails.logger.level = save_level
   #*******************
   end
+
+  def save_bookmarks_for_book_bags
+    if guest_user.bookmarks.present? && guest_user.bookmarks.count > 0
+      session[:bookmarks_for_book_bags] = guest_user.bookmarks.collect { |b| b.document_id.to_s }
+    end
+  #******************
+  if @@jgr25_debug
+    save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+    Rails.logger.warn "jgr25_log\n#{__method__} #{__LINE__} #{__FILE__}:"
+    msg = ["****************** #{__method__}"]
+    msg << "guest_user.bookmarks.count " + guest_user.bookmarks.count.inspect unless guest_user.bookmarks.nil?
+    msg << "session[:bookmarks_for_book_bags] " + session[:bookmarks_for_book_bags].inspect
+    msg << '******************'
+    puts msg.to_yaml
+    Rails.logger.level = save_level
+  end
+  #*******************
+  end
+
+  def get_saved_bookmarks
+      session[:bookmarks_for_book_bags]
+  end
+
+  def clear_saved_bookmarks
+      session[:bookmarks_for_book_bags] = nil;
+  end
+
 
 end
