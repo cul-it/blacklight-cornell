ffc959971e484ef1d927f336203a18814c391958
Greg Delisle <gad22@cornell.edu>
2020-08-17 17:36:19 -0400

    Try changing user back to rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :use_sudo, false
 set :scm, :git
 set :scm_verbose, true 
 #set :user, "es287"
-set :user, "jenkins"
+set :user, "rails"
 set :default_environment, {
   'PATH' => "/usr/local/rvm/gems/ruby-2.5.5/bin:/usr/local/rvm/gems/ruby-2.5.5@global/bin:/usr/local/rvm/rubies/ruby-2.5.5/bin:/usr/local/rvm/bin:/opt/rh/devtoolset-2/root/usr/bin:$PATH",
   'RUBY_VERSION' => "ruby-2.5.5",
