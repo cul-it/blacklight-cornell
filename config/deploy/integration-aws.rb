03a2401c11896ad1931bea63eb965b2bcac3bbd4
Greg Delisle <gad22@cornell.edu>
2020-08-17 22:40:59 -0400

    remove hack

--- config/deploy/integration-aws.rb
+++ config/deploy/integration-aws.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/cul/web/newcatalog-int-aws.library.cornell.edu/rails-app"
 #this avoids an error message from git, but i don't think it's really necessary.
 #as i don't think the message actually affects what gets installed.
 set :rails_env, 'integration'
-set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
+#set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
 task :install_env, :roles => [ :app, :db, :web ] do
   run "cp #{deploy_to}/../conf/latest-integration.env  #{shared_path}/.env"
   run "cat #{shared_path}/.env"
