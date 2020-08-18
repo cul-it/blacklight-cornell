6026c870a824600ed1a822ad730c5d9940122e64
Greg Delisle <gad22@cornell.edu>
2020-08-17 23:13:24 -0400

    hacking

--- config/deploy/integration-aws.rb
+++ config/deploy/integration-aws.rb
@@ -1,5 +1,5 @@
 
-server 'newcatalog7.library.cornell.edu', roles: ['app','web','db'], ssh_options: { user: 'rails', keys: '~/.ssh/id_rsa' }
+server 'newcatalog7.library.cornell.edu', :app, :web, :db', ssh_options: { user: 'rails', keys: '~/.ssh/id_rsa' }
 
 #server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
 #role :app, "newcatalog7.library.cornell.edu"
