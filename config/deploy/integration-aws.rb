290ef9f5a35747f7d74b5ef0d072a37da4ed7976
Greg Delisle <gad22@cornell.edu>
2020-08-18 09:49:34 -0400

    test

--- config/deploy/integration-aws.rb
+++ config/deploy/integration-aws.rb
@@ -24,10 +24,9 @@ server 'newcatalog7.library.cornell.edu', :app, :web, :db, user: 'jenkins'
 set :deploy_to, "/cul/web/newcatalog-int-aws.library.cornell.edu/rails-app"
 #this avoids an error message from git, but i don't think it's really necessary.
 #as i don't think the message actually affects what gets installed.
-set :rails_env, 'integration'
 #set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
+set :rails_env, 'integration'
 task :install_env, :roles => [ :app, :db, :web ] do
-#  set :ssh_options, { user: 'jenkins', keys: '~/.ssh/id_rsa' }
   run "cp #{deploy_to}/../conf/latest-integration.env  #{shared_path}/.env"
   run "cat #{shared_path}/.env"
 end
