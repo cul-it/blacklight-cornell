07121568b47a615808153605b08aba234deb2185
James Reidy <jreidy@cornell.edu>
2020-08-28 13:03:52 -0400

    whitespace

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Bookmark < ActiveRecord::Base
-  
+
   belongs_to :user, polymorphic: true
   belongs_to :document, polymorphic: true
 
@@ -11,14 +11,14 @@ class Bookmark < ActiveRecord::Base
   def document
     document_type.new document_type.unique_key => document_id
   end
-  
+
   def document_type
     default_document_type
     #(super.constantize if defined?(super)) || default_document_type
   end
-  
+
   def default_document_type
     SolrDocument
   end
-  
+
 end
