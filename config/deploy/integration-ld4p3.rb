d0a31984fb907afb402de04420abdb7b2d548f36
hudajkhan <hjk54@cornell.edu>
2020-08-31 11:53:08 -0400

    Ensuring latest file is in dev

--- config/deploy/integration-ld4p3.rb
+++ config/deploy/integration-ld4p3.rb
@@ -1,5 +1,5 @@
-server 'aws-108-114.internal.library.cornell.edu',  :app, :web, :db, user: 'jenkins' 
-server 'aws-108-199.internal.library.cornell.edu',  :app, :web, :db, user: 'jenkins' 
+server 'aws-108-114.internal.library.cornell.edu',  :app, :web, :db, :primary => true, user: 'jenkins' 
+server 'aws-108-199.internal.library.cornell.edu',  :app, :web, :db, :primary => true, user: 'jenkins' 
 # deploy second server in cluster
 
 #server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
@@ -15,17 +15,9 @@ server 'aws-108-199.internal.library.cornell.edu',  :app, :web, :db, user: 'jenk
 #role :web, "newcatalog9.library.cornell.edu"
 #role :db, "newcatalog9.library.cornell.edu", :primary => true
 
-# newrelic seems to be no longer in use
-#desc "Install newrelic file -- too sensitive for git - production"
-#task :install_new_relic_yml, :roles => [ :app, :db, :web ] do
-#        run "cat #{deploy_to}/../common/config/newrelic.yml  | sed -e 's/developer_mode:.*true/developer_mode: false/' > #{release_path}/config/newrelic.yml"          
-#end
-
 set :deploy_to, "/cul/web/ld4p3-web.library.cornell.edu/rails-app"
-#this avoids an error message from git, but i don't think it's really necessary.
-#as i don't think the message actually affects what gets installed.
 set :rails_env, 'development'
-#set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
+set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
 task :install_env, :roles => [ :app, :db, :web ] do
   run "cp #{deploy_to}/../conf/latest-integration.env  #{shared_path}/.env"
   run "cat #{shared_path}/.env"
