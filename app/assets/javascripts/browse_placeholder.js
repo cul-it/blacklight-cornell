41055ee7f7ba5778a3b48103d31f165961566b01
tworrall <tlw72@cornell.edu>
2020-09-14 10:21:50 -0400

    set the placeholder text when there's no authq param

--- app/assets/javascripts/browse_placeholder.js
+++ app/assets/javascripts/browse_placeholder.js
@@ -2,3 +2,12 @@ $('#browse_type').on('change', function() {
     var placeholder = $(this).find(':selected').data('placeholder');
     $('#authq').attr('placeholder', placeholder);
 });
+Blacklight.onLoad(function() {
+    if ( $('body').prop('className').indexOf("browse-index") >= 0 ) {
+         var placeholder = $('#browse_type').find(':selected').data('placeholder');
+         $('#authq').attr('placeholder', placeholder);
+         if ( !$('#outer-container').length && $('#authq').val() == "") {
+             $('#authq').focus();
+         }
+    }
+});  
