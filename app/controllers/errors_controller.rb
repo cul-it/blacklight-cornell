79db98eeb03629796ad9ecf5c5fc94706c82d576
Melissa Wallace <mhk33@cornell.edu>
2020-09-24 11:39:10 -0400

    DISCOVERYACCESS-2931: more error template refinement and add message and request

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,6 +6,8 @@ class ErrorsController < ApplicationController
   def internal_server_error
     @time = Time.now.to_s
     @server = request.env['SERVER_NAME'].to_s
+    @message = request.env["action_dispatch.exception"].class.name
+    @request = request.env["REQUEST_URI"].to_s
     @test = "text"
     render(:status => 500)
   end
