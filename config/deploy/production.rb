26fdf606faecef1041753b0d0c8f42bd8db0800b
Greg Delisle <gad22@cornell.edu>
2020-11-10 11:26:59 -0500

    Remove newrelic file from deploy process

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,10 +11,11 @@ role :app, "newcatalog6.library.cornell.edu"
 role :web, "newcatalog6.library.cornell.edu"
 role :db, "newcatalog6.library.cornell.edu", :primary => true
 
-desc "Install newrelic file -- too sensitive for git - production"
-task :install_new_relic_yml, :roles => [ :app, :db, :web ] do
-        run "cat #{deploy_to}/../common/config/newrelic.yml  | sed -e 's/developer_mode:.*true/developer_mode: false/' > #{release_path}/config/newrelic.yml"          
-end
+# Newrelic is no longer used
+#desc "Install newrelic file -- too sensitive for git - production"
+#task :install_new_relic_yml, :roles => [ :app, :db, :web ] do
+#        run "cat #{deploy_to}/../common/config/newrelic.yml  | sed -e 's/developer_mode:.*true/developer_mode: false/' > #{release_path}/config/newrelic.yml"          
+#end
 
 set :deploy_to, "/cul/web/newcatalog.library.cornell.edu/rails-app"
 #this avoids an error message from git, but i don't think it's really necessary.
