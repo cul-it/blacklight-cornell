62b1f68a98466d26d7efe1a1309e851bbd501715
Greg Delisle <gad22@cornell.edu>
2020-11-11 16:44:41 -0500

    deleting old code

--- config/deploy/production-new.rb
+++ config/deploy/production-new.rb
@@ -1,4 +1,3 @@
-#server "da-prod-web1.library.cornell.edu", "da-prod-web2.library.cornell.edu", :app, :web, :db, :primary => true
 role :app, "newcatalog4.library.cornell.edu"
 role :web, "newcatalog4.library.cornell.edu"
 role :db, "newcatalog4.library.cornell.edu", :primary => true
@@ -10,11 +9,6 @@ role :db, "newcatalog5.library.cornell.edu", :primary => true
 role :app, "newcatalog6.library.cornell.edu"
 role :web, "newcatalog6.library.cornell.edu"
 role :db, "newcatalog6.library.cornell.edu", :primary => true
-
-desc "Install newrelic file -- too sensitive for git - production"
-task :install_new_relic_yml, :roles => [ :app, :db, :web ] do
-        run "cat #{deploy_to}/../common/config/newrelic.yml  | sed -e 's/developer_mode:.*true/developer_mode: false/' > #{release_path}/config/newrelic.yml"          
-end
 
 set :deploy_to, "/cul/web/newcatalog.library.cornell.edu/rails-app"
 #this avoids an error message from git, but i don't think it's really necessary.
