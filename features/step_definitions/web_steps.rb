2c9a08e94e73245b1d22deb15f14247b179aab4f
James Reidy <jreidy@cornell.edu>
2020-11-16 11:54:24 -0500

    wip

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -124,7 +124,9 @@ Then /^there should be ([0-9]+) items selected$/ do |int|
 end
 
 Then("navigation should show Book Bag contains {int}") do |int|
-  page.find('a#book_bags_nav > span > span', text: "#{int.to_s}")
+  patiently do
+    expect(page.find('a#book_bags_nav > span > span').text).to eq(int.to_s)
+  end
 end
 
 Then /^navigation should( not)? show '([^']*)'$/ do |negation, string|
