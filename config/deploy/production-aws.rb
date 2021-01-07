e93f1c1f5af94f86da961e1ebe063688938c13b3
Greg Delisle <gad22@cornell.edu>
2021-01-07 12:25:05 -0500

    Removing comments to force a new PR

--- config/deploy/production-aws.rb
+++ config/deploy/production-aws.rb
@@ -1,11 +1,8 @@
-
 server 'newcatalog8.library.cornell.edu', :app, :web, :db, primary: true, user: 'jenkins' 
 server 'newcatalog9.library.cornell.edu', :app, :web, :db, primary: true, user: 'jenkins' 
 server 'newcatalog0.library.cornell.edu', :app, :web, :db, primary: true, user: 'jenkins' 
 
 set :deploy_to, "/cul/web/newcatalog-aws.library.cornell.edu/rails-app"
-#this avoids an error message from git, but i don't think it's really necessary.
-#as i don't think the message actually affects what gets installed.
 set :branch, ENV['GIT_BRANCH'].gsub("origin/","")
 task :install_env, :roles => [ :app, :db, :web ] do
   run "cp #{deploy_to}/../conf/production.env  #{shared_path}/.env"
