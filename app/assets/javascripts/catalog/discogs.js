15ed840db4a8aeb665bcdd299f21d76798d0d93a
tworrall <tlw72@cornell.edu>
2021-01-13 09:16:51 -0500

    Added a comment to keep the PR conga line moving.

--- app/assets/javascripts/catalog/discogs.js
+++ app/assets/javascripts/catalog/discogs.js
@@ -1,4 +1,5 @@
 // For musical recordings, used to bring in metadata from the Discogs site.
+// The discogs_id variable is set in the show.html.erb template in a javascript_tag.
 var remote = true;
 getDiscogsDetails(discogs_id)
 function getDiscogsDetails(id) {
