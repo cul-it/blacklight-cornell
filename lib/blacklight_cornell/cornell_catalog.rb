8a5e7f596e90a548de2540c99aaf81a3bac05a6f
James Reidy <jreidy@cornell.edu>
2021-01-15 13:25:22 -0500

    whitespace

--- lib/blacklight_cornell/cornell_catalog.rb
+++ lib/blacklight_cornell/cornell_catalog.rb
@@ -16,6 +16,7 @@ module BlacklightCornell::CornellCatalog extend Blacklight::Catalog
 
 
   def set_return_path
+
     Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__}  params = #{params.inspect}")
     op = request.original_fullpath
     # if we headed for the login page, should remember PREVIOUS return to.
@@ -313,7 +314,7 @@ Rails.logger.level = save_level
     @documents = [ @document ]
     # set_bag_name
     logger.info "es287_debug #{__FILE__}:#{__LINE__}:#{__method__} params = #{params.inspect}"
-    
+
     # For musical recordings, if the solr doc doesn't have a discogs id, call the Discogs module.
     # If it does have the id, save it globally and just get the image url.
     if @document["format_main_facet"] == "Musical Recording" && @document["discogs_display"].nil?
@@ -322,7 +323,7 @@ Rails.logger.level = save_level
       @discogs_id = @document["discogs_display"][0]
       @discogs_image_url = get_discogs_image(@document["discogs_display"][0])
     end
-    
+
     respond_to do |format|
       format.endnote_xml  { render :layout => false } #wrapped render :layout => false in {} to allow for multiple items jac244
       format.endnote  { render :layout => false } #wrapped render :layout => false in {} to allow for multiple items jac244
