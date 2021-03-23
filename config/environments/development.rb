d370be5b48f82b8165fce3679bb9b6a6881d69c9
James Reidy <jreidy@cornell.edu>
2021-03-23 14:53:02 -0400

    match settings in https://hixonrails.com/ruby-on-rails-tutorials/ruby-on-rails-action-mailer-configuration/

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,14 +16,15 @@ BlacklightCornell::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
     port: ENV["SMTP_PORT"],
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
-    authentication: :login,
+    authentication: :plain,
     enable_starttls_auto: true
   }
   # Print deprecation notices to the Rails logger
