aad83bb2982122aae50a67e951ee34bb4b6a312c
James Reidy <jreidy@cornell.edu>
2021-03-23 09:02:49 -0400

    DISCOVERYACCESS-6936 - debug record_mailer

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -14,6 +14,18 @@ class RecordMailer < ActionMailer::Base
 
     @availability = []
     @documents.each do |doc|
+#******************
+save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+jgr25_context = "#{__FILE__}:#{__LINE__}"
+Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
+msg = [" #{__method__} ".center(60,'Z')]
+msg << jgr25_context
+msg << "doc: " + doc.inspect
+msg << 'Z' * 60
+msg.each { |x| puts 'ZZZ ' + x.to_yaml }
+Rails.logger.level = save_level
+binding.pry
+#*******************
       doc_availability = []
       if doc['availability_json'].present?
         availability = JSON.parse(doc['availability_json'])
