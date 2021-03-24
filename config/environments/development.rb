f7decc5dc8acfc0e29db9de6b30593d4b6b58d3d
James Reidy <jreidy@cornell.edu>
2021-03-24 10:55:41 -0400

    DISCOVERYACCESS-6936 - turn off deliveries for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ BlacklightCornell::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
