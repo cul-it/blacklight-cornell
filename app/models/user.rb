106c93be535ce52eeade621ceb4db92d2cf3aaab
James Reidy <jreidy@cornell.edu>
2021-11-02 10:14:11 -0400

    whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
-# Connects this user object to Blacklights Bookmarks. 
+# Connects this user object to Blacklights Bookmarks.
  include Blacklight::User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   #devise :database_authenticatable, :registerable,
          #:recoverable, :rememberable, :trackable, :validatable
-         
-  devise :registerable, :trackable, :database_authenticatable, :timeoutable 
+
+  devise :registerable, :trackable, :database_authenticatable, :timeoutable
   devise :omniauthable, :omniauth_providers => [:saml,:google_oauth2,:facebook]
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
-  # the account. 
+  # the account.
   def to_s
     email
   end
