c8d9bec82b4fbeec4e7fffae10b6c0d58f5936ad
James Reidy <jreidy@cornell.edu>
2022-03-15 14:37:05 -0400

    use signin_path instead of user_saml_omniauth_authorze_path to get the POST action

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -13,7 +13,7 @@ class BookmarksController < CatalogController
 
   # same as show_email_login_required_bookmarks but for the catalog item view
   def show_email_login_required_item
-    login = ENV['GOOGLE_CLIENT_ID'] ?  catalog_logins_path :  user_saml_omniauth_authorize_path
+    login = ENV['GOOGLE_CLIENT_ID'] ?  catalog_logins_path :  signin_path
     render :partial=>"bookmarks/email_login_required_item_view", locals: { login_path: login, document_id: params[:id] }
   end
 
