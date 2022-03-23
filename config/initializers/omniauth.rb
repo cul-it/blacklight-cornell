b20913874c5a243562ef9b8b1abd233a9098be91
James Reidy <jreidy@cornell.edu>
2022-03-23 13:38:55 -0400

    Use omniauth via POST

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ OmniAuth.config.logger = Rails.logger if Rails.env.development?
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
   end
-OmniAuth.config.allowed_request_methods = %i[get]
+OmniAuth.config.allowed_request_methods = %i[post]
 
