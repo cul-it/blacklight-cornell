352f59ec7e497047747c31fc4c041ee662d1b5db
James Reidy <jreidy@cornell.edu>
2022-03-23 15:58:02 -0400

    conflicts with config/initializers/devise.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,2 @@
 # Rails application example
 OmniAuth.config.logger = Rails.logger if Rails.env.development?
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  end
-OmniAuth.config.allowed_request_methods = %i[post]
-
