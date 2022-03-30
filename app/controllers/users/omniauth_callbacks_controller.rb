554b010c7a52df375f43070f7479a6bee3be34ce
James Reidy <jreidy@cornell.edu>
2022-03-30 12:33:27 -0400

    DISCOVERYACCESS-7454 debugging code for saml

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -71,6 +71,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def saml
+ #******************
+ save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+ jgr25_context = "#{__FILE__}:#{__LINE__}"
+ Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
+ msg = [" #{__method__} ".center(60,'Z')]
+ msg << jgr25_context
+ msg << "request.env: " + request.env.inspect
+ msg << "session: " + session.inspect
+ msg << 'Z' * 60
+ msg.each { |x| puts 'ZZZ ' + x.to_yaml }
+ Rails.logger.level = save_level
+ #binding.pry
+#*******************
     auth = request.env["omniauth.auth"]
     semail = auth.info.email[0]
     u = User.where(email: semail).first
