8ea0ea0918dfc3e2881de2c85a386df1abd517cd
James Reidy <jreidy@cornell.edu>
2022-03-31 14:02:23 -0400

    DISCOVERYACCESS-7461 - def index for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,6 +4,22 @@ require 'repost'
 class BookmarksController < CatalogController
   include Blacklight::Bookmarks
 
+  def index
+    #******************
+    save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+    jgr25_context = "#{__FILE__}:#{__LINE__}"
+    Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
+    msg = [" #{__method__} ".center(60,'Z')]
+    msg << jgr25_context
+    msg << "args: " + args.inspect
+    msg << 'Z' * 60
+    msg.each { |x| puts 'ZZZ ' + x.to_yaml }
+    Rails.logger.level = save_level
+    #binding.pry
+    #*******************
+    super
+  end
+
   def can_add
     return current_or_guest_user.bookmarks.count < BookBagsController::MAX_BOOKBAGS_COUNT
   end
