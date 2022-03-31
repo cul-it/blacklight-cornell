3b3f5478d19b7872f7d81e1c7567ca94485051cc
James Reidy <jreidy@cornell.edu>
2022-03-31 13:57:10 -0400

    DISCOVERYACCESS-7461 - add debug code in concern to see if this is where we are

--- app/controllers/concerns/blacklight/bookmarks.rb
+++ app/controllers/concerns/blacklight/bookmarks.rb
@@ -43,6 +43,18 @@ module Blacklight::Bookmarks
   end
 
   def index
+#******************
+save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+jgr25_context = "#{__FILE__}:#{__LINE__}"
+Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
+msg = [" #{__method__} ".center(60,'Z')]
+msg << jgr25_context
+msg << "args: " + args.inspect
+msg << 'Z' * 60
+msg.each { |x| puts 'ZZZ ' + x.to_yaml }
+Rails.logger.level = save_level
+#binding.pry
+#*******************
     # if block is custom code
     if current_user && BookBag.enabled?
       flash[:notice] = I18n.t('blacklight.bookmarks.use_book_bag')
