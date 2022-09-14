7de89b0c60ec8880c53849b4023b3dbaf4a6df3c
James Reidy <jreidy@cornell.edu>
2022-09-14 15:50:58 -0400

    DISCOVERYACCESS-7627 - add debugging and avoid Location

--- config/initializers/locations.rb
+++ config/initializers/locations.rb
@@ -7,11 +7,23 @@ if ActiveRecord::Base.connection.table_exists? :locations
     LOCATIONS_CONFIG = YAML.load_file("#{::Rails.root}/config/locations.yml")
     LOCATIONS_CONFIG['locations'].each do |loc|
       Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} loc =  #{loc.inspect}")
+#******************
+save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+jgr25_context = "#{__FILE__}:#{__LINE__}"
+Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
+msg = [" #{__method__} ".center(60,'Z')]
+msg << jgr25_context
+msg << "loc: " + loc.inspect
+msg << 'Z' * 60
+msg.each { |x| puts 'ZZZ ' + x.to_yaml }
+Rails.logger.level = save_level
+#binding.pry
+#*******************
       d = loc[1]
-      l = Location.exists?("voyager_id: d['voyager_id']")
+      #jgr l = Location.exists?("voyager_id: d['voyager_id']")
       Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} l =  #{l.inspect}")
       if (l)
-        r = Location.where("voyager_id: d['voyager_id']")
+        #jgr r = Location.where("voyager_id: d['voyager_id']")
         r.first.update(d)
       else
         nl = Location.new(d)
