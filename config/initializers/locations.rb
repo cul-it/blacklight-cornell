e3c1f47f4ca89b395fa8ed97dd4319aedac583c6
James Reidy <jreidy@cornell.edu>
2022-09-14 15:02:32 -0400

    DISCOVERYACCESS-7627 - whitespace

--- config/initializers/locations.rb
+++ config/initializers/locations.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-# Contains initialization for locations 
+# Contains initialization for locations
 require 'location'
 if ActiveRecord::Base.connection.table_exists? :locations
   begin
     LOCATIONS_CONFIG = YAML.load_file("#{::Rails.root}/config/locations.yml")
-    LOCATIONS_CONFIG['locations'].each do |loc|  
+    LOCATIONS_CONFIG['locations'].each do |loc|
       Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} loc =  #{loc.inspect}")
       d = loc[1]
       l = Location.exists?(d['voyager_id'])
@@ -14,23 +14,23 @@ if ActiveRecord::Base.connection.table_exists? :locations
         r = Location.where(d['voyager_id'])
         r.first.update(d)
       else
-        nl = Location.new(d) 
-        nl.save 
+        nl = Location.new(d)
+        nl.save
       end
-       
+
     end
-    
+
   rescue Errno::ENOENT
     puts <<-eos
-  
+
     ******************************************************************************
     Your locations.yml config file is missing.
     See config/locations.yml.example
     ******************************************************************************
-  
+
     eos
-  
-  
+
+
   end
-  
+
 end
