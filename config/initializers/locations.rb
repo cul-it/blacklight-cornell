de1afe1065ea2f137b874c855cac7c336dd7bdbe
James Reidy <jreidy@cornell.edu>
2022-09-15 14:49:37 -0400

    DISCOVERYACCESS-7627 - remove Location debugging code

--- config/initializers/locations.rb
+++ config/initializers/locations.rb
@@ -7,49 +7,10 @@ if ActiveRecord::Base.connection.table_exists? :locations
     LOCATIONS_CONFIG = YAML.load_file("#{::Rails.root}/config/locations.yml")
     LOCATIONS_CONFIG['locations'].each do |loc|
       Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} loc =  #{loc.inspect}")
-#******************
-save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-jgr25_context = "#{__FILE__}:#{__LINE__}"
-Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-msg = [" #{__method__} ".center(60,'Z')]
-msg << jgr25_context
-msg << "loc: " + loc.inspect
-msg << 'Z' * 60
-msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-Rails.logger.level = save_level
-#binding.pry
-#*******************
       d = loc[1]
       query_hash = { voyager_id: d['voyager_id'] }
-      voyager_id = d["voyager_id"].nil? ? 0 : d["voyager_id"]
-      l=0
-#******************
-save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-jgr25_context = "#{__FILE__}:#{__LINE__}"
-Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-msg = [" #{__method__} ".center(60,'Z')]
-msg << jgr25_context
-msg << "voyager_id: " + voyager_id.inspect
-msg << "query_hash: " + query_hash.inspect
-msg << 'Z' * 60
-msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-Rails.logger.level = save_level
-#binding.pry
-#*******************
       l = Location.exists?(query_hash)
-#******************
-save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-jgr25_context = "#{__FILE__}:#{__LINE__}"
-Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-msg = [" #{__method__} ".center(60,'Z')]
-msg << jgr25_context
-msg << " l: " +  l.inspect
-msg << 'Z' * 60
-msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-Rails.logger.level = save_level
-#binding.pry
-#*******************
-#jgr Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} l =  #{l.inspect}")
+      Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} l =  #{l.inspect}")
       if (l)
         r = Location.where(query_hash)
         r.first.update(d)
