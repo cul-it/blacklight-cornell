da88870efb7e6839bf4fe90d1f515ea6023b4927
James Reidy <jreidy@cornell.edu>
2022-09-16 14:59:12 -0400

    DISCOVERYACCESS-7627 - typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ BlacklightCornell::Application.configure do
   # Raise exception on mass assignment protection for Active Record models
   #config.active_record.mass_assignment_sanitizer = :strict
   #config.active_record.mass_assignment_sanitizer = false
-  condig.active_record.yaml_column_permitted_classes = [
+  config.active_record.yaml_column_permitted_classes = [
     ActiveSupport::HashWithIndifferentAccess
   ]
 
