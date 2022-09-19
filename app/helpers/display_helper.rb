97cb880b73083c46522851e45e3fdc9e839c6e53
James Reidy <jreidy@cornell.edu>
2022-09-19 10:35:17 -0400

    DISCOVERYACCESS-7627 - move field name into opts

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -903,10 +903,10 @@ end
   end
 
   # Overrides original method from blacklight_helper_behavior.rb
-  def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true})
+  def link_to_document(doc, opts={:label=>nil, :counter => nil, :results_view => true, :field => 'title_display'})
     opts[:label] ||= blacklight_config.index.show_link.to_sym unless blacklight_config.index.show_link == nil
     # label = _cornell_render_document_index_label doc, opts
-    label = render_index_field_label doc, opts, 'title_display'
+    label = render_index_field_label doc, opts
     if params[:controller] == 'bookmarks'
       docID = doc.id
       link_to label, '/bookmarks/' + docID
