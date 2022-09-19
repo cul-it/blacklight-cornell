000edc7d3737c74f1f6706438b325863fda0f6d3
James Reidy <jreidy@cornell.edu>
2022-09-19 12:59:01 -0400

    DISCOVERYACCESS-7627 - more debug code

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1069,6 +1069,20 @@ end
       if fulltitle_vern.present? && english.present?
         label.prepend(fulltitle_vern + ' / ')
       end
+#******************
+save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
+jgr25_context = "#{__FILE__}:#{__LINE__}"
+Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
+msg = [" #{__method__} ".center(60,'Z')]
+msg << jgr25_context
+msg << "label: " + label.inspect
+msg << "opts: " + opts.inspect
+msg << 'Z' * 60
+msg.each { |x| puts 'ZZZ ' + x.to_yaml }
+Rails.logger.level = save_level
+#binding.pry
+#*******************
+      label
     end
 
     # Rewriting because we can't get the above to work properly....
