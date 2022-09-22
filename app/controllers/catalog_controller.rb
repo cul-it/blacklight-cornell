a852433f55cc178a46b1039b605d8e5b9495706a
James Reidy <jreidy@cornell.edu>
2022-09-22 14:10:19 -0400

    DISCOVERYACCESS-7627 - change order to provide add_show_tools_partial

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -5,8 +5,8 @@ class CatalogController < ApplicationController
   include Blacklight::Catalog
 
 #  include Blacklight::SearchHelper
-  include BlacklightCornell::CornellCatalog
   include Blacklight::DefaultComponentConfiguration
+  include BlacklightCornell::CornellCatalog
   include BlacklightUnapi::ControllerExtension
   include Blacklight::Marc::Catalog
   require 'net/http'
