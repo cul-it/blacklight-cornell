2061e09f924931e63e6dfeaaff555e82fcca5158
James Reidy <jreidy@cornell.edu>
2022-09-26 11:00:24 -0400

    DISCOVERYACCESS-7721 - add steps for jenkins pipeline

---
+++ jenkins/cucumber-features.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+echo ""
+echo "*********************************************************************************"
+echo ""
+source jenkins/environment.sh
+echo "PATH is:$PATH"
+echo "Solr: $SOLR_URL"
+which bundle
+if [ $# -eq 0 ]
+    then
+        echo "Running all feature tests."
+        COVERAGE=true ../jenkins-opts.sh
+    else
+        echo "Running feature: $1"
+        COVERAGE=true ../jenkins-opts.sh "$1"
+fi
---
+++ jenkins/prelude.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# bundler for automated tests
+set -e
+echo ""
+echo "*********************************************************************************"
+echo ""
+source jenkins/environment.sh
+gem install bundler -v 2.3.14
+bundle _2.3.14_ install
+echo ""
+echo "*********************************************************************************"
+echo "Starting tests..."
+echo ""
