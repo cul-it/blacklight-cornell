950d3e903e47b551d4a8b10e75eb8df7db9464d2
James Reidy <jreidy@cornell.edu>
2022-09-26 16:22:47 -0400

    DISCOVERYACCESS-7721 - move the cucumber options into here and run cucumber directly to catch COVERAGE

--- jenkins/cucumber-features.sh
+++ jenkins/cucumber-features.sh
@@ -7,11 +7,15 @@ source jenkins/environment.sh
 echo "PATH is:$PATH"
 echo "Solr: $SOLR_URL"
 which bundle
+OPT1="-t ~@oclc_request  -t ~@search_with_view_all_webs_match_box_with_percent "
+OPT3="-t ~@boundwith  -p jenkins_lax --format junit --out results/ "
+OPT2="-t ~@search_availability_title_mission_etrangeres_missing "
+OPT4=" -t ~@saml_off "
 if [ $# -eq 0 ]
     then
         echo "Running all feature tests."
-        COVERAGE=true ./jenkins-opts.sh
+        COVERAGE=true bundle exec cucumber RAILS_ENV=test $OPT1 $OPT2 $OPT3 $OPT4
     else
         echo "Running feature: $1"
-        COVERAGE=true ./jenkins-opts.sh "$1"
+        COVERAGE=true bundle exec cucumber RAILS_ENV=test $OPT1 $OPT2 $OPT3 $OPT4 "$1"
 fi
