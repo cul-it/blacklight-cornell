0a23681f0485bb48665a92f4960315c8616a2393
James Reidy <jreidy@cornell.edu>
2022-09-26 18:06:29 -0400

    DISCOVERYACCESS-7721 - try exporting ENV

--- jenkins/cucumber-features.sh
+++ jenkins/cucumber-features.sh
@@ -11,11 +11,13 @@ OPT1="-t ~@oclc_request  -t ~@search_with_view_all_webs_match_box_with_percent "
 OPT3="-t ~@boundwith  -p jenkins_lax --format junit --out results/ "
 OPT2="-t ~@search_availability_title_mission_etrangeres_missing "
 OPT4=" -t ~@saml_off "
+export COVERAGE=on
+export RAILS_ENV=test
 if [ $# -eq 0 ]
     then
         echo "Running all feature tests."
-        bundle exec COVERAGE=on RAILS_ENV=test cucumber $OPT1 $OPT2 $OPT3 $OPT4
+        bundle exec cucumber $OPT1 $OPT2 $OPT3 $OPT4
     else
         echo "Running feature: $1"
-        bundle exec COVERAGE=on RAILS_ENV=test cucumber $OPT1 $OPT2 $OPT3 $OPT4 "$1"
+        bundle exec cucumber $OPT1 $OPT2 $OPT3 $OPT4 "$1"
 fi
