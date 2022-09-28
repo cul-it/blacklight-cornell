8253779fb09ba3837297a13066b1d032355b4786
Greg Delisle <gad22@cornell.edu>
2022-09-28 12:41:56 -0400

    Update production-ruby3.rb

--- config/deploy/production-ruby3.rb
+++ config/deploy/production-ruby3.rb
@@ -2,7 +2,7 @@ set :stage, :production
 
 server 'aws-108-028.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
 #server 'aws-108-075.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
-#server 'aws-108-045.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
+server 'aws-108-045.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
 
 set :deploy_to, "/cul/web/newcatalog.library.cornell.edu/rails-app"
 
