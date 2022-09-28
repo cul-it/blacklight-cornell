2d9c193967ce766f9916fc1c77409ace334335f0
Greg Delisle <gad22@cornell.edu>
2022-09-28 11:10:34 -0400

    Update production-ruby3.rb

--- config/deploy/production-ruby3.rb
+++ config/deploy/production-ruby3.rb
@@ -1,14 +1,14 @@
-set :stage, :integration
+set :stage, :production
 
 server 'aws-108-028.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
-server 'aws-108-075.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
-server 'aws-108-045.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
+#server 'aws-108-075.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
+#server 'aws-108-045.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
 
 set :deploy_to, "/cul/web/newcatalog.library.cornell.edu/rails-app"
 
 set :branch, ENV['BRANCH'].gsub("origin/","") if ENV['BRANCH']
 
-set :rails_env, 'integration'
+set :rails_env, 'production'
 
 #task :install_env, :roles => [ :app, :db, :web ] do
 #  run "cp #{deploy_to}/../conf/latest-integration.env  #{shared_path}/.env"
