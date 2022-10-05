49e3aa461fa6348cdad69209b707031762d33b2b
James Reidy <jreidy@cornell.edu>
2022-10-05 17:54:01 -0400

    DISCOVERYACCESS-7721 - update bundler version to avoid gems/bundler-2.3.14/lib/bundler/dsl (LoadError)

--- jenkins/prelude.sh
+++ jenkins/prelude.sh
@@ -5,7 +5,7 @@ echo ""
 echo "*********************************************************************************"
 echo ""
 source jenkins/environment.sh
-gem install bundler -v 2.3.14
+gem install bundler -v 2.3.18
 bundle update blacklight_unapi blacklight_cornell_requests my_account sqlite3
 bundle install
 RAILS_ENV=test bin/rake db:migrate
