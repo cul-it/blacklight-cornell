8c4b651bb462a96edf2d753f906bcc44d8135112
James Reidy <jreidy@cornell.edu>
2022-10-10 10:35:52 -0400

    DISCOVERYACCESS-7755 - display solr from .env

--- jenkins/environment.sh
+++ jenkins/environment.sh
@@ -3,7 +3,6 @@ echo "**************** environment ****************"
 echo "Branch:"
 git rev-parse --abbrev-ref HEAD
 echo "JENKINS_HOME: $JENKINS_HOME"
-echo "Solr: $SOLR_URL"
 PATH=$PWD/bin:$PATH
 PATH=$GEM_HOME/bin:$PWD/bin:/usr/local/bin:$PATH
 source /etc/profile.d/rvm.sh
@@ -20,5 +19,9 @@ which bundle
 chromedriver --version
 echo "Xvfb DISPLAY value is $DISPLAY"
 cp /cul/data/jenkins/environments/blacklight-cornell.env .env
-echo 'DEBUG_USER=ditester@example.edu' >>.env
+uuid=$(uuidgen)
+DEBUG_USER="ditester${uuid}@example.edu"
+echo "Diligent Tester: $DEBUG_USER"
+echo "DEBUG_USER=${DEBUG_USER}" >>.env
+grep ^SOLR_URL .env
 export RAILS_ENV=test
