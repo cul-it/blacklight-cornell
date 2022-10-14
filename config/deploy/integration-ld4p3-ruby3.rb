caeb9dd963da5cb33f8a7bca7fa47aac30f176ac
Greg Delisle <gad22@cornell.edu>
2022-10-14 14:04:24 -0400

    Updates for ruby/cap 3

--- config/deploy/integration-ld4p3-ruby3.rb
+++ config/deploy/integration-ld4p3-ruby3.rb
@@ -1,12 +1,12 @@
 set :stage, :integration
 
-server 'aws-108-239.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
+server 'aws-108-114.internal.library.cornell.edu', roles: %w{app web db}, user: "jenkins"
 
-set :deploy_to, "/cul/web/newcatalog-int.library.cornell.edu/rails-app"
+set :deploy_to, "/cul/web/ld4p3-web.library.cornell.edu/rails-app"
 
 set :branch, ENV['BRANCH'].gsub("origin/","") if ENV['BRANCH']
 
-set :rails_env, 'integration'
+set :rails_env, 'development'
 
 #task :install_env, :roles => [ :app, :db, :web ] do
 #  run "cp #{deploy_to}/../conf/latest-integration.env  #{shared_path}/.env"
