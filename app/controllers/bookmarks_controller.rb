3b7a79119fbb4996658d63b16d04e905bc2ea467
James Reidy <jreidy@cornell.edu>
2022-10-31 13:10:04 -0400

    DISCOVERYACCESS-7852 - change sign in link

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,7 +15,7 @@ class BookmarksController < CatalogController
 
   # same as show_email_login_required_bookmarks but for the catalog item view
   def show_email_login_required_item
-    login = ENV['GOOGLE_CLIENT_ID'] ?  catalog_logins_path :  signin_path
+    login = user_saml_omniauth_authorize_path
     render :partial=>"bookmarks/email_login_required_item_view", locals: { login_path: login, document_id: params[:id] }
   end
 
