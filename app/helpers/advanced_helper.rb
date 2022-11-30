977c85377eb3d0e597d323a32deb2badd1869498
James Reidy <jreidy@cornell.edu>
2022-11-30 14:36:32 -0500

    DISCOVERYACCESS-7882 - sanitize the query string to remove injected html

--- app/helpers/advanced_helper.rb
+++ app/helpers/advanced_helper.rb
@@ -89,6 +89,7 @@ module AdvancedHelper
     else
       query = params[:q_row][0]
     end
+    query = ActionView::Base.full_sanitizer.sanitize(query)
 #    params[:q_row][0].gsub!('\\\"','')
     row1 << "<input autocapitalize=\"off\" id=\"q_row\" class=\"form-control adv-search-control\" name=\"q_row[]\" placeholder=\"Search....\" type=\"text\"" 
     row1 << " value="  
