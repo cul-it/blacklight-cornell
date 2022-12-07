bc69590b77529aa3b6a715224faa4980ead6cdc9
James Reidy <jreidy@cornell.edu>
2022-12-07 16:30:33 -0500

    DICOVERYACCESS-7882-b - try other strings

--- app/models/record_mailer.rb
+++ app/models/record_mailer.rb
@@ -77,6 +77,8 @@ jgr25_context = "#{__FILE__}:#{__LINE__}"
 Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
 msg = [" #{__method__} ".center(60,'Z')]
 msg << jgr25_context
+msg << "to: " + to.inspect
+msg << "subject: " + subject.inspect
 msg << "delivery_options: " + delivery_options.inspect
 msg << 'Z' * 60
 msg.each { |x| puts 'ZZZ ' + x.to_yaml }
