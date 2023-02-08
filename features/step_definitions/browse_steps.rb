e448d27285b00e9e2fbd50756a4566b73315bd70
James Reidy <jreidy@cornell.edu>
2023-02-08 15:27:22 -0500

    DISCOVERYACCESS-7959 - call the do_visit function instead of visit to apply remote url as prefix

--- features/step_definitions/browse_steps.rb
+++ features/step_definitions/browse_steps.rb
@@ -4,15 +4,15 @@ When /^I fill in the authorities search box with '(.*?)'$/ do |query|
 end
 
 Given /^I request the personal name author item view for (.*?)$/ do |author|
-  visit "/browse/info?authq=#{author}&browse_type=Author&headingtype=Personal%20Name"
+	do_visit "/browse/info?authq=#{author}&browse_type=Author&headingtype=Personal%20Name"
 end
 
 Given /^I request the geographic name subject item view for (.*?)$/ do |subject|
-  visit "/browse/info?authq=#{subject}&browse_type=Subject&headingtype=Geographic%20Name"
+	do_visit "/browse/info?authq=#{subject}&browse_type=Subject&headingtype=Geographic%20Name"
 end
 
 Given /^I request the author title item view for (.*?)$/ do |at|
-  visit "/browse/info?authq=#{at}&browse_type=Author-Title"
+	do_visit "/browse/info?authq=#{at}&browse_type=Author-Title"
 end
 
 Given /^I click a link with text '(.*?)' within '(.*?)'$/ do |text, id|
