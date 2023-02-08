03759e11bcb7702bc4b420ea413daddac6587a30
James Reidy <jreidy@cornell.edu>
2023-02-08 15:24:46 -0500

    DISCOVERYACCESS-7959 - apply any CUCUMBER_TEST_SITE in .env as a prefix to urls during testing

---
+++ features/step_definitions/domain_steps.rb
@@ -0,0 +1,15 @@
+
+Before do |scenario|
+    if ENV.key?("CUCUMBER_TEST_SITE")
+        # end with a single slash, whether it's there or not
+        @url = File.join(ENV["CUCUMBER_TEST_SITE"], "")
+        Capybara.app_host = @url
+    else
+        @url = nil
+    end
+end
+
+def do_visit(path)
+    target = "#{@url}#{path}"
+	visit target
+end
