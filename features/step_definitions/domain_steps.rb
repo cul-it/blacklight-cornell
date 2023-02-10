bca803e17d752ae5416bd83be416a6f910639274
James Reidy <jreidy@cornell.edu>
2023-02-10 16:54:35 -0500

    DISCOVERYACCESS-7959 - use the headless version

--- features/step_definitions/domain_steps.rb
+++ features/step_definitions/domain_steps.rb
@@ -4,7 +4,7 @@ Before do |scenario|
         # end with a single slash, whether it's there or not
         @url = File.join(ENV["CUCUMBER_TEST_SITE"], "")
         # default driver does not support remote javascript
-        Capybara.current_driver = :selenium
+        Capybara.current_driver = :selenium_headless
         Capybara.run_server = false
         Capybara.app_host = @url
     else
