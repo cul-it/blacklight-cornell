2f4044947f0b68f926c28139570cd106fd804454
James Reidy <jreidy@cornell.edu>
2023-02-10 16:29:59 -0500

    DISCOVERYACCESS-7959 - use seleneium driver - default does not support remote javascript

--- features/step_definitions/domain_steps.rb
+++ features/step_definitions/domain_steps.rb
@@ -3,6 +3,9 @@ Before do |scenario|
     if ENV.key?("CUCUMBER_TEST_SITE")
         # end with a single slash, whether it's there or not
         @url = File.join(ENV["CUCUMBER_TEST_SITE"], "")
+        # default driver does not support remote javascript
+        Capybara.current_driver = :selenium
+        Capybara.run_server = false
         Capybara.app_host = @url
     else
         @url = nil
@@ -10,5 +13,6 @@ Before do |scenario|
 end
 
 def do_visit(path)
-    visit "#{@url}#{path}"
+    # didn't actually need @url once app_host is set
+    visit "#{path}"
 end
