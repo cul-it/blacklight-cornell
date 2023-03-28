6755ea8e530a5abf548db913382edbfd3714e90f
Melissa Wallace <mhk33@cornell.edu>
2023-03-28 14:31:40 -0400

    Add aeon.scss and precompile it

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+
+Rails.application.config.assets.precompile += %w( aeon.css )
