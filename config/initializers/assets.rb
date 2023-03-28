3d568ba06994b9eac7fd7c98474eea9cd65c2d88
Melissa Wallace <mhk33@cornell.edu>
2023-03-28 15:56:03 -0400

    Add Aeon js to the pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
 
 Rails.application.config.assets.precompile += %w( aeon.css )
+Rails.application.config.assets.precompile += %w( aeon.js )
