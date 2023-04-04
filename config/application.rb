6122b82c6338b464f46d715bf5d18738ce59f86d
James Reidy <jreidy@cornell.edu>
2023-04-04 08:40:43 -0400

    DISCOVERYACCESS-7317 - prepare for Zeitwerk
    https://guides.rubyonrails.org/classic_to_zeitwerk_howto.html

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module BlacklightCornell
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
     #config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
