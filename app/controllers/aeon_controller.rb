c12d4986c3aad95c9973f415b477c7c57718a147
John Cline <jac244@cornell.edu>
2023-05-07 13:59:17 -0400

    remove all references to newcatalog-login

--- app/controllers/aeon_controller.rb
+++ app/controllers/aeon_controller.rb
@@ -237,101 +237,7 @@ class AeonController < ApplicationController
 #    return request_options.sort_by { |option| option[:estimate] }
 #  end
 
-#  def prelim( bibid, title, doctype, webreq, cart, loginurl, re506)
-#  	global bibid;
-#	global boxtype;
-#	global finding_aid;
-#	delivery_time = ""
-#	disclaimer = "Once your order is reviewed by our staff you will then be sent an invoice. Your invoice will include information on how to pay for your order. You must pre-pay, staff cannot fulfill your request until you pay the charges."
-    #re506 = ""
-    #webreq = ""
-#	fa = '';
-#	if (!@@finding_aid.empty? and @@finding_aid != '?')
-# 		fa = "
-#        <a href='" + @@finding_aid + "' target='_blank'>  Finding Aid</a>
-#        <br/>
-#		"
-#	else
-#		fa = "<a href='?scan=" + params["scan"] + "' target='_blank'>Finding Aid<a/>
-#		fa = "<br/>"
-#    end
-#	prelim = '
-#	<!DOCTYPE html>
-#	<html lang="en">
-#	<head>
-#	<title>Scanning Request for ' + title + '</title>
-	
-#	<script>var itemdata = {};</script>
-#   <meta data-name="aeon_wpv" data-bn="v5.1.14" data-bid="17648" data-cid="5169011a1c864ea61424ec386d248ba1398a6730" />
-#	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-#	<meta name="apple-mobile-web-app-capable" content="yes">
-#	<meta name="apple-mobile-web-app-status-bar-style" content="default">
-#	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
-# <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-#<link rel="stylesheet" type="text/css" href="rmc-aeon.library.cornell.edu/aeon/css/cookieconsent.min.css" />
 
-#<!-- Optional JavaScript -->
-#<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-
-#<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-#<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-#<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-#<script src="https://rmc-aeon.library.cornell.edu/aeon/js/atlasUtility.js"></script>
-#<script src="https://rmc-aeon.library.cornell.edu/aeon/js/custom.js"></script>
-#<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment-with-locales.min.js" integrity="sha256-VrmtNHAdGzjNsUNtWYG55xxE9xDTz4gF63x/prKXKH0=" crossorigin="anonymous"></script>
-#<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.21/moment-timezone-with-data.min.js" integrity="sha256-VX6SyoDzanqBxHY3YQyaYB/R7t5TpgjF4ZvotrViKAY=" crossorigin="anonymous"></script>
-#<script src="https://rmc-aeon.library.cornell.edu/aeon/js/webAlerts.js"></script>
-#<script src="https://rmc-aeon.library.cornell.edu/aeon/js/cookieconsent.min.js" data-cfasync="false"></script>
-#<script src="https://rmc-aeon.library.cornell.edu/aeon/js/atlasCookieConsent.js"></script>
-#<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js" ></script>
-#<link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/redmond/jquery-ui.css" media="screen" />
-#<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-#<script type="text/javascript" src="https://newcatalog-login.library.cornell.edu/aeon511/js/request.js"></script>
-#<link rel="stylesheet" type="text/css" href="https://newcatalog-login.library.cornell.edu/aeon/css/request.css" >
-#<link rel="stylesheet" type="text/css" href="https://newcatalog-login.library.cornell.edu/aeon511/css/aeon.css" >
-#<link rel="stylesheet" type="text/css" href="https://newcatalog-login.library.cornell.edu/aeon511/css/custom.css" >
-#<script type="text/javascript" src="https://newcatalog-login.library.cornell.edu/aeon511/js/rmc_scripts.js"></script>
-#	</head>
-#	<body>
-
-#    <header class="head">
-#  <div>
-#    <a href="#content" accesskey="S" onclick="$(\'#content\').focus();" class="offscreen">Skip to Main Content</a>
-#  </div>
-#  <div class="container">
-#    <div class="cornell-logo d-none d-sm-block">
-#      <a href="http://www.cornell.edu" class="insignia">Cornell University</a>
-#      <div class="library-brand">
-#        <a href="https://library.cornell.edu">Library</a>
-#      </div>
-#    </div>
-#    <div class="d-block d-sm-none">
-#      <a class="mobile-cornell-logo" href="https://www.cornell.edu">Cornell University</a>
-#      <a class="library-brand-mobile" href="https://library.cornell.edu">Library</a>
-#    </div>
-#    <h1>Division of Rare and Manuscript Collections</h1>
-#  </div>
-#</header>
-#    <h1>RMC Scanning Request</h1>
-    
-#    <div>' + disclaimer + '</div> 
-#	<div id="main-content">
-#	<form id="RequestForm" 
-#	action="' + loginurl + '"
-#              method="GET" class="form-horizontal">
-#	<h4>' + title + '</h4>' + fa +
-#	'<strong> ' + re506 + '</strong>' +
-#	cart + '
-#	<input type="hidden" name="AeonForm" value="PhotoduplicationRequest">
-#	<input type="hidden" name="SkipOrderEstimate" value="Yes">
-#	<input type="hidden" id="ReferenceNumber" name="ReferenceNumber" value="' + bibid + '"/>
-#	<input type="hidden" id="ItemNumber" name="ItemNumber" value=""/>
-#	<input type="hidden" name="RequestType" value="Copy"/>
-#	<input type="hidden" id="DocumentType" name="DocumentType" value="' + doctype + '"/>
-#	<input type="hidden" name="WebRequestForm" value="' + webreq + '"/> '
-
-#	return prelim
-# end
 
   def scan_prelim( bibid, title, doctype, webreq, cart, loginurl, re506)
 #  	global bibid;
@@ -382,11 +288,12 @@ class AeonController < ApplicationController
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js" ></script>
 <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/redmond/jquery-ui.css" media="screen" />
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-<script type="text/javascript" src="https://newcatalog-login.library.cornell.edu/aeon511/js/request.js"></script>
-<link rel="stylesheet" type="text/css" href="https://newcatalog-login.library.cornell.edu/aeon/css/request.css" >
-<link rel="stylesheet" type="text/css" href="https://newcatalog-login.library.cornell.edu/aeon511/css/aeon.css" >
-<link rel="stylesheet" type="text/css" href="https://newcatalog-login.library.cornell.edu/aeon511/css/custom.css" >
-<script type="text/javascript" src="https://newcatalog-login.library.cornell.edu/aeon511/js/rmc_scripts.js"></script>
+		<%= stylesheet_link_tag "aeon", :media => "all" %>
+		<!-- <link rel="stylesheet" media="all" href="/assets/aeon/_request.scss" > -->
+		<!-- <link rel="stylesheet" media="all" href="/assets/aeon/_aeon.scss" > -->
+		<!-- <link rel="stylesheet" media="all" href="/assets/aeon/_aeon_custom.scss" > -->
+		<!-- <script type="text/javascript" src="/assets/aeon/rmc_scripts.js"></script> -->
+		<%= javascript_include_tag "aeon" %>
 	</head>
 	<body>
 
@@ -451,11 +358,9 @@ class AeonController < ApplicationController
   end
 
   def loginurl
-#  	return "/aeon/aeon_login"
-#	return "https://newcatalog-login.library.cornell.edu/aeon511/aeon_test-login.php"
+  	return "/aeon/aeon_login"
 # 	return "http://dev-jac2445.library.cornell.edu/aeon511/aeon-login.php"
  # 	return "http://voy-api.library.cornell.edu/aeon/aeon_test-login.php"
-    return "http://newcatalog-login.library.cornell.edu/aeon511_test/aeon-login.php"
   end
 
   def warning(title)
