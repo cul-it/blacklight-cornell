01ce09dfe0f1a6826fc8edb30c3ef7a4b6e91c29
Shinwoo Kim <sk274@cornell.edu>
2023-06-28 10:47:15 -0400

    Use javascript strategy deletion on containerized tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -129,7 +129,12 @@ end
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-Cucumber::Rails::Database.javascript_strategy = :truncation
+# Cucumber::Rails::Database.javascript_strategy = :truncation
+Cucumber::Rails::Database.javascript_strategy = if ENV['USE_TEST_CONTAINER']
+                                                  :deletion
+                                                else
+                                                  :truncation
+                                                end
 
 # DO this to load fixture before each scenario.
 Before do
