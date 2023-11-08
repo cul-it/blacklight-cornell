b6038a46ebe7577bfcccd32b4433994848ca6aac
James Reidy <jreidy@cornell.edu>
2023-11-08 16:57:15 -0500

    DISCOVERYACCESS-7795 - remove debugging

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -962,18 +962,6 @@ end
   end
 
   def is_exportable document
-#******************
-    save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-    jgr25_context = "#{__FILE__}:#{__LINE__}"
-    Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-    msg = [" #{__method__} ".center(60,'Z')]
-    msg << jgr25_context
-    msg << "document: " + document.inspect
-    msg << 'Z' * 60
-    msg.each { |x| puts 'ZZZ ' + x.to_yaml }
-    Rails.logger.level = save_level
-    #binding.pry
-#*******************
     if document.present?
       if document.export_formats.keys.include?(:refworks_marc_txt) || document.export_formats.keys.include?(:endnote)
         return true
