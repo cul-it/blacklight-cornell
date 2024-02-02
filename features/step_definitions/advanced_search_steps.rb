e09cc686a7b167b8b523ec5075be355f84b1fbc2
James Reidy <jreidy@cornell.edu>
2024-02-02 09:57:52 -0500

    DACCESS-174 - query entry step

--- features/step_definitions/advanced_search_steps.rb
+++ features/step_definitions/advanced_search_steps.rb
@@ -8,7 +8,7 @@ When("I select {string} from the row logic radio before line {int}") do |string,
   end
 end
 
-When("I select {string} from the term logic drop-down on line {int}") do |string, int|
+When("I select {string} from the query logic drop-down on line {int}") do |string, int|
   if int == 1
     id = "op_row"
   else
@@ -26,5 +26,7 @@ When("I select {string} from the fields drop-down on line {int}") do |string, in
   page.find_by_id(id).select( "#{string}")
 end
 
-# html.js body.advanced_search.advanced_search-index div#maincontent.main-content div#main-container.container div.row div.col-sm-12 div.card.card-body.card-well form.advanced div.query_zone div.input_row fieldset div.boolean_row.radio.adv-search-control div.form-check.form-check-inline label input
-# div.input_row:nth-child(3) > fieldset:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(1)
+When("I enter {string} as the query on line {int}") do |string, int|
+  id = "q_row#{int}"
+  page.find_by_id(id).set("#{string}")
+end
