8f3185d43c3286b93ea6ece5a30c518f658c482a
James Reidy <jreidy@cornell.edu>
2024-04-19 13:53:05 -0400

    DACCESS-215 - change deprecation notice

--- blacklight-cornell/config/environments/test.rb
+++ blacklight-cornell/config/environments/test.rb
@@ -57,4 +57,7 @@ BlacklightCornell::Application.configure do
   # this allows WEBrick to handle pipe symbols in query parameters
 #URI::DEFAULT_PARSER = URI::Parser.new(:UNRESERVED => URI::REGEXP::PATTERN::UNRESERVED + '|')
 
+  # suppress deprecation warnings
+  Deprecation.default_deprecation_behavior = :silence # :stderr # :silence
+
 end
