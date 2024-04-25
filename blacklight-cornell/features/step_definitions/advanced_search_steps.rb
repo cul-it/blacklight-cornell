e92cbc621bd16beb9dcc33c290fa894e1dff1219
James Reidy <jreidy@cornell.edu>
2024-04-25 17:16:33 -0400

    DACCESS-297 - resolve steps

--- blacklight-cornell/features/step_definitions/advanced_search_steps.rb
+++ blacklight-cornell/features/step_definitions/advanced_search_steps.rb
@@ -1,37 +1,10 @@
 When("I select {string} from the row logic radio before line {int}") do |string, int|
   row_offset = int
-<<<<<<< HEAD:features/step_definitions/advanced_search_steps.rb
-  within("div.input_row:nth-child(#{row_offset}) > fieldset > div.adv-search-control") do 
-=======
   within("div.input_row:nth-child(#{row_offset}) > fieldset > div.adv-search-control") do
->>>>>>> dev:blacklight-cornell/features/step_definitions/advanced_search_steps.rb
     find_field(string, :disabled => :all).set(true)
   end
 end
 
-<<<<<<< HEAD:features/step_definitions/advanced_search_steps.rb
-When("I select {string} from the query logic drop-down on line {int}") do |string, int|
-  if int == 1
-    id = "op_row"
-  else
-    id = "op_row#{int}"
-  end
-  page.find_by_id(id).select( "#{string}")
-end
-
-When("I select {string} from the fields drop-down on line {int}") do |string, int|
-  if int == 1
-    id = "search_field_advanced"
-  else
-    id = "search_field_advanced#{int}"
-  end
-  page.find_by_id(id).select( "#{string}")
-end
-
-When("I enter {string} as the query on line {int}") do |string, int|
-  id = "q_row#{int}"
-  page.find_by_id(id).set("#{string}")
-=======
 When("I use {string} with {string} logic for field {string} on line {int} of advanced search") do |query, logic, field, line|
   id = "q_row#{line}"
   if line == 1
@@ -65,5 +38,4 @@ When("I view the {string} version of the search results") do |format|
 
   # Visit the new URL
   visit new_url
->>>>>>> dev:blacklight-cornell/features/step_definitions/advanced_search_steps.rb
 end
