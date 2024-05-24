05c9a8ba4620c6b727a37d5df65b8139538777b6
James Reidy <jreidy@cornell.edu>
2024-05-24 09:49:48 -0400

    DACCESS-311 - second search_field_row parameter is somehow necessary

--- blacklight-cornell/lib/blacklight_cornell/cornell_catalog.rb
+++ blacklight-cornell/lib/blacklight_cornell/cornell_catalog.rb
@@ -445,9 +445,9 @@ module BlacklightCornell::CornellCatalog extend Blacklight::Catalog
     @id = ActionController::Base.helpers.sanitize(params[:id])
 
     redirect_to utf8: "✓",
-      q_row: ["#{@id}"],
-      op_row: ["AND"],
-      search_field_row: ["number"],
+      q_row: ["#{@id}", ""],
+      op_row: ["AND", "AND"],
+      search_field_row: ["number", "all_fields"],
       sort: "score desc, pub_date_sort desc, title_sort asc",
       search_field: "advanced",
       advanced_query: "yes",
@@ -470,6 +470,21 @@ module BlacklightCornell::CornellCatalog extend Blacklight::Catalog
       controller: "catalog",
       action: "index"
   end
+
+  def worldcat_isbnissn
+    @id = ActionController::Base.helpers.sanitize(params[:id])
+
+    redirect_to utf8: "✓",
+      q_row: ["#{@id}", ""],
+      op_row: ["AND", "AND"],
+      search_field_row: ["isbnissn", "all_fields"],
+      sort: "score desc, pub_date_sort desc, title_sort asc",
+      search_field: "advanced",
+      advanced_query: "yes",
+      commit: "Search",
+      controller: "catalog",
+      action: "index"
+    end
 
 protected
 
