83ed76eb8ecb095563ac4831beed275b3d60d1b6
James Reidy <jreidy@cornell.edu>
2024-05-29 12:50:38 -0400

    DACCESS-313 - increase timeout

--- blacklight-cornell/features/step_definitions/single_search/search_steps.rb
+++ blacklight-cornell/features/step_definitions/single_search/search_steps.rb
@@ -44,7 +44,7 @@ end
 Then("Digital Collections should list {string}") do |string|
   # don't require "Articles & Full Text" to be in second column
   digcoll = page.find("#digcoll", visible: false, wait: 10)
-  webpane = digcoll.find("div.web-pane", visible: false, wait: 10)
+  webpane = digcoll.find("div.web-pane", visible: false, wait: 20)
   within(webpane) do
     expect(page.first("h3.bento_item_title", :text => string, visible: false, wait: 10))
   end
