ba7698a87cd51603328fbd3edcfac395c1f0cb6a
James Reidy <jreidy@cornell.edu>
2024-05-29 09:16:39 -0400

    DACCESS-313 - try uninstall concurrent-ruby

--- jenkins/prelude.sh
+++ jenkins/prelude.sh
@@ -6,9 +6,11 @@ echo "**************************************************************************
 echo ""
 source jenkins/environment.sh
 gem install bundler -v 2.3.26
+gem uninstall concurrent-ruby
 cd blacklight-cornell
 bundle update blacklight_unapi blacklight_cornell_requests my_account sqlite3
 bundle install
+bundle info concurrent-ruby
 RAILS_ENV=test bin/rake db:migrate
 brakeman --fast  -o brakeman-output.json
 echo $RAILS_ENV
