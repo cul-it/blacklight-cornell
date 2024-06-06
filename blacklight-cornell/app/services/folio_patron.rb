c6200ed2ca7a99c9aaf37dc9b65b978865e96c96
James Reidy <jreidy@cornell.edu>
2024-06-06 13:41:33 -0400

    LP-995 - remove debugging

--- blacklight-cornell/app/services/folio_patron.rb
+++ blacklight-cornell/app/services/folio_patron.rb
@@ -22,17 +22,5 @@ class FolioPatron < StatusPage::Services::Base
     rescue StandardError => e
       raise "Folio Patron: #{e.message}"
     end
-    #******************
-    save_level = Rails.logger.level; Rails.logger.level = Logger::WARN
-    jgr25_context = "#{__FILE__}:#{__LINE__}"
-    Rails.logger.warn "jgr25_log\n#{jgr25_context}:"
-    msg = [" #{__method__} ".center(60, "Z")]
-    msg << jgr25_context
-    msg << "response: " + response.inspect
-    msg << "Z" * 60
-    msg.each { |x| puts "ZZZ " + x.to_yaml }
-    Rails.logger.level = save_level
-    #binding.pry
-    #*******************
   end
 end
