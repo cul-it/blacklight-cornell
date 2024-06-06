cd1ce09dd422cd7eee492b87449951abb9104793
James Reidy <jreidy@cornell.edu>
2024-06-06 16:04:28 -0400

    LP-995 - simplify catalog solr status test

--- blacklight-cornell/app/services/catalog_solr.rb
+++ blacklight-cornell/app/services/catalog_solr.rb
@@ -1,35 +1,31 @@
-require 'net/http'
+require "net/http"
 
 class CatalogSolr < StatusPage::Services::Base
   def check!
-    solr_config = YAML.load(ERB.new(File.read("#{::Rails.root}/config/blacklight.yml")).result)
-    uri = solr_config[ENV['RAILS_ENV']]["url"] unless SOLR_CONFIG.nil? or SOLR_CONFIG[ENV['RAILS_ENV']].nil?
-
-    # Parse the uri and replace the path and query
-    uri = URI(uri)
-    uri.path = '/solr/admin/cores'
-    uri.query = 'action=STATUS&indexInfo=false'
-
     begin
+      solr_config = YAML.load(ERB.new(File.read("#{::Rails.root}/config/blacklight.yml")).result)
+      uri = solr_config[ENV["RAILS_ENV"]]["url"] unless SOLR_CONFIG.nil? or SOLR_CONFIG[ENV["RAILS_ENV"]].nil?
+
+      # Parse the uri and replace the path and query
+      uri = URI(uri)
+      uri.path = "/solr/admin/cores"
+      uri.query = "action=STATUS&indexInfo=false"
+
       response = Net::HTTP.get_response(uri)
     rescue SocketError => e
       raise "SocketError occurred"
-    rescue URI::InvalidURIError => e
-      raise "Invalid URL"
-    rescue Net::OpenTimeout, Net::ReadTimeout => e
-      raise "Network timeout occurred"
-    rescue StandardError => e
-      raise "An error occurred while checking Solr"
+    rescue SocketError, URI::InvalidURIError, Net::OpenTimeout, Net::ReadTimeout, StandardError => e
+      raise e
     else
-        begin
-            parsed_response = JSON.parse(response.body)
-        rescue JSON::ParserError => e
-            raise "Failed to parse JSON response"
-        end
-        if parsed_response.nil? || parsed_response.empty?
-            # Handle empty response
-            raise "Received an empty response"
-        end
+      begin
+        parsed_response = JSON.parse(response.body)
+      rescue JSON::ParserError => e
+        raise e
+      end
+
+      if parsed_response.nil? || parsed_response.empty?
+        raise StandardError.new("Received an empty response")
+      end
     end
   end
 end
