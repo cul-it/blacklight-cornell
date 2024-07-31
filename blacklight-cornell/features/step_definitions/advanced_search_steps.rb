9ee74a8db19cc74c2af8028c402cddaff6075ee5
James Reidy <jreidy@cornell.edu>
2024-07-31 10:51:30 -0400

    DACCESS-332 - whitespace and quotes

--- blacklight-cornell/features/step_definitions/advanced_search_steps.rb
+++ blacklight-cornell/features/step_definitions/advanced_search_steps.rb
@@ -15,24 +15,24 @@ When("I use {string} with {string} logic for field {string} on line {int} of adv
     fid = "search_field_advanced#{line}"
   end
   page.find_by_id(id).set("#{query}")
-  page.find_by_id(op).select( "#{logic}")
-  page.find_by_id(fid).select( "#{field}")
+  page.find_by_id(op).select("#{logic}")
+  page.find_by_id(fid).select("#{field}")
 end
 
 When("I view the {string} version of the search results") do |format|
   # Get the current URL
   uri = URI.parse(current_url)
 
-  atom_formats = ['xml', 'dc_xml', 'oai_dc_xml', 'ris', 'mendeley', 'zotero', 'rdf_zotero']
+  atom_formats = ["xml", "dc_xml", "oai_dc_xml", "ris", "mendeley", "zotero", "rdf_zotero"]
 
   if atom_formats.include? format
-    uri.path = '/catalog.atom'
+    uri.path = "/catalog.atom"
     params = CGI.parse(uri.query.to_s)
-    params['content_format'] = format
+    params["content_format"] = format
     uri.query = URI.encode_www_form(params)
   else
     # Substitute 'catalog' with 'catalog.<format>'
-    uri.path = '/catalog.' + format
+    uri.path = "/catalog." + format
   end
   new_url = uri.to_s
 
