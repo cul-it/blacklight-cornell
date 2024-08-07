f514a24794036f0ded752396976dda58d4cc29b8
Matthew Connolly <mjc12@cornell.edu>
2024-08-07 14:06:30 -0400

    Refactor headers and remove comments

--- blacklight-cornell/app/controllers/aeon_controller.rb
+++ blacklight-cornell/app/controllers/aeon_controller.rb
@@ -70,7 +70,6 @@ class AeonController < ApplicationController
     '<a href="https://www.library.cornell.edu/libraries/rmc">open</a>.'
     @review_text = 'Keep this request saved in your account for later review. ' \
     'It will not be sent to library staff for fulfillment.'
-    @quest_text = 'Please email <a href=mailto:rareref@cornell.edu>rareref@cornell.edu</a> if you have any questions.'
   end
 
   def set_scan_instance_variables
@@ -97,27 +96,8 @@ class AeonController < ApplicationController
     @warning = warning(@title)
   end
 
-  # def selecter
-  #   '
-  #     <div id="shoppingcart">
-  #     <span id="numitems">Number of items selected:</span>
-  #     <span id="num-selections-wrapper">
-  #     <span id="num-selections">
-  #     </span>
-  #     </span>
-
-  #     <div id="selections-wrapper">
-  #     <ol><div id="selections"></div>
-  #     </ol>
-  #     </div>
-  #     </div>
-  #   '
-  # end
-
   def loginurl
     '/aeon/aeon_login'
-    # 	return "http://dev-jac2445.library.cornell.edu/aeon511/aeon-login.php"
-    # 	return "http://voy-api.library.cornell.edu/aeon/aeon_test-login.php"
   end
 
   def warning(title)
@@ -128,89 +108,9 @@ class AeonController < ApplicationController
     end
   end
 
-  # def clearer
-  #   '
-  #     <div class="control-group">
-  #     <label class="control-label sr-only" for="SubmitButton">Submit request</label>
-  #     <input type="submit" class="btn btn-dark" id="SubmitButton" name="SubmitButton" value="Submit Request">
-  #     <label class="control-label sr-only" for="clear">Clear</label>
-  #     <input type="button" class="btn btn-secondary" id="clear"  name="clear" value="Clear Form">
-  #     <br/>' + @quest_text + '<br/>
-  #     </div>
-  #   '
-  # end
-
-  # def former
-  #   '</form>'
-  # end
-
-  # def submitter
-  #   ''
-  # end
-
-  # def xsubmitter
-  #   '
-  #       <div class="control-group">
-  #       <div class="controls">
-  #       <label class="control-label sr-only" for="SubmitButton">Submit request</label>
-  #       <input type="submit" class="btn" id="SubmitButton" name="SubmitButton" value="Submit Request">
-  #       </div>
-  #       </div>
-  #   '
-  # end
-
   def login
     'woops'
   end
-
-  # redirect_shib is redefined later in the class.
-  # def redirect_shib
-  #   redirect_to 'https://rmc-aeon.library.cornell.edu'
-  # end
-
-  # NOTE: This function doesn't seem to do anything useful - it always returns a static string for bibdata_output_hash,
-  # and that string doesn't appear to be used anywhere else in the code.
-  # def make_bibdata(document)
-  #   holding_id = ''
-  #   publisher = document['publisher_display'][0] || ''
-  #   pubdate = document['pub_date_display'][0] || ''
-  #   pubplace = document['pubplace_display'][0] || ''
-  #   holdings_json = Hash(JSON.parse(document['holdings_json']))
-
-  #   firstkeyout = ''
-  #   count = 0
-  #   bibdata_output_hash = '{"items": [{"author":null,"title":null,"pub_place":null,"publisher":null,"publisher_date":null,"edition":null,"bib_format":null,"permlocation":null,"permlocationcode":null,"holdings":[]}]}'
-  #   if !document['items_json'].nil?
-  #     bibdata_hash = Hash(JSON.parse(document['items_json']))
-  #     bibdata_hash.each do | firstKey, value |
-  #       if count == 0
-  #         firstkeyout = firstKey
-  #         count = count + 1
-  #         valueHash = Hash(value[0])
-  #         # 	        bibdata_output_hash = bibdata_output_hash + firstkeyout + '":['
-  #       end
-  #     end
-  #   end
-  #   if firstkeyout != ''
-  #     callnum = holdings_json[firstkeyout]['call']
-  #   else
-  #     callnum = ''
-  #   end
-
-  #   if !document['items_json'].nil?
-  #     bibdata_hash.each do | key, value |
-  #       #	if count == 0
-  #       holding_id = key
-  #       valueArray = value.to_a
-  #       valueArray.each do | key, hold |
-  #       valueHash = Hash(hold)
-  #       keyout = Hash[key]
-  #     end
-  #   end
-  # end
-
-  # return bibdata_output_hash
-  # end
 
   def aeon_login
     params
