6f3801f44e4e5c13e892ff69690c2c3eab788000
Matthew Connolly <mjc12@cornell.edu>
2024-08-13 16:38:33 -0400

    Further refactoring

--- blacklight-cornell/app/assets/javascripts/aeon/request.js
+++ blacklight-cornell/app/assets/javascripts/aeon/request.js
@@ -1,174 +1,180 @@
-/*
-* Schedule Date date picker
-*/
-
-(function () {
-  "use strict";
-
-  function clearForm (event) {
+/**
+ * Clears the form by resetting the form fields and removing the selected items.
+ * @param {Event} event - The event object.
+ */
+function clearForm(event) {
   $("form")[0].reset();
   $("#num-selections").text('0');
   $("#selections").html('');
-  }
-   
-  function buildRequestForItem(_, element) {
-    const req = $(element).attr('name');
-    if ($(element).is(':checked')) {
-      const {
-        enumeration = '',
-        barcode = '',
-        chron = '',
-        cslocation = '',
-        callnumber = '',
-        copy = '',
-        Restrictions = ''
-      } = itemdata[req] ?? {};
-      const en = `${enumeration} ${chron}`;
-      const fields = [
-        { name: "Request[]", value: req },
-        { name: `ItemVolume_${req}`, value: en },
-        { name: `Location_${req}`, value: cslocation },
-        { name: `CallNumber_${req}`, value: callnumber },
-        { name: `Restrictions_${req}`, value: Restrictions },
-        { name: `ItemInfo1_${req}`, value: chron },
-        { name: `ItemNumber_${req}`, value: barcode },
-        { name: `ItemIssue_${req}`, value: copy }
-      ];
-  
-      fields.forEach(field => {
-        $("#EADRequest").append(`<input type="hidden" name="${field.name}" value="${field.value}">`);
-      });
-    }
-  }
+}
 
-  function doSubmit(_) {
-    const numSelected = $("#num-selections").text();
-    const docType = $("#DocumentType").val();
-
-    if (!numSelected || numSelected == "0") {
-      alert("Please select an item or items for your request.");
-      return false;
-    }
-
-    if (docType == 'Manuscript' || numSelected > 1)  {
-       $("#AeonForm").val('EADRequest'); 
-       $('.ItemNo').each(buildRequestForItem);
-       const fixed = ["ItemTitle","ItemInfo3",
-        "ItemPublisher","ItemPlace","ItemDate",
-        "ItemEdition"];
-        for (let i=0;i<fixed.length;i++) {
-           $("#EADRequest").
-           append('<input type=hidden name="' + fixed[i] + '_FIXED" value="'
-           + $('#'+fixed[i]).val() + '">');
-        }
-        const fixed2 = ["SpecialRequest"]; 
-        for (let i=0;i<fixed2.length;i++) {
-            // first check to see if the field exists
-            if ($('#' + fixed2[i]).length) { 
-                $("#EADRequest").
-                append('<input type="hidden" name="' + fixed2[i] + '_FIXED" value="'
-                + $('#' + fixed2[i]).val() + '">');
-            }
-        }
-    }
-    if (docType === 'Photoduplication') {
-      const fields = ['SpecialRequest', 'ItemCitation', 'Notes', 'ServiceLevel', 'ShippingOption'];
-      
-      fields.forEach(field => {
-        const $element = $(`#${field}`);
-        if ($element.length) {
-          $("#RequestForm").append(`<input type="hidden" name="${field}" value="${$element.val()}">`);
-        }
-      });
-    }
-
-    return true;
-  }
-
-  /**
-  * doClick handles the click event on a checkbox
-  * 
-  * When a checkbox is checked or unchecked, the item is moved
-  * to the selected items box.
-  *
-  * This function is bound to the checkbox click event only if
-  * there are more than 1 items that can be selected
-  */
-  function doClick (event) {
-    const id = $(this).val();
+/**
+ * Builds the request for the selected item and appends the necessary hidden input fields to the form.
+ * @param {number} _ - The index of the element in the jQuery collection.
+ * @param {HTMLElement} element - The DOM element.
+ */
+function buildRequestForItem(_, element) {
+  const req = $(element).attr('name');
+  if ($(element).is(':checked')) {
     const {
-      location,
-      callnumber,
-      enumeration,
-      copy = '',
+      enumeration = '',
+      barcode = '',
       chron = '',
-      Restrictions
-    } = itemdata[id];
-    const {
-      publisher,
-      publisher_date,
-      pub_place,
-      edition
-    } = bibdata.items[0];
+      cslocation = '',
+      callnumber = '',
+      copy = '',
+      Restrictions = ''
+    } = itemdata[req] ?? {};
+    const en = `${enumeration} ${chron}`;
+    const fields = [
+      { name: "Request[]", value: req },
+      { name: `ItemVolume_${req}`, value: enumeration },
+      { name: `Location_${req}`, value: cslocation },
+      { name: `CallNumber_${req}`, value: callnumber },
+      { name: `Restrictions_${req}`, value: Restrictions },
+      { name: `ItemInfo1_${req}`, value: chron },
+      { name: `ItemNumber_${req}`, value: barcode },
+      { name: `ItemIssue_${req}`, value: copy }
+    ];
 
-    $("#ItemNumber").val(id); 
-    $("#Location").val(location); 
-    $("#CallNumber").val(callnumber); 
-    $("#ItemVolume").val(enumeration); 
-    $("#ItemPublisher").val(publisher); 
-    $("#ItemPlace").val(pub_place); 
-    $("#ItemDate").val(publisher_date); 
-    $("#ItemEdition").val(edition); 
-    $("#ItemIssue").val(copy);
-    $("#Restrictions").val(Restrictions); 
-  
-    if ($(this).is(":checked")) {
-      const remId= `tremid${id}`;
-      const remSpan = `<span id='${remId}'>&nbsp;<image src='/img/cross-small.png' alt='Remove'>&nbsp;</span>`;
-      const itemDiv = `<div id='t${id}'> <li>${callnumber} ${copy} ${enumeration} ${chron} ${remSpan}</li></div>`;
-      $("#selections").append(itemDiv);
+    fields.forEach(field => {
+      $("#EADRequest").append(`<input type="hidden" name="${field.name}" value="${field.value}">`);
+    });
+  }
+}
 
-      let numSelections = $("#num-selections").text();
-      $("#num-selections").text(++numSelections);
+/**
+ * Submits the form after performing necessary validations and appending hidden input fields based on the document type.
+ * @param {Event} _ - The event object.
+ * @returns {boolean} - Returns true if the form is successfully submitted, false otherwise.
+ */
+function doSubmit(_) {
+  const numSelected = $("#num-selections").text();
+  const docType = $("#DocumentType").val();
 
-      // Bind a click event to the remove icon to give us a way to remove the item from the list
-      $(`#${remId}`).click(() => {
-        $("#num-selections").text(--numSelections);
-        $(`#t${id}`).remove();
-        $(`#${id}`).prop('checked',false); 
-      });
-    } else {
-      let numSelections = $("#num-selections").text();
+  if (!numSelected || numSelected == "0") {
+    alert("Please select an item or items for your request.");
+    return false;
+  }
+
+  if (docType === 'Manuscript' || numSelected > 1) {
+    $("#AeonForm").val('EADRequest');
+    $('.ItemNo').each(buildRequestForItem);
+
+    const fixed = ["ItemTitle", "ItemInfo3", "ItemPublisher", "ItemPlace", "ItemDate", "ItemEdition"];
+    fixed.forEach(item => {
+      $("#EADRequest").append(`<input type="hidden" name="${item}_FIXED" value="${$('#' + item).val()}">`);
+    });
+
+    const fixed2 = ["SpecialRequest"];
+    fixed2.forEach(item => {
+      if ($('#' + item).length) {
+        $("#EADRequest").append(`<input type="hidden" name="${item}_FIXED" value="${$('#' + item).val()}">`);
+      }
+    });
+  }
+
+  if (docType === 'Photoduplication') {
+    const fields = ['SpecialRequest', 'ItemCitation', 'Notes', 'ServiceLevel', 'ShippingOption'];
+
+    fields.forEach(field => {
+      const $element = $(`#${field}`);
+      if ($element.length) {
+        $("#RequestForm").append(`<input type="hidden" name="${field}" value="${$element.val()}">`);
+      }
+    });
+  }
+
+  return true;
+}
+
+/**
+ * Handles the click event on a checkbox.
+ * Moves the item to the selected items box when a checkbox is checked or unchecked.
+ * This function is bound to the checkbox click event only if there are more than 1 items that can be selected.
+ * @param {Event} event - The event object.
+ */
+function doClick(event) {
+  const id = $(this).val();
+  const {
+    location,
+    callnumber,
+    enumeration,
+    copy = '',
+    Restrictions
+  } = itemdata[id];
+  const {
+    publisher,
+    publisher_date,
+    pub_place,
+    edition
+  } = bibdata.items[0];
+
+  $("#ItemNumber").val(id);
+  $("#Location").val(location);
+  $("#CallNumber").val(callnumber);
+  $("#ItemVolume").val(enumeration);
+  $("#ItemPublisher").val(publisher);
+  $("#ItemPlace").val(pub_place);
+  $("#ItemDate").val(publisher_date);
+  $("#ItemEdition").val(edition);
+  $("#ItemIssue").val(copy);
+  $("#Restrictions").val(Restrictions);
+
+  if ($(this).is(":checked")) {
+    const remId = `tremid${id}`;
+    appendItemToSelection(id, true);
+
+    let numSelections = $("#num-selections").text();
+    $("#num-selections").text(++numSelections);
+
+    // Bind a click event to the remove icon to give us a way to remove the item from the list
+    $(`#${remId}`).click(() => {
       $("#num-selections").text(--numSelections);
       $(`#t${id}`).remove();
-    }
-  }
-
-  /**
-   * Sets up the click event handlers for the page
-   * and if there is only 1 item to select, it 
-   * will be selected by default.
-  */
-  $(document).ready(function () {
-    $("#num-selections").text('0');
-    $('#clear').click(clearForm);
-    $('#SubmitButton').click(doSubmit);
-    $('.ItemNo').each((_, element) => {
-      $(element).click(doClick);
+      $(`#${id}`).prop('checked', false);
     });
-    const n = $('.ItemNo').length;
-    if (n == 1) {
-      $('.ItemNo').each((_, obj) =>{
-        $(obj).click();
-        $(obj).unbind('click');
-        $("#num-selections").text('1');
-        var bc= $(obj).val();
-        var cn = itemdata[bc].callnumber; 
-        var en = itemdata[bc].enumeration; 
-        var co = itemdata[bc].copy?itemdata[bc].copy: ''; 
-        var cr = itemdata[bc].chron?itemdata[bc].chron: ''; 
-        $("#selections").append("<div id='t" + bc+"'> <li>"+" "+cn+" "+co+" "+en+" "+cr+"</li></div>");
-      });
-    }
-  });
-} ());
+  } else {
+    let numSelections = $("#num-selections").text();
+    $("#num-selections").text(--numSelections);
+    $(`#t${id}`).remove();
+  }
+}
+
+/**
+ * Appends the selected item to the selected items box.
+ * 
+ * @param {number} id - The item id.
+ * @param {boolean} removable - Whether the item can be removed from the selected items box.
+ */
+const appendItemToSelection = (id, removable) => {
+  const {
+    callnumber,
+    enumeration,
+    copy = '',
+    chron = '',
+  } = itemdata[id];
+  const remId = `tremid${id}`;
+  const remSpan = removable ? `<span id='${remId}'>&nbsp;<image src='/img/cross-small.png' alt='Remove'>&nbsp;</span>` : '';
+  const itemDiv = `<div id='t${id}'> <li>${callnumber} ${copy} ${enumeration} ${chron} ${remSpan}</li></div>`;
+  $("#selections").append(itemDiv);
+}
+
+/**
+ * Sets up the click event handlers for the page and selects the item by default if there is only 1 item to select.
+ */
+$(document).ready(function () {
+  $("#num-selections").text('0');
+  $('#clear').click(clearForm);
+  $('#SubmitButton').click(doSubmit);
+
+  if ($('.ItemNo').length === 1) {
+    const item = $('.ItemNo').first();
+    item.click().unbind('click');
+    $("#num-selections").text('1');
+    appendItemToSelection(item.val(), false);
+  }
+  else {
+    $('.ItemNo').each((_, element) => $(element).click(doClick));
+  }
+});
