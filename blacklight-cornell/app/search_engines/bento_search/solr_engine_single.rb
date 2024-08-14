ca029d3a896d7302bd1050184e7a1417d87f556d
James Reidy <jreidy@cornell.edu>
2024-08-14 13:16:06 -0400

    DACCESS-353 - whitespace

--- blacklight-cornell/app/search_engines/bento_search/solr_engine_single.rb
+++ blacklight-cornell/app/search_engines/bento_search/solr_engine_single.rb
@@ -26,19 +26,19 @@ class BentoSearch::SolrEngineSingle
     #solr = RSolr.connect :url => 'http://da-prod-solr1.library.cornell.edu/solr/blacklight'
     Rails.logger.debug("mjc12test: #{self.class.name} #{__FILE__} #{__LINE__} url is #{configuration.solr_url}")
     solr = RSolr.connect :url => configuration.solr_url
-    solr_response = solr.get 'select', :params => {
-                                        :q => q,
-                                        #:fq => "format:\"#{format}\"",
-                                       # :rows => args[:per_page],
-                                        :rows => 20, # from sample single query; should set this dynamically?
-                                        :group => true,
-                                        'group.field' => 'format_main_facet',
-                                        'group.limit' => 3,
-                                        'group.ngroups' => 'true',
-                                        :sort => 'score desc, pub_date_sort desc, title_sort asc',
-                                        :fl => 'id,pub_date_display,format,fulltitle_display,fulltitle_vern_display,author_display,score,pub_info_display,availability_json',
-                                        :mm => 1
-                                        #:defType => 'edismax'
+    solr_response = solr.get "select", :params => {
+                                         :q => q,
+                                         #:fq => "format:\"#{format}\"",
+                                         # :rows => args[:per_page],
+                                         :rows => 20, # from sample single query; should set this dynamically?
+                                         :group => true,
+                                         "group.field" => "format_main_facet",
+                                         "group.limit" => 3,
+                                         "group.ngroups" => "true",
+                                         :sort => "score desc, pub_date_sort desc, title_sort asc",
+                                         :fl => "id,pub_date_display,format,fulltitle_display,fulltitle_vern_display,author_display,score,pub_info_display,availability_json",
+                                         :mm => 1,
+                                       #:defType => 'edismax'
                                        }
 
     Rails.logger.debug("mjc12test: BlacklightEngine2 search called. #{__FILE__} #{__LINE__} solr_response #{solr_response}")
@@ -46,12 +46,9 @@ class BentoSearch::SolrEngineSingle
     # in order to have bento_search process it correctly. We'll split up into different facets
     # once we get back to the controller!
     result = BentoSearch::ResultItem.new
-    result.custom_data = solr_response['grouped']['format_main_facet']['groups']
+    result.custom_data = solr_response["grouped"]["format_main_facet"]["groups"]
     bento_results << result
     bento_results.total_items = 1
     return bento_results
-
   end
-
-
 end
