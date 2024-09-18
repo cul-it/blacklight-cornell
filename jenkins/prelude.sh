b0271ef8f2940d108526dffb70ed5286c2ce2d67
James Reidy <jreidy@cornell.edu>
2024-09-18 17:20:35 -0400

    DIGCOLL-380 - clear gems from cache

--- jenkins/prelude.sh
+++ jenkins/prelude.sh
@@ -21,6 +21,10 @@ export PATH=$GEM_HOME/bin:$PATH
 which bundle
 bundle --version
 
+# Clear cached gems in RVM
+rvm gemset empty default
+rm -rf $rvm_path/gem-cache
+
 bundle update --redownload blacklight_unapi blacklight_cornell_requests my_account sqlite3
 bundle install
 bundle info concurrent-ruby
