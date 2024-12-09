af26e7bc3f6830425c81cc509b13dbaa0630b9c1
Jeremy Duncan <jpd294@cornell.edu>
2024-12-09 10:27:23 -0500

    fix: made config changes

--- blacklight-cornell/spec/rails_helper.rb
+++ blacklight-cornell/spec/rails_helper.rb
@@ -2,7 +2,8 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-require_relative '../features/support/selenium'
+# require_relative '../features/support/selenium'
+require_relative 'support/selenium'
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
