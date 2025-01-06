56d1e37037901c06b6f74d20a0fced014f9f15ff
Jeremy Duncan <jpd294@cornell.edu>
2025-01-06 13:49:55 -0500

    fix: updated jenkins scripts

--- jenkins/rspec.sh
+++ jenkins/rspec.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
+
 echo ""
-echo "*********************************************************************************"
+echo "##########################################################################"
+echo "Running rspec tests..."
+echo "##########################################################################"
 echo ""
+
 source jenkins/environment.sh
 cd blacklight-cornell
 echo "PATH is:$PATH"
@@ -10,6 +14,20 @@ echo "Solr: $SOLR_URL"
 which bundle
 export COVERAGE=on
 export RAILS_ENV=test
+
+echo ""
+echo "##########################################################################"
+echo "Google Chrome version..."
+which google-chrome
+echo "##########################################################################"
+echo ""
+echo ""
+echo "##########################################################################"
+echo "Chrome Driver..."
+which chromedriver
+echo "##########################################################################"
+echo ""
+
 if [ $# -eq 0 ]
     then
         echo "Running all rspec tests."
