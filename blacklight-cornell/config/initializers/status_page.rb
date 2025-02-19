ab31ac4bac41c35cfde1506d3c1788da2b7c91d5
Jeremy Duncan <jpd294@cornell.edu>
2025-02-19 15:03:52 -0500

    feature-status- refactored name

--- blacklight-cornell/config/initializers/status_page.rb
+++ blacklight-cornell/config/initializers/status_page.rb
@@ -1,13 +1,10 @@
-StatusPage.configure do
-  # Cache check status result 10 seconds
-  self.interval = 10
-  # Use service
-  self.use :database
-  self.use :cache
-  self.add_custom_service(CatalogSolr)
-  self.add_custom_service(RepositoriesSolr)
-  # self.add_custom_service(FolioPatron)
-  # self.add_custom_service(IlliadTransactions)
-  # self.add_custom_service(ReshareStatus)
-  self.add_custom_service(MyAccountService)
-end
+  StatusPage.configure do
+    self.interval = 10
+    self.use :database
+    self.use :cache
+    self.add_custom_service(CatalogSolr)
+    self.add_custom_service(RepositoriesSolr)
+    self.add_custom_service(FolioPatron)
+    self.add_custom_service(IlliadStatus)
+    self.add_custom_service(ReshareStatus)
+  end
