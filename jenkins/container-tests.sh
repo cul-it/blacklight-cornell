763dd68a3c13b713062d37f457bb7ffc82749689
Shinwoo Kim <sk274@cornell.edu>
2025-02-20 13:25:22 -0500

    Fix chrome path

--- jenkins/container-tests.sh
+++ jenkins/container-tests.sh
@@ -11,15 +11,16 @@ cp /cul/data/jenkins/environments/blacklight-cornell.env container_env_test.env
 img_id=$(git rev-parse HEAD)
 img=092831676293.dkr.ecr.us-east-1.amazonaws.com/da/blacklight-cornell-prod:${img_id} 
 
-./build_test.sh
 export COVERAGE=on
 export RAILS_ENV_FILE=./container_env_test.env
 export COVERAGE_PATH=${JENKINS_WORKSPACE}/blacklight-cornell/coverage
 project_name="container-discovery-test-$(openssl rand -hex 8)"
+./build_test.sh
 docker compose -p $project_name -f docker-compose-test.yaml up
 EXIT_CODE=$?
 echo $EXIT_CODE
 export USE_RSPEC=1
+unset FEATURE
 docker compose -p $project_name -f docker-compose-test.yaml up
 R_EXIT_CODE=$?
 echo $R_EXIT_CODE
