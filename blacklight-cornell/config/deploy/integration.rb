134ec83cb64bc46e00b21d6a0e2b2471700429c3
Jeremy Duncan <jpd294@cornell.edu>
2025-04-29 14:27:23 -0400

    fix: updated capistrano

--- blacklight-cornell/config/deploy/integration.rb
+++ blacklight-cornell/config/deploy/integration.rb
@@ -4,23 +4,26 @@ server 'aws-108-077.internal.library.cornell.edu', roles: %w{app web db}, user: 
 
 set :deploy_to, "/cul/web/catalog-int.library.cornell.edu/rails-app"
 
-set :branch, ENV['BRANCH'].gsub("origin/","") if ENV['BRANCH']
+set :branch, ENV['BRANCH'].gsub("origin/", "") if ENV['BRANCH']
 
 set :rails_env, 'integration'
 
+# ==============================================================================
+# Uninstall any precompiled nokogiri binary and reinstall from source manually
+# ------------------------------------------------------------------------------
 namespace :deploy do
   task :uninstall_and_reinstall_nokogiri do
     on roles(:app) do
       within release_path do
-        # Uninstall the existing Nokogiri gem
+        # Uninstall all versions and ignore failure if none is present
         execute :gem, 'uninstall nokogiri -a -x -I || true'
 
-        # Install Nokogiri from source with the ruby platform
-        execute :gem, 'install nokogiri -v 1.16.8 --platform=ruby'
+        # Reinstall Nokogiri from source, pinned to specific version
+        execute :gem, 'install nokogiri -v 1.16.8 --platform=ruby -- --use-system-libraries'
       end
     end
   end
 
-  # Hook this task into the deploy process (after bundling)
-  after 'deploy:updated', 'deploy:uninstall_and_reinstall_nokogiri'
+  # Run this right before asset precompilation
+  before 'deploy:assets:precompile', 'deploy:uninstall_and_reinstall_nokogiri'
 end
