c42bfc620f3f1845139b3d32b6a21f49cfe49546
Jeremy Duncan <jpd294@cornell.edu>
2025-04-29 11:29:30 -0400

    fix: added method to capistrano

--- blacklight-cornell/config/deploy/integration.rb
+++ blacklight-cornell/config/deploy/integration.rb
@@ -9,9 +9,11 @@ set :branch, ENV['BRANCH'].gsub("origin/","") if ENV['BRANCH']
 set :rails_env, 'integration'
 
 # ==============================================================================
-# Force Nokogiri to build from source instead of using precompiled binaries
+# Force Nokogiri and other gems to build from source instead of using binaries
+# and limit make parallelism to avoid "Argument list too long" errors
 # ------------------------------------------------------------------------------
 before 'bundler:install', 'deploy:set_force_ruby_platform'
+before 'bundler:install', 'deploy:set_makeflags_serial'
 
 namespace :deploy do
   task :set_force_ruby_platform do
@@ -19,6 +21,15 @@ namespace :deploy do
       info "👉 Setting Bundler to force Ruby platform on #{host.hostname}..."
       execute :bundle, "config set force_ruby_platform true"
       info "✅ Bundler config force_ruby_platform set successfully."
+    end
+  end
+
+  task :set_makeflags_serial do
+    on roles(:app) do
+      info "👉 Setting MAKEFLAGS=-j1 to prevent make overflow on #{host.hostname}..."
+      execute "echo 'export MAKEFLAGS=-j1' >> ~/.bash_profile"
+      execute "source ~/.bash_profile"
+      info "✅ MAKEFLAGS environment variable set successfully."
     end
   end
 end
