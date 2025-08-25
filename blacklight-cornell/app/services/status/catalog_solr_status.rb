ab0965ef5a7c902e6471c178c4635d07b4208b17
Jeremy Duncan <jpd294@cornell.edu>
2025-08-25 09:17:58 -0400

    updated to check solr_config

--- blacklight-cornell/app/services/status/catalog_solr_status.rb
+++ blacklight-cornell/app/services/status/catalog_solr_status.rb
@@ -5,7 +5,7 @@ module Status
     def check!
       begin
         solr_config = YAML.load(ERB.new(File.read("#{::Rails.root}/config/blacklight.yml")).result)
-        uri = solr_config[ENV["RAILS_ENV"]]["url"] unless SOLR_CONFIG.nil? or SOLR_CONFIG[ENV["RAILS_ENV"]].nil?
+        uri = solr_config[ENV["RAILS_ENV"]]["url"] unless solr_config.nil? or solr_config[ENV["RAILS_ENV"]].nil?
 
         # Parse the uri and replace the path and query
         uri = URI(uri)
