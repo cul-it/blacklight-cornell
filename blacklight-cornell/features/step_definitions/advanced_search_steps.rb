5217df7f199231e9d881dc0c7805aafaa9178951
Jeremy Duncan <jpd294@cornell.edu>
2025-10-10 10:58:01 -0400

    DACCESS-594: added cucumber tests

--- blacklight-cornell/features/step_definitions/advanced_search_steps.rb
+++ blacklight-cornell/features/step_definitions/advanced_search_steps.rb
@@ -41,3 +41,67 @@ Then("the solr query should contain {string}") do |string|
   sq = page.find("dl#solr-query-display > dd").text
   expect(sq).to match(string)
 end
+
+
+#######################################-----------------------------------------
+##  Steps for Year Range validation  ##
+#######################################
+When("I focus the start year field") do
+  find('#range_pub_date_facet_begin, [data-date-start]', match: :first, visible: :all).click
+end
+
+When("I focus the end year field") do
+  find('#range_pub_date_facet_end, [data-date-end]', match: :first, visible: :all).click
+end
+
+When("I type {string} into the focused field") do |text|
+  active = page.driver.browser.switch_to.active_element
+  active.send_keys(text) #simulate incremental typing ('300' before '3000').
+end
+
+When("Leave from the date range section of the form") do
+  2.times { page.driver.browser.switch_to.active_element.send_keys(:tab) }
+end
+
+Then("the date range alert should be hidden") do
+  if page.has_css?('#date-range-error')
+    expect(page).to have_css('#date-range-error', visible: :hidden)
+  end
+end
+
+Then("the date range alert should be visible with message {string}") do |msg|
+  using_wait_time 3 do
+    expect(page).to have_css('#date-range-error')
+  end
+  within('#date-range-error') do
+    expect(page).to have_css('.msg', text: msg)
+  end
+  expect(page).to have_css('#date-range-error', visible: :visible)
+end
+
+Then("the advanced search submit button should be disabled") do
+  btn = find('#advanced_search', visible: :all)
+  disabled_attr = btn[:disabled]
+  expect(disabled_attr).to be_truthy
+  expect(btn[:class].to_s).to include('is-disabled')
+end
+
+Then("the advanced search submit button should be enabled") do
+  using_wait_time 3 do
+    expect(page.evaluate_script("document.querySelector('#advanced_search')?.disabled === false")).to be true
+  end
+end
+
+Then("the start year field should be invalid") do
+  el = find('#range_pub_date_facet_begin, [data-date-start]', match: :first, visible: :all)
+  expect(el[:class].to_s).to include('is-invalid')
+  expect(el['aria-invalid']).to eq('true')
+  expect(el[:title].to_s.strip).not_to eq('')
+end
+
+Then("the end year field should be invalid") do
+  el = find('#range_pub_date_facet_end, [data-date-end]', match: :first, visible: :all)
+  expect(el[:class].to_s).to include('is-invalid')
+  expect(el['aria-invalid']).to eq('true')
+  expect(el[:title].to_s.strip).not_to eq('')
+end
