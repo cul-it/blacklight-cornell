f37da6a9f29047e9f2da9ff04b0aa9f17af04ce6
Jeremy Duncan <jpd294@cornell.edu>
2025-12-02 11:47:46 -0500

    DACCESS-630: cleaned up code, updated comments, removed old code

--- blacklight-cornell/app/controllers/catalog_controller.rb
+++ blacklight-cornell/app/controllers/catalog_controller.rb
@@ -870,20 +870,11 @@ class CatalogController < ApplicationController
         end
       end
     end
-end
+  end
 
-  ##############################################################################
-  ## TOU FOR FOLIO SESSION  ####################################################
-  ##############################################################################
   def tou
-    # service = TouLookupService.new(session: session)
     service = TouLookupService.new
-
-    r = service.resolve_catalog_terms_of_use(
-      id: params[:id],
-      dbcode: params[:dbcode],
-      providercode: params[:providercode]
-    )
+    r = service.resolve_catalog_terms_of_use(id: params[:id], dbcode: params[:dbcode], providercode: params[:providercode])
     @dbResponse        = r[:db_response]
     @db                = r[:db]
     @dbResponse2       = r[:db_response2]
@@ -896,65 +887,9 @@ end
 
   # TODO: mjc12: I don't understand why we have two functions for TOU: tou and new_tou. The former gets TOU info from
   def new_tou
-    # service = TouLookupService.new(session: session)
     service = TouLookupService.new
-
     r = service.resolve_new_tou(title_id: params[:title_id], id: params[:id])
     @newTouResult = r[:new_tou_result]
-  end
-
-  def eholdings_record(id)
-    # eholdings title JSON response described here:
-    # https://s3.amazonaws.com/foliodocs/api/mod-kb-ebsco-java/r/titles.html#eholdings_titles_get
-    folio_request("#{ENV['OKAPI_URL']}/eholdings/titles/#{id}?include=resources")
-  end
-
-  # Make a FOLIO request to retrieve an array of subscription agreements linked to an e-holdings record
-  # specified by id.
-  def subscription_agreements(id)
-    folio_request("#{ENV['OKAPI_URL']}/erm/sas?filters=items.reference=#{id}&sort=startDate:desc")
-  end
-
-  # Make a FOLIO request to retrieve a license object linked to an e-holdings record
-  # specified by id ('remoteId' in the JSON).
-  def license(id)
-    folio_request("#{ENV['OKAPI_URL']}/licenses/licenses/#{id}")
-  end
-
-  # Given a URL, make a FOLIO request and return the results (or nil in case of a RestClient exception).
-  def folio_request(url)
-    token = folio_token
-    if url && token
-      headers = {
-        'X-Okapi-Tenant' => ENV['OKAPI_TENANT'],
-        'x-okapi-token' => token,
-        :accept => 'application/json, application/vnd.api+json'
-      }
-      response = RestClient.get(url, headers)
-      JSON.parse(response.body) if response && response.code == 200
-    end
-  rescue RestClient::ExceptionWithResponse => err
-    Rails.logger.error "TOU: Error making FOLIO request (#{err})"
-    nil
-  end
-
-  # Return a FOLIO authentication token for API calls -- either from the session if a token
-  # was prevoiusly created, or directly from FOLIO otherwise.
-  #
-  # TODO: Caching is being disabled for now, since it's causing problems with the new expiring
-  # token mechanism in FOLIO. We need to figure out how to cache the token properly. (mjc12)
-  def folio_token
-   #  if session[:folio_token].nil?
-      url = ENV['OKAPI_URL']
-      tenant = ENV['OKAPI_TENANT']
-      response = CUL::FOLIO::Edge.authenticate(url, tenant, ENV['OKAPI_USER'], ENV['OKAPI_PW'])
-      if response[:code] >= 300
-        Rails.logger.error "TOU error: Could not create a FOLIO token for #{user}"
-      else
-        session[:folio_token] = response[:token]
-      end
-   #  end
-    session[:folio_token]
   end
 
   def redirect_browse
--- blacklight-cornell/app/services/tou_lookup_service.rb
+++ blacklight-cornell/app/services/tou_lookup_service.rb
@@ -1,3 +1,6 @@
+################################################################################
+##  Process TOU requests for Folio and Solr  ###################################
+################################################################################
 class TouLookupService
   def initialize(
     solr: Blacklight.default_index.connection,
@@ -12,7 +15,9 @@ class TouLookupService
     @token_provider = token_provider || FolioTokenProvider.new(session: session, logger: logger)
   end
 
-  # Builds the "new TOU" payload with FOLIO licenses plus database TOU fallbacks.
+  # ============================================================================
+  # Builds the "new TOU" payload with FOLIO licenses plus database TOU fallbacks
+  # ----------------------------------------------------------------------------
   def resolve_new_tou(title_id:, id:)
     new_tou_result = []
     record         = eholdings_record(title_id) || {}
@@ -58,23 +63,35 @@ class TouLookupService
       columns: @erm_model.column_names.map(&:to_sym)
     }
   end
-
+  # ============================================================================
   # Fetches an e-holdings title record (with resources included) from FOLIO.
+  # eholdings title JSON response described here:
+  # https://s3.amazonaws.com/foliodocs/api/mod-kb-ebsco-java/r/titles.html#eholdings_titles_get
+  # ----------------------------------------------------------------------------
   def eholdings_record(title_id)
     folio_request("#{ENV['OKAPI_URL']}/eholdings/titles/#{title_id}?include=resources")
   end
 
-  # Fetches subscription agreements linked to a specific package.
+  # =========================================================
+  # Make a FOLIO request to retrieve an array of subscription
+  # agreements linked to an e-holdings record specified by id
+  # ---------------------------------------------------------
   def subscription_agreements(package_id)
     folio_request("#{ENV['OKAPI_URL']}/erm/sas?filters=items.reference=#{package_id}&sort=startDate:desc")
   end
 
-  # Fetches a single license by remoteId from FOLIO licenses app.
+  # =========================================================================
+  # Make a FOLIO request to retrieve a license object linked to an e-holdings
+  # record specified by 'remoteId' in the JSON
+  # -------------------------------------------------------------------------
   def license(remote_id)
     folio_request("#{ENV['OKAPI_URL']}/licenses/licenses/#{remote_id}")
   end
 
-  # Performs a GET to a FOLIO endpoint with Okapi headers.
+  # ========================================================
+  # Given a URL, make a FOLIO request and return the results
+  # (or nil in case of a RestClient exception)
+  # --------------------------------------------------------
   def folio_request(url)
     tok = @token_provider.fetch
     return nil unless url && tok
@@ -107,54 +124,10 @@ class TouLookupService
     nil
   end
 
-  # Provides cached FOLIO token management using the Rails session.
-  class FolioTokenProvider
-    def initialize(session:, logger: Rails.logger)
-      @session = session
-      @logger  = logger
-    end
-
-    # Returns a valid FOLIO token, caching it in the session when possible.
-    def fetch
-      if @session
-        current_session_token = @session[:folio_token]
-        session_token_exp     = @session[:folio_token_expires_at]
-        current_time          = Time.now.to_i
-        return current_session_token if current_session_token && session_token_exp && session_token_exp > current_time
-      end
-
-      url    = ENV['OKAPI_URL']
-      tenant = ENV['OKAPI_TENANT']
-      resp   = CUL::FOLIO::Edge.authenticate(url, tenant, ENV['OKAPI_USER'], ENV['OKAPI_PW'])
-
-      if resp[:code].to_i >= 300
-        @logger.error "FolioTokenProvider: auth failed (code=#{resp[:code]})"
-        return nil
-      end
-
-      token      = resp[:token]
-      ttl_secs   = Integer(ENV.fetch('FOLIO_TOKEN_TTL_SECONDS', 45 * 60)) rescue 2700
-      expires_at = Time.now.to_i + ttl_secs
-
-      if @session
-        @session[:folio_token]            = token
-        @session[:folio_token_expires_at] = expires_at
-      end
-
-      token
-    end
-
-    # Deletes any cached token so the next fetch will re-authenticate.
-    def invalidate
-      return unless @session
-
-      @session.delete(:folio_token)
-      @session.delete(:folio_token_expires_at)
-    end
-  end
-
-  # Resolve TOU using the "database" core by ID. Mirrors DatabasesController logic.
-  # Extracts db/provider codes and queries Erm_data with fallbacks.
+  # ==============================================================
+  # Resolve TOU using the "database" core by ID.
+  # Extracts db/provider codes and queries Erm_data with fallbacks
+  # --------------------------------------------------------------
   def resolve_for_database(id:)
     increment_metric('db_tou')
 
@@ -164,12 +137,12 @@ class TouLookupService
     doc = db_docs.first
     dbcode, providercode = pull_db_and_provider_codes(doc)
 
-    if blank?(dbcode)
+    if dbcode.blank?
       json = safe_parse_first_url_access_json(doc)
       return build_result(db_docs, [], 'Use default rights text') if json.nil?
 
       providercode = first_or_self(json['providercode'])
-      return build_result(db_docs, [], 'Use default rights text') if blank?(providercode)
+      return build_result(db_docs, [], 'Use default rights text') if providercode.blank?
 
       dbcode = json['dbcode']
       erm_records, default_text = query_erm_with_fallbacks(dbcode: dbcode, providercode: providercode)
@@ -181,7 +154,9 @@ class TouLookupService
     end
   end
 
-  # Resolve Terms of Use for CatalogController (termsOfUse path).
+  # ============================================================
+  # Resolve Terms of Use for CatalogController (termsOfUse path)
+  # ------------------------------------------------------------
   def resolve_catalog_terms_of_use(id:, dbcode:, providercode:)
     db_response = solr_get('termsOfUse', id: id)
     num_found   = db_response.dig('response', 'numFound').to_i
@@ -214,7 +189,7 @@ class TouLookupService
       link_provider = first_or_self(l['providercode'])
       link_dbcode   = l['dbcode']
 
-      if normalize(link_provider) == normalize(providercode) && normalize(link_dbcode) == normalize(dbcode)
+      if link_provider.to_s.strip == providercode.to_s.strip && link_dbcode.to_s.strip == dbcode.to_s.strip
         default = ''
         erm_recs, default = query_erm_for_terms_link(l)
         break
@@ -238,7 +213,9 @@ class TouLookupService
 
   private
 
-  # Solr GET wrapper with error capture.
+  # ===================================
+  # Solr GET wrapper with error capture
+  # -----------------------------------
   def solr_get(path, params = {})
     @solr.get(path, params: params)
   rescue => e
@@ -246,14 +223,18 @@ class TouLookupService
     {}
   end
 
-  # Pull dbcode/providercode from a database doc, handling arrays/blank values.
+  # ==========================================================================
+  # Pull dbcode/providercode from a database doc, handling arrays/blank values
+  # --------------------------------------------------------------------------
   def pull_db_and_provider_codes(doc)
     dbcode       = doc['dbcode']
     providercode = first_or_self(doc['providercode'])
     [dbcode, providercode]
   end
 
-  # Safely parse the first url_access_json entry if present; returns Hash or nil.
+  # ============================================================================
+  # Safely parse the first url_access_json entry if present; returns Hash or nil
+  # ----------------------------------------------------------------------------
   def safe_parse_first_url_access_json(doc)
     arr = doc['url_access_json']
     return nil unless arr.is_a?(Array) && arr.first.present?
@@ -261,14 +242,18 @@ class TouLookupService
     safe_json(arr.first)
   end
 
-  # JSON.parse with rescue.
+  # =======================
+  # JSON.parse with rescue
+  # -----------------------
   def safe_json(str)
     JSON.parse(str)
   rescue JSON::ParserError
     nil
   end
 
-  # Erm fallbacks for "database" path.
+  # =================================
+  # Erm fallbacks for "database" path
+  # ---------------------------------
   def query_erm_with_fallbacks(dbcode:, providercode:)
     records = @erm_model.where(Database_Code: dbcode, Provider_Code: providercode, Prevailing: 'true')
     if records.empty?
@@ -278,7 +263,9 @@ class TouLookupService
     [records, '']
   end
 
-  # Erm fallbacks for "termsOfUse" path (SSID-aware), descending specificity.
+  # ========================================================================
+  # Erm fallbacks for "termsOfUse" path (SSID-aware), descending specificity
+  # ------------------------------------------------------------------------
   def query_erm_for_terms_link(link_hash)
     pc  = first_or_self(link_hash['providercode'])
     dbc = link_hash['dbcode']
@@ -299,22 +286,16 @@ class TouLookupService
     [[], 'Use default rights text']
   end
 
-  # Normalize provider codes that can arrive as Arrays.
+  # ==================================================
+  # Normalize provider codes that can arrive as Arrays
+  # --------------------------------------------------
   def first_or_self(value)
     value.is_a?(Array) ? value.first : value
   end
 
-  # Small helper to normalize strings for equality checks.
-  def normalize(value)
-    value.to_s.strip
-  end
-
-  # Predicate for nil/empty strings.
-  def blank?(value)
-    value.nil? || value == ''
-  end
-
-  # Constructs a controller-friendly result hash. Avoid mutating frozen arrays.
+  # ==========================================================================
+  # Constructs a controller-friendly result hash. Avoid mutating frozen arrays
+  # --------------------------------------------------------------------------
   def build_result(db_docs, erm_records, default_text)
     {
       db_docs: db_docs,
@@ -324,10 +305,64 @@ class TouLookupService
     }
   end
 
-  # Increment a counter safely without hard dependency on Appsignal presence.
+  # ========================================================================
+  # Increment a counter safely without hard dependency on Appsignal presence
+  # ------------------------------------------------------------------------
   def increment_metric(name)
     Appsignal.increment_counter(name, 1) if defined?(Appsignal)
   rescue => e
     @logger.debug("TOU: metric #{name} not incremented: #{e}")
+  end
+
+  ##############################################################################
+  ##  Provides cached FOLIO token management using the Rails session.  #########
+  ##############################################################################
+  class FolioTokenProvider
+    def initialize(session:, logger: Rails.logger)
+      @session = session
+      @logger  = logger
+    end
+
+    # ====================================================================
+    # Returns a valid FOLIO token, caching it in the session when possible
+    # --------------------------------------------------------------------
+    def fetch
+      if @session
+        current_session_token = @session[:folio_token]
+        session_token_exp     = @session[:folio_token_expires_at]
+        current_time          = Time.now.to_i
+        return current_session_token if current_session_token && session_token_exp && session_token_exp > current_time
+      end
+
+      url    = ENV['OKAPI_URL']
+      tenant = ENV['OKAPI_TENANT']
+      resp   = CUL::FOLIO::Edge.authenticate(url, tenant, ENV['OKAPI_USER'], ENV['OKAPI_PW'])
+
+      if resp[:code].to_i >= 300
+        @logger.error "FolioTokenProvider: auth failed (code=#{resp[:code]})"
+        return nil
+      end
+
+      token      = resp[:token]
+      ttl_secs   = Integer(ENV.fetch('FOLIO_TOKEN_TTL_SECONDS', 45 * 60)) rescue 2700
+      expires_at = Time.now.to_i + ttl_secs
+
+      if @session
+        @session[:folio_token]            = token
+        @session[:folio_token_expires_at] = expires_at
+      end
+
+      token
+    end
+
+    # ===============================================================
+    # Deletes any cached token so the next fetch will re-authenticate
+    # ---------------------------------------------------------------
+    def invalidate
+      return unless @session
+
+      @session.delete(:folio_token)
+      @session.delete(:folio_token_expires_at)
+    end
   end
 end
