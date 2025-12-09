059da075c08127d44080e49d4ee59b4ec4fa4466
Jeremy Duncan <jpd294@cornell.edu>
2025-12-09 16:00:54 -0500

    DACCESS-690: added logging method back

--- blacklight-cornell/app/controllers/application_controller.rb
+++ blacklight-cornell/app/controllers/application_controller.rb
@@ -23,10 +23,17 @@ class ApplicationController < ActionController::Base
 
 # An array of strings to be added to HTML HEAD section of view.
 # See ApplicationHelper#render_head_content for details.
-   def extra_head_content
-       #Deprecation.warn Blacklight::LegacyControllerMethods, "#extra_head_content is NOT deprecated"
-        @extra_head_content ||= []
-   end
+  def extra_head_content
+    #Deprecation.warn Blacklight::LegacyControllerMethods, "#extra_head_content is NOT deprecated"
+    @extra_head_content ||= []
+  end
+
+
+  def show_login_action
+    # :nocov:
+      Rails.logger.info("es287_debug #{__FILE__}:#{__LINE__} logging in before hook")
+    # :nocov:
+  end
 
 protected
   def authenticate_user!
