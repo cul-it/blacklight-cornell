f8fb76a81d6e950373a4e80ce726e7a50f64bac4
Jeremy Duncan <jpd294@cornell.edu>
2025-12-22 13:21:55 -0500

    DACCESS-687: updated to handle tests separately

--- blacklight-cornell/app/controllers/book_bags_controller.rb
+++ blacklight-cornell/app/controllers/book_bags_controller.rb
@@ -30,8 +30,10 @@ class BookBagsController < CatalogController
   def authenticate
     #binding.pry
     if developer_bookbag?
-      # mock_auth
       dev_sign_in
+    elsif test_bookbag?
+      mock_auth
+      :authenticate_user!
     else
       :authenticate_user!
       user = current_user
@@ -40,31 +42,31 @@ class BookBagsController < CatalogController
   end
 
   #####  TODO: Is this needed or used?  #######
-  # def mock_auth
-  #   return unless developer_bookbag?
-  #
-  #   OmniAuth.config.test_mode = true
-  #   OmniAuth.config.mock_auth[:saml] = OmniAuth::AuthHash.new({
-  #     provider: "saml",
-  #     "saml_resp" => "hello",
-  #     uid: "12345678910",
-  #     extra: { raw_info: {} },
-  #     info: {
-  #       email: [ENV["DEBUG_USER"]],
-  #       name: ["Diligent Tester"],
-  #       netid: "jgr25",
-  #       groups: ["staff", "student"],
-  #       primary: ["staff"],
-  #       first_name: "Diligent",
-  #       last_name: "Tester",
-  #     },
-  #     credentials: {
-  #       token: "abcdefg12345",
-  #       refresh_token: "12345abcdefg",
-  #       expires_at: DateTime.now,
-  #     },
-  #   })
-  # end
+  def mock_auth
+    if !ENV["LOGIN_REQUIRED"].present? && ENV["DEBUG_USER"].present? && (Rails.env.development? || Rails.env.test?)
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:saml] = OmniAuth::AuthHash.new({
+        provider: "saml",
+        "saml_resp" => "hello",
+        uid: "12345678910",
+        extra: { raw_info: {} },
+        info: {
+          email: [ENV["DEBUG_USER"]],
+          name: ["Diligent Tester"],
+          netid: "jgr25",
+          groups: ["staff", "student"],
+          primary: ["staff"],
+          first_name: "Diligent",
+          last_name: "Tester",
+        },
+        credentials: {
+          token: "abcdefg12345",
+          refresh_token: "12345abcdefg",
+          expires_at: DateTime.now,
+        },
+      })
+    end
+  end
 
   def set_book_bag_name
     #binding.pry
@@ -251,7 +253,11 @@ class BookBagsController < CatalogController
   ### Developer-Mode Helper Methods  ##
   #####################################
   def developer_bookbag?
-    !ENV["LOGIN_REQUIRED"].present? && ENV["DEBUG_USER"].present? && (Rails.env.development? || Rails.env.test?)
+    !ENV["LOGIN_REQUIRED"].present? && ENV["DEBUG_USER"].present? && (Rails.env.development?)
+  end
+
+  def test_bookbag?
+    !ENV["LOGIN_REQUIRED"].present? && ENV["DEBUG_USER"].present? && (Rails.env.test?)
   end
 
   def dev_sign_in
