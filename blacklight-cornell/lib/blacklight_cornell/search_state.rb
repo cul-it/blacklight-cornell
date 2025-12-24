0a47a5fc188d2a45dbab22d47d56da8b1d0d3cd4
Jeremy Duncan <jpd294@cornell.edu>
2025-12-24 13:35:48 -0500

    DACCESS-751: Override method to clear deprecation warning

--- blacklight-cornell/lib/blacklight_cornell/search_state.rb
+++ blacklight-cornell/lib/blacklight_cornell/search_state.rb
@@ -9,5 +9,16 @@ module BlacklightCornell
       params[:q_row]
     end
 
+    ############################################################################
+    ##  Resolves -- DEPRECATION WARNING:
+    ##  add_facet_params is deprecated and will be removed from a future release
+    ##  (Use filter(field).add(item) instead).
+    ## -------------------------------------------------------------------------
+    def add_facet_params_and_redirect(field, item)
+      new_params = filter(field).add(item).params.to_h.with_indifferent_access
+      request_keys = blacklight_config.facet_paginator_class.request_keys
+      new_params.extract!(*request_keys.values)
+      new_params
+    end
   end
 end
