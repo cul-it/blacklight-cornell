4e5117943b53ad65fa5bc3a48cb14c897be37d90
Emily Toner <ebtoner@users.noreply.github.com>
2026-03-09 14:49:27 -0400

    DACCESS-859 Address Blacklight::DefaultComponentConfiguration deprecation warning

--- blacklight-cornell/app/controllers/catalog_controller.rb
+++ blacklight-cornell/app/controllers/catalog_controller.rb
@@ -3,10 +3,7 @@ class CatalogController < ApplicationController
 
   include BlacklightRangeLimit::ControllerOverride
   include Blacklight::Catalog
-
-#  include Blacklight::SearchHelper
   include BlacklightCornell::CornellCatalog
-  include Blacklight::DefaultComponentConfiguration
   include BlacklightUnapi::ControllerExtension
   include Blacklight::Marc::Catalog
   require 'rest-client'
@@ -188,6 +185,18 @@ class CatalogController < ApplicationController
     # solr field configuration for document/show views
     config.show.title_field = 'title_display'
     config.show.display_type_field = 'format'
+
+    # other solr field configurations for index/show views
+    config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    config.add_results_collection_tool(:sort_widget)
+    config.add_results_collection_tool(:per_page_widget)
+    config.add_results_collection_tool(:view_type_group)
+    config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
+    # config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
+    # config.add_show_tools_partial(:citation)
+    config.add_nav_action(:bookmark, partial: 'blacklight/nav/bookmark', if: :render_bookmarks_control?)
+    config.add_nav_action(:search_history, partial: 'blacklight/nav/search_history')
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
