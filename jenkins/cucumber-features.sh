502968257bf819b9befefe748ca0366f2f3d27a1
Jeremy Duncan <jpd294@cornell.edu>
2026-03-11 13:33:03 -0400

    DACCESS-737: added mkdir for coverage, added Docker UID & GID

--- jenkins/cucumber-features.sh
+++ jenkins/cucumber-features.sh
@@ -11,6 +11,9 @@ cp /cul/data/jenkins/environments/blacklight-cornell.env container_env_test.env
 export COVERAGE=on
 export RAILS_ENV_FILE=./container_env_test.env
 export COVERAGE_PATH=${JENKINS_WORKSPACE}/blacklight-cornell/coverage
+export DOCKER_UID=$(id -u)
+export DOCKER_GID=$(id -g)
+mkdir -p ${COVERAGE_PATH}
 project_name="container-discovery-test-${TEST_ID}"
 echo "Cucumber tests for ${project_name}"
 docker compose -f docker-compose-test.yaml build
