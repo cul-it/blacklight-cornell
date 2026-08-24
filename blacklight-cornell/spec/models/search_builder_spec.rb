6d8616b6650a424ca0e433761f6a016222705f5f
Christina Cortland <christina.cortland@cornell.edu>
2026-08-24 12:14:43 -0400

    DACCESS-978 - Fix broken emails by not setting query for q with ids

--- blacklight-cornell/spec/models/search_builder_spec.rb
+++ blacklight-cornell/spec/models/search_builder_spec.rb
@@ -1218,6 +1218,20 @@ RSpec.describe SearchBuilder, type: :model do
       end
     end
 
+    context 'id lookup with no search_field param (e.g. email/sms/citation actions)' do
+      # Blacklight::SearchBuilder#where (used by fetch_many for id-based lookups)
+      # stashes the id-filter conditions Hash into blacklight_params[:q]. set_query
+      # must leave solr_parameters[:q] alone in this case so the id-filter query
+      # built later by add_additional_filters isn't clobbered.
+      let(:blacklight_params) { { q: { 'id' => ['8638864'] } } }
+      let(:solr_params) { { q: '{!lucene}id:("8638864")' } }
+
+      it 'does not overwrite the existing solr q param' do
+        search_builder.set_query(solr_params)
+        expect(solr_params[:q]).to eq('{!lucene}id:("8638864")')
+      end
+    end
+
     context 'simple catalog search with search_field param' do
       let(:q) { 'test1 test2 test3'}
       let(:blacklight_params) { { q: q, search_field: search_field } }
